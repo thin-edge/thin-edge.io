c36a914ba1d7a3a9bfa30fb8a8a15ce7e27961b0
Wenzek <diw@softwareag.com>
2020-12-08 17:04:37 +0000

    [CIT-122] Refactore example `temperature_publisher`
    * Ease reading
    * Manage parallel asynchronous pub and sub
    * Test proper MQTT disconnection

--- mqtt/mqtt_client/examples/temperature_publisher.rs
+++ mqtt/mqtt_client/examples/temperature_publisher.rs
@@ -13,27 +13,21 @@ pub async fn main() -> Result<(), mqtt_client::Error> {
     let c8y_cmd = Topic::new("c8y/s/ds")?;
     let c8y_err = Topic::new("c8y/s/e")?;
 
-    let mut errors = mqtt.subscribe_errors();
-    tokio::spawn(async move {
-        while let Some(error) = errors.next().await {
-            eprintln!("ERROR: {}", error);
-        }
-    });
+    tokio::select! {
+        _ = publish_temperature(&mqtt, c8y_msg) => (),
+        _ = listen_command(&mqtt, c8y_cmd) => (),
+        _ = listen_c8y_error(&mqtt, c8y_err) => (),
+        _ = listen_error(&mqtt) => (),
+    }
 
-    let mut messages = mqtt.subscribe(c8y_cmd.filter()).await?;
-    tokio::spawn(async move {
-        while let Some(message) = messages.next().await {
-            if message.topic == c8y_cmd {
-                println!("C8Y command: {:?}", message.payload);
-            } else if message.topic == c8y_err {
-                println!("C8Y error: {:?}", message.payload);
-            }
-        }
-    });
+    mqtt.disconnect().await
+}
 
+async fn publish_temperature(mqtt: &Client, c8y_msg: Topic) -> Result<(), mqtt_client::Error> {
     let mut rng = thread_rng();
     let mut temperature: i32 = rng.gen_range(-10, 20);
-    loop {
+
+    for _ in 1..10 {
         let delta = rng.gen_range(-1, 2);
         temperature = temperature + delta;
 
@@ -42,4 +36,42 @@ pub async fn main() -> Result<(), mqtt_client::Error> {
 
         sleep(Duration::from_millis(1000)).await;
     }
+
+    Ok(())
+}
+
+async fn listen_command(mqtt: &Client, c8y_cmd: Topic) -> Result<(), mqtt_client::Error> {
+    let mut messages = mqtt.subscribe(c8y_cmd.filter()).await?;
+
+    while let Some(message) = messages.next().await {
+        println!("C8Y command: {:?}", message.payload);
+        if let Some(cmd) = std::str::from_utf8(&message.payload).ok() {
+            if cmd.contains(C8Y_TPL_RESTART) {
+                println!("Stopping on remote request ... should be restarted by the daemon monitor.");
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn listen_c8y_error(mqtt: &Client, c8y_err: Topic) -> Result<(), mqtt_client::Error> {
+    let mut messages = mqtt.subscribe(c8y_err.filter()).await?;
+
+    while let Some(message) = messages.next().await {
+        println!("C8Y error: {:?}", message.payload);
+    }
+
+    Ok(())
+}
+
+async fn listen_error(mqtt: &Client) -> Result<(), mqtt_client::Error> {
+    let mut errors = mqtt.subscribe_errors();
+
+    while let Some(error) = errors.next().await {
+        eprintln!("ERROR: {}", error);
+    }
+
+    Ok(())
 }
