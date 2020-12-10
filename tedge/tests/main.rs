d5f80804d014265e71741ba8d831839f08554ca3
Your Name <you@example.com>
2020-12-10 20:36:41 +0000

    Add basic tests for cli.

---
+++ tedge/tests/main.rs
@@ -0,0 +1,31 @@
+mod tests {
+
+    use assert_cmd::prelude::*; // Add methods on commands
+    use predicates::prelude::*; // Used for writing assertions
+    use std::process::Command; // Run programs
+
+    #[test]
+    fn run_help() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("tedge")?;
+
+        cmd.arg("--help");
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("USAGE"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn run_version() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("tedge")?;
+
+        let version_string = format!("tedge {}", env!("CARGO_PKG_VERSION"));
+        cmd.arg("-V");
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::starts_with(version_string));
+
+        Ok(())
+    }
+}
