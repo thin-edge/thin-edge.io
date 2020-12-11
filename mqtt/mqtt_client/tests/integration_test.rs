45183d98fa1bfb0ce25278819677684e9b9aa485
Wenzek <diw@softwareag.com>
2020-12-11 10:37:14 +0000

    [CIT-122] Fix system tests
    Use `test.mosquitto.org`

--- mqtt/mqtt_client/tests/integration_test.rs
+++ mqtt/mqtt_client/tests/integration_test.rs
@@ -1,39 +1,31 @@
-use mqtt_client::{Client,Message,Topic};
+use mqtt_client::{Config,Message,Topic};
 use std::time::Duration;
 use tokio::time::sleep;
 
 #[test]
 fn sending_and_receiving_a_message() {
-    //broker::start();
 
     async fn scenario(payload: String) -> Result<Option<Message>, mqtt_client::Error> {
-        let topic = Topic::new("test")?;
-        let subscriber = Client::connect("subscriber").await?;
+        let test_broker = Config { host: String::from("test.mosquitto.org"), port: 1883 };
+
+        let topic = Topic::new("test/uubpb9wyi9asi46l624f")?;
+        let subscriber = test_broker.connect("subscriber").await?;
         let mut received = subscriber.subscribe(topic.filter()).await?;
 
         let message = Message::new(&topic, payload);
-        let publisher = Client::connect("publisher").await?;
+        let publisher = test_broker.connect("publisher").await?;
         publisher.publish(message).await?;
 
         tokio::select! {
             msg = received.next() => Ok(msg),
-            _ = sleep(Duration::from_millis(3000)) => Ok(None)
+            _ = sleep(Duration::from_millis(1000)) => Ok(None)
         }
     };
 
     let payload= String::from("Hello there!");
     match tokio_test::block_on(scenario(payload.clone())) {
         Ok(Some(rcv_message)) => assert_eq!(rcv_message.payload, payload.as_bytes()),
-        Ok(None) => panic!("Got no message after 3s"),
+        Ok(None) => panic!("Got no message after 1s"),
         Err(e) => panic!("Got an error: {}", e),
     }
 }
-/*
-mod broker {
-    use librumqttd::{Broker,Config};
-
-    pub fn start() {
-        let mut broker = Broker::new(Config::default());
-        broker.start().unwrap();
-    }
-}*/
