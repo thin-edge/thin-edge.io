c8fafaceca66febcd2025bc5cf028cb0666813bc
Wenzek <diw@softwareag.com>
2020-12-18 09:54:14 +0000

    [CIT-122] Remove unused file

--- mqtt/mqtt_client/tests/common/mod.rs
+++
@@ -1,2 +0,0 @@
-// Code shared by all the integration tests, using `use common`.
-// See [Submodules in Integration Tests](https://doc.rust-lang.org/book/ch11-03-test-organization.html#submodules-in-integration-tests)
