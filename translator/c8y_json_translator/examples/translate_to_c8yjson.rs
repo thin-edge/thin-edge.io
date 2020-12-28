e2222f3355e345152f53862dc7caaea204e5f8e3
Pradeep K J <pradeekumar.kj@softwareag.com>
2020-12-28 15:00:47 +0530

    [CIT-102] Added a test to test timestamp and type translation

--- translator/c8y_json_translator/examples/translate_to_c8yjson.rs
+++ translator/c8y_json_translator/examples/translate_to_c8yjson.rs
@@ -2,8 +2,7 @@ use c8y_json_translator::ThinEdgeJson;
 use chrono::prelude::*;
 
 fn single_value_translation() {
-    let single_value_thin_edge_json = 
-    r#"  {
+    let single_value_thin_edge_json = r#"  {
     "temperature": 23,
     "pressure": 220
    }"#;
@@ -25,8 +24,7 @@ fn multi_value_translation() {
     let time: DateTime<Utc> = Utc::now();
     let msg_type = "MultiValueThinEdgeMeasurement";
 
-    let multi_value_thin_edge_json =
-    r#"   {
+    let multi_value_thin_edge_json = r#"   {
       "temperature": 0 ,
       "location": {
         "latitude": 32.54,
@@ -45,7 +43,33 @@ fn multi_value_translation() {
     );
 }
 
+fn thin_edge_translation_with_type_and_time_stamp() {
+    let single_value_thin_edge_json_with_type_and_time = r#"   {
+     "type": "ThinEdgeWithTimeStamp",
+     "time" : "2013-06-22T17:03:14.000+02:00",
+     "temperature": 23,
+     "pressure": 220
+    }"#;
+
+    let time: DateTime<Utc> = Utc::now();
+    let msg_type = "SingleValueThinEdgeMeasurementWithTimeStamp";
+
+    println!(
+        "\nThin_Edge_Json: \n{:#}",
+        single_value_thin_edge_json_with_type_and_time
+    );
+    println!(
+        "\nc8yjson: \n {}",
+        ThinEdgeJson::from_utf8(
+            &String::from(single_value_thin_edge_json_with_type_and_time).into_bytes()
+        )
+        .unwrap()
+        .into_cumulocity_json(time, msg_type)
+    );
+}
+
 pub fn main() {
     single_value_translation();
     multi_value_translation();
+    thin_edge_translation_with_type_and_time_stamp();
 }
