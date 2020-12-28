233311f495c817ab19dfaea18d484d8940f67b9b
Pradeep K J <pradeekumar.kj@softwareag.com>
2020-12-28 14:42:15 +0530

    [CIT-102] Fixed the formating and names in example

--- translator/c8y_json_translator/examples/translate_to_c8yjson.rs
+++ translator/c8y_json_translator/examples/translate_to_c8yjson.rs
@@ -2,19 +2,20 @@ use c8y_json_translator::ThinEdgeJson;
 use chrono::prelude::*;
 
 fn single_value_translation() {
-    let single_value_thinedge_json = r#"{
-                  "temperature": 23,
-                  "pressure": 220
-               }"#;
+    let single_value_thin_edge_json = 
+    r#"  {
+    "temperature": 23,
+    "pressure": 220
+   }"#;
 
     let time: DateTime<Utc> = Utc::now();
     let msg_type = "SingleValueThinEdgeMeasurement";
 
-    println!("Tedge_Json: {:#}", single_value_thinedge_json);
+    println!("Thin_Edge_Json: \n{:#}", single_value_thin_edge_json);
 
     println!(
-        "c8yjson: \n {}",
-        ThinEdgeJson::from_utf8(&String::from(single_value_thinedge_json).into_bytes())
+        "\nc8yjson: \n {:#}",
+        ThinEdgeJson::from_utf8(&String::from(single_value_thin_edge_json).into_bytes())
             .unwrap()
             .into_cumulocity_json(time, msg_type)
     );
@@ -22,22 +23,23 @@ fn single_value_translation() {
 
 fn multi_value_translation() {
     let time: DateTime<Utc> = Utc::now();
-    let msg_type = "SingleValueThinEdgeMeasurement";
+    let msg_type = "MultiValueThinEdgeMeasurement";
 
-    let input = r#"{
-                "temperature": 0 ,
-                "location": {
-                      "latitude": 32.54,
-                      "longitude": -117.67,
-                      "altitude": 98.6
-                  },
-                "pressure": 98
-    }"#;
+    let multi_value_thin_edge_json =
+    r#"   {
+      "temperature": 0 ,
+      "location": {
+        "latitude": 32.54,
+        "longitude": -117.67,
+        "altitude": 98.6
+      },
+      "pressure": 98
+   }"#;
 
-    println!("Tedge_Json: {:#}", input);
+    println!("\nThin_Edge_Json: \n{:#}", multi_value_thin_edge_json);
     println!(
-        "c8yjson: \n {}",
-        ThinEdgeJson::from_utf8(&String::from(input).into_bytes())
+        "\nc8yjson: \n {}",
+        ThinEdgeJson::from_utf8(&String::from(multi_value_thin_edge_json).into_bytes())
             .unwrap()
             .into_cumulocity_json(time, msg_type)
     );
