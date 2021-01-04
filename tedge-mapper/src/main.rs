0e3ba13f2ca3201d10e1fff00496d41d89574c62
Your Name <you@example.com>
2021-01-04 12:35:20 +0000

    Add client mapper struct to map from open json to csv mqtt

--- tedge-mapper/src/main.rs
+++ tedge-mapper/src/main.rs
@@ -1,24 +1,49 @@
 use env_logger::Env;
 
-// use rumqttc::AsyncClient;
-// use rumqttc::Event;
-// use rumqttc::Incoming;
-// use rumqttc::MqttOptions;
-// use rumqttc::QoS;
+use log;
 
-use std::error::Error;
+use mqtt_client::Client;
+
+use std::net::TcpStream;
 
 mod mapper;
 
+const DEFAULT_LOG_LEVEL: &str = "warn";
+
+const EX_NOHOST: i32 = 68;
+
+const NAME: &str = "tedge_mapper";
+
+const MQTT_PORT: u16 = 1883;
+const MQTT_URL: &str = "localhost";
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
+async fn main() -> Result<(), mqtt_client::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or(DEFAULT_LOG_LEVEL)).init();
 
     log::info!("tedge-mapper starting!");
 
-    let mut mapper = mapper::Mapper::new();
-    mapper.connect().await?;
-    mapper.run_forever().await?;
+    let server_available = scan_port(MQTT_PORT);
+    if !server_available {
+        log::error!("tedge-mapper failed to start, no server available!");
+        std::process::exit(EX_NOHOST)
+    }
+
+    let config = mqtt_client::Config {
+        host: MQTT_URL.to_owned(),
+        port: MQTT_PORT,
+    };
+    let mqtt = Client::connect(NAME, &config).await?;
+
+    let mapper = mapper::Mapper::new(mqtt, "tedge/measurements", "c8y/s/us", "tedge/errors");
+    mapper.subscribe_messages().await?;
 
     Ok(())
+}
+
+fn scan_port(port: u16) -> bool {
+    match TcpStream::connect(("localhost", port)) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
--- tedge-mapper/src/mapper.rs
+++ tedge-mapper/src/mapper.rs
@@ -1,59 +1,152 @@
-use rumqttc::AsyncClient;
-use rumqttc::Event;
-use rumqttc::Incoming;
-use rumqttc::MqttOptions;
-use rumqttc::QoS;
+use std::time::Duration;
 
-use std::error::Error;
+use log;
 
-const MQTT_ID: &str = "tedge-mapper";
-const LOCALHOST: &str = "localhost";
-const MQTT_HOST: &str = LOCALHOST;
-const MQTT_PORT: u16 = 1883;
+use json::JsonValue;
+
+use mqtt_client::Client;
+use mqtt_client::Message;
+use mqtt_client::Topic;
+use tokio::task::JoinHandle;
+
+const IN_TOPIC: &str = "tedge/measurements";
+const C8Y_TOPIC: &str = "c8y/s/us";
+const ERRORS_TOPIC: &str = "tedge/errors";
 
 pub struct Mapper {
-    client: rumqttc::AsyncClient,
-    eventloop: rumqttc::EventLoop,
+    client: mqtt_client::Client,
+    in_topic: mqtt_client::Topic,
+    out_topic: mqtt_client::Topic,
+    err_topic: mqtt_client::Topic,
 }
 
+const C8Y_TEMPLATE_TEMPERATURE: &str = "211";
+
 impl Mapper {
-    pub fn new() -> Mapper {
-        log::info!("tedge-mapper starting!");
+    pub fn new(
+        client: mqtt_client::Client,
+        in_topic: &str,
+        out_topic: &str,
+        err_topic: &str,
+    ) -> Mapper {
+        let new_in_topic = match Topic::new(in_topic) {
+            Ok(topic) => topic,
+            Err(error) => {
+                log::error!("{}", error);
+                Topic {
+                    name: IN_TOPIC.to_string(),
+                }
+            }
+        };
 
-        let mut mqttoptions = MqttOptions::new(MQTT_ID, MQTT_HOST, MQTT_PORT);
-        mqttoptions.set_keep_alive(30);
-        mqttoptions.set_connection_timeout(10);
+        let new_out_topic = match Topic::new(out_topic) {
+            Ok(topic) => topic,
+            Err(error) => {
+                log::error!("{}", error);
+                Topic {
+                    name: C8Y_TOPIC.to_string(),
+                }
+            }
+        };
 
-        let (client, eventloop) = AsyncClient::new(mqttoptions, 10);
+        let new_err_topic = match Topic::new(err_topic) {
+            Ok(topic) => topic,
+            Err(error) => {
+                log::error!("{}", error);
+                Topic {
+                    name: ERRORS_TOPIC.to_string(),
+                }
+            }
+        };
 
         Mapper {
             client: client,
-            eventloop: eventloop,
+            in_topic: new_in_topic,
+            out_topic: new_out_topic,
+            err_topic: new_err_topic,
         }
     }
 
-    pub async fn connect(&self) -> Result<(), Box<dyn Error>> {
-        Ok(self
-            .client
-            .subscribe("tedge/measurements", QoS::AtMostOnce)
-            .await?)
+    fn subsribe_errors(&self) -> JoinHandle<()> {
+        let mut errors = self.client.subscribe_errors();
+        tokio::spawn(async move {
+            while let Some(error) = errors.next().await {
+                log::error!("{}", error);
+            }
+        })
     }
 
-    pub async fn run_forever(&mut self) -> Result<(), Box<dyn Error>> {
-        loop {
-            match self.eventloop.poll().await {
-                Ok(Event::Incoming(Incoming::Publish(p))) => {
-                    log::debug!("Topic: {}, Payload: {:?}", p.topic, p.payload)
+    pub async fn subscribe_messages(&self) -> Result<(), mqtt_client::Error> {
+        self.subsribe_errors();
+        let mut messages = self.client.subscribe(self.in_topic.filter()).await?;
+        Ok(while let Some(message) = messages.next().await {
+            log::debug!("Mapping {:?}", message);
+            match Mapper::map(&message.payload) {
+                Ok(mapped) => {
+                    self.client
+                        .publish(Message::new(&self.out_topic, mapped))
+                        .await?
                 }
-                Ok(Event::Incoming(i)) => {
-                    log::debug!("Incoming = {:?}", i);
-                }
-                Ok(Event::Outgoing(o)) => println!("Outgoing = {:?}", o),
-                Err(e) => {
-                    println!("Error = {:?}", e);
-                    return Err(Box::new(e));
+                Err(error) => {
+                    log::debug!("Translation error: {}", error);
+                    self.client
+                        .publish(Message::new(&self.err_topic, error))
+                        .await?
                 }
             }
+        })
+    }
+
+    /// mapper which extracts the temperature field from a ThinEdge Json value.
+    ///
+    ///
+    pub fn map(input: &Vec<u8>) -> Result<Vec<u8>, String> {
+        let input = std::str::from_utf8(input).map_err(|err| format!("ERROR: {}", err))?;
+        let json = json::parse(input).map_err(|err| format!("ERROR: {}", err))?;
+        match json {
+            JsonValue::Object(obj) => {
+                for (key, value) in obj.iter() {
+                    match key {
+                        "temperature" => {}
+                        _ => {
+                            return Err(format!("ERROR: only supported temperature, not '{}'", key))
+                        }
+                    };
+
+                    match value {
+                        JsonValue::Number(num) => {
+                            let value: f64 = (*num).into();
+                            if value == 0.0 || value.is_normal() {
+                                return Ok(
+                                    format!("{},{}", C8Y_TEMPLATE_TEMPERATURE, value).into_bytes()
+                                );
+                            } else {
+                                return Err(format!("ERROR: value out of range '{}'", value));
+                            }
+                        }
+                        _ => return Err(format!("ERROR: expected a number, not '{}'", value)),
+                    }
+                }
+                return Err(String::from("ERROR: empty measurement"));
+            }
+            _ => return Err(format!("ERROR: expected a JSON object, not {}", json)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_mapper_convert_number() {
+        let input = String::into_bytes("{\"temperature\": 124}".to_owned());
+        let output = Result::Ok(String::into_bytes("211,124".to_owned()));
+        assert_eq!(Mapper::map(&input), output);
+    }
+    #[test]
+    fn test_mapper_convert_string() {
+        let input = String::into_bytes("{\"temperature\": \"test\"}".to_owned());
+        let output = Result::Err("ERROR: expected a number, not \'test\'".to_owned());
+        assert_eq!(Mapper::map(&input), output);
     }
 }
