ec52c949597ce3c1e564c112727a59a866c144d7
Didier Wenzek <didier.wenzek@acidalie.com>
2021-01-11 18:21:56 +0000

    [CIT-111] First working version

--- tedge/src/certificate.rs
+++ tedge/src/certificate.rs
@@ -1,5 +1,11 @@
 use super::command::Cmd;
+use chrono::offset::Utc;
+use chrono::Duration;
+use rcgen::Certificate;
+use rcgen::CertificateParams;
 use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -28,7 +34,49 @@ pub enum CertCmd {
 
 impl Cmd for CertCmd {
     fn run(&self, _verbose: u8) -> Result<(), Box<dyn Error>> {
-        println!("{:?}", self);
-        Ok(())
+        match self {
+            CertCmd::Create {
+                id,
+                cert_path,
+                key_path,
+            } => create_test_certificate(id, cert_path, key_path),
+            _ => {
+                println!("{:?}", self);
+                Ok(())
+            }
+        }
     }
+}
+
+fn create_test_certificate(
+    id: &str,
+    cert_path: &str,
+    key_path: &str,
+) -> Result<(), Box<dyn Error>> {
+
+    let mut distinguished_name = rcgen::DistinguishedName::new();
+    distinguished_name.push(rcgen::DnType::CommonName, id);
+    distinguished_name.push(rcgen::DnType::OrganizationName, "Thin Edge");
+    distinguished_name.push(rcgen::DnType::OrganizationalUnitName, "Test Device");
+
+    let not_before = Utc::now();
+    let not_after = not_before + Duration::days(90);
+
+    let mut params = CertificateParams::default();
+    params.distinguished_name = distinguished_name;
+    params.not_before = not_before;
+    params.not_after = not_after;
+    params.alg = &rcgen::PKCS_ECDSA_P256_SHA256; // ECDSA signing using the P-256 curves and SHA-256 hashing as per RFC 5758
+
+    let cert = Certificate::from_params(params)?;
+
+    let cert_pem = cert.serialize_pem()?;
+    let mut cert_file = File::create(cert_path)?;
+    cert_file.write_all(cert_pem.as_bytes())?;
+
+    let cert_key = cert.serialize_private_key_pem();
+    let mut key_file = File::create(key_path)?;
+    key_file.write_all(cert_key.as_bytes())?;
+
+    Ok(())
 }
