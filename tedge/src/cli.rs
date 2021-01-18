8bdad75e47f6c574712d5d6184423eed85d11fa5
Didier Wenzek <didier.wenzek@acidalie.com>
2021-01-18 12:58:13 +0100

    [CIT-111] Create self signed certificates (#17)
    * [CIT-111] Add `tedge cert` help
    
    * [CIT-111] Add a Cmd trait for all commands
    
    * [CIT-111] First working version
    
    * [CIT-111] Fix the CA attribute of the generated certificate
    
    Cumulocity requires that
    "Uploaded certificate in version 3 needs to have CA : true constraint extension"
    
    * [CIT-111] Ensure the certificate is valid today
    
    * [CIT-111] Add pre-requisite checks
    
    * [CIT-111] Improve the Command trait
    
    * [CIT-111] Add tests
    
    * [CIT-111] Use unimplented! instead of println!("unimplemeted")
    
    * [CIT-122] Add explicit dependency to rcgen pem feature.
    
    * [CIT-111] Simplify code
    
    Removing a `map_err(|err| err.map_into())`
    
    * [CIT-111] Simplify the logic to display error messages in context
    
    * [CIT-111] Move the magic values in CertConfig
    
    * [CIT-111] Check the parameters are correct
    
    * [CIT-111] Improve error handling
    
    * [CIT-111] Zero the private key after use
    
    * [CIT-111] Fix: private key is not zeroized on error
    
    * [CIT-111] Fix race condition with pre-checks on files
    
    * [CIT-111] Remove unnecessary checks
    
    Co-authored-by: Wenzek <diw@softwareag.com>

--- tedge/src/cli.rs
+++ tedge/src/cli.rs
@@ -1,3 +1,4 @@
+use super::command::Command;
 use structopt::clap;
 use structopt::StructOpt;
 
@@ -21,6 +22,9 @@ pub struct Opt {
 enum TEdgeCmd {
     /// Configure Thin Edge.
     Config(ConfigCmd),
+
+    /// Create and manage device certificate
+    Cert(super::certificate::CertCmd),
 }
 
 #[derive(StructOpt, Debug)]
@@ -36,4 +40,45 @@ enum ConfigCmd {
 
     /// Get value.
     Get { key: String },
+}
+
+impl ToString for Opt {
+    fn to_string(&self) -> String {
+        self.tedge_cmd.to_string()
+    }
+}
+
+impl Opt {
+    pub fn run(&self) -> Result<(), anyhow::Error> {
+        self.tedge_cmd.run(self.verbose)
+    }
+}
+
+impl TEdgeCmd {
+    fn sub_command(&self) -> &dyn Command {
+        match self {
+            TEdgeCmd::Config(ref cmd) => cmd,
+            TEdgeCmd::Cert(ref cmd) => cmd,
+        }
+    }
+}
+
+impl Command for TEdgeCmd {
+    fn to_string(&self) -> String {
+        self.sub_command().to_string()
+    }
+
+    fn run(&self, verbose: u8) -> Result<(), anyhow::Error> {
+        self.sub_command().run(verbose)
+    }
+}
+
+impl Command for ConfigCmd {
+    fn to_string(&self) -> String {
+        format!("{:?}", self)
+    }
+
+    fn run(&self, _verbose: u8) -> Result<(), anyhow::Error> {
+        unimplemented!("{:?}", self);
+    }
 }
--- tedge/src/main.rs
+++ tedge/src/main.rs
@@ -1,8 +1,12 @@
+use anyhow::Context;
 use structopt::StructOpt;
 
+mod certificate;
 mod cli;
+mod command;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let opt = cli::Opt::from_args();
-    println!("{:?}", opt);
+    opt.run()
+        .with_context(|| format!("fail to {}", opt.to_string()))
 }
