06dc599a73b85f74907569f25d0ddf17a6a97b6a
Pradeep K J <pradeekumar.kj@softwareag.com>
2021-02-26 19:00:19 +0530

    az working except check connection

--- tedge/src/cli/connect/mod.rs
+++ tedge/src/cli/connect/mod.rs
@@ -1,6 +1,10 @@
 use crate::command::Command;
 use structopt::StructOpt;
 
+use crate::utils::{paths,services};
+use crate::config::ConfigError;
+use tempfile::PersistError;
+
 mod c8y;
 mod az;
 
@@ -35,3 +39,43 @@ impl Command for ConnectCmd {
         self.sub_command().run(verbose)
     }
 }
+
+#[derive(thiserror::Error, Debug)]
+enum ConnectError {
+    #[error("Bridge has been configured, but {cloud} connection check failed.")]
+    BridgeConnectionFailed {cloud: String},
+
+    #[error("Couldn't load certificate, provide valid certificate path in configuration. Use 'tedge config --set'")]
+    Certificate,
+
+    #[error(transparent)]
+    Configuration(#[from] ConfigError),
+
+    #[error("Connection is already established. To remove existing connection use 'tedge disconnect {cloud}' and try again.",)]
+    ConfigurationExists {cloud: String},
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+
+    #[error("Required configuration item is not provided '{item}', run 'tedge config set {item} <value>' to add it to config.")]
+    MissingRequiredConfigurationItem { item: String },
+
+    #[error(transparent)]
+    MqttClient(#[from] mqtt_client::Error),
+
+    #[error(transparent)]
+    PathsError(#[from] paths::PathsError),
+
+    #[error(transparent)]
+    PersistError(#[from] PersistError),
+
+    #[error("Couldn't find path to 'sudo'. Update $PATH variable with 'sudo' path.\n{0}")]
+    SudoNotFound(#[from] which::Error),
+
+    #[error("Provided endpoint url is not valid, provide valid url.\n{0}")]
+    UrlParse(#[from] url::ParseError),
+
+    #[error(transparent)]
+    ServicesError(#[from] services::ServicesError),
+}
+
