38cfa635275b34812c74a8dfcb43065b42861da6
Wenzek <diw@softwareag.com>
2021-03-18 17:16:04 +0000

    [CIT-273] Add integration test

--- tedge/tests/mqtt.rs
+++ tedge/tests/mqtt.rs
@@ -3,6 +3,10 @@ mod tests {
 
     // These test cases need mosquitto on localhost on GH hosted machine.
 
+    use assert_cmd::Command;
+    use predicates::prelude::predicate;
+    use assert_cmd::assert::OutputAssertExt;
+
     #[test]
     #[cfg(feature = "mosquitto-available")]
     fn test_cli_pub_basic() -> Result<(), Box<dyn std::error::Error>> {
