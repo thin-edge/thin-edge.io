860acdb654d0bc3bcf00f3abfa3f9a6794ff7d2d
Michael Abel <info@abel-ikt.de>
2021-03-22 14:47:11 +0100

    Fix typos and review comments

--- ci/ci_run_all_tests.sh
+++ ci/ci_run_all_tests.sh
@@ -6,11 +6,11 @@
 # Expected environment variables to be set:
 # C8YPASS : Cumulocity password
 # C8YUSERNAME : Cumolocity username
-# C8YTENANT : Cumolocity tennant
+# C8YTENANT : Cumolocity tenant
 # C8YDEVICE : The device name
 # C8YDEVICEID : The device ID in Cumolocity
 # TIMEZONE : Your timezone (temporary)
-# TEBASEDIR : Base directory for the thin edge repo
+# TEBASEDIR : Base directory for the Thin-Edge repo
 # EXAMPLEDIR : The direcory of the sawtooth example
 
 set -e
