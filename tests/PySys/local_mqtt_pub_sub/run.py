274c5afed6992bfbc3d22f9d08c99df25e6a6dfe
Albin Suresh <albin.suresh@softwareag.com>
2021-03-29 11:42:06 +0530

    [CIT-212] mqtt sub command to print the message target topic

--- tests/PySys/local_mqtt_pub_sub/run.py
+++ tests/PySys/local_mqtt_pub_sub/run.py
@@ -20,10 +20,17 @@ class PySysTest(BaseTest):
         tedge = "/usr/bin/tedge"
         sudo = "/usr/bin/sudo"
 
-        sub = self.startProcess(
+        sub1 = self.startProcess(
             command=sudo,
             arguments=[tedge, "mqtt", "sub", "atopic"],
             stdouterr="tedge_sub",
+            background=True,
+        )
+
+        sub2 = self.startProcess(
+            command=sudo,
+            arguments=[tedge, "mqtt", "sub", "--no-topic", "atopic"],
+            stdouterr="tedge_sub_no_topic",
             background=True,
         )
 
@@ -47,10 +54,17 @@ class PySysTest(BaseTest):
         # not have the rights to do it
         kill = self.startProcess(
             command=sudo,
-            arguments=["kill", "-9", str(sub.pid)],
+            arguments=["kill", "-9", str(sub1.pid)],
+            stdouterr="kill_out",
+        )
+
+        kill = self.startProcess(
+            command=sudo,
+            arguments=["kill", "-9", str(sub2.pid)],
             stdouterr="kill_out",
         )
 
     def validate(self):
-        self.assertGrep("tedge_sub.out", "amessage", contains=True)
-        self.assertGrep("tedge_sub.out", "the message", contains=True)
+        self.assertGrep("tedge_sub.out", "\[atopic\] amessage", contains=True)
+        self.assertGrep("tedge_sub.out", "\[atopic\] the message", contains=True)
+        self.assertGrep("tedge_sub_no_topic.out", "atopic", contains=False)
