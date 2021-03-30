17a9e0295b462331887ca31e118deb8adfcdd16c
Michael Neumann <mneumann@ntecs.de>
2021-03-30 18:28:01 +0200

    Move tedge_config under providers
    - There can be many configuration providers, not just TEdgeConfig.

--- tedge_config/src/lib.rs
+++ tedge_config/src/lib.rs
@@ -1,13 +1,13 @@
 mod config_setting;
 mod error;
 mod settings;
-mod tedge_config;
+mod providers;
 mod traits;
 mod types;
 
 pub use self::config_setting::*;
 pub use self::error::*;
 pub use self::settings::*;
-pub use self::tedge_config::*;
+pub use self::providers::tedge_config::*;
 pub use self::traits::*;
 pub use self::types::*;
---
+++ tedge_config/src/providers/mod.rs
@@ -0,0 +1,1 @@
+pub mod tedge_config;
