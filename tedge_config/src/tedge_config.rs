6472cb57fe196b5e19bc7a5eceeb994843dfa095
Michael Neumann <mneumann@ntecs.de>
2021-04-07 15:19:06 +0200

    Refactor TEdge config (#155)
    - Refactor TEdgeConfig
    
    - Move configuration into `tedge_config` crate.
    
    - Add `ConfigSetting` trait. Each "thing" that we want to make
      configurable will have it's own struct-type. For example `struct
      DeviceIdSetting` or `C8yUrlSetting`.
    
    - The `ConfigSetting` trait does not provide any means of how to
      read/write/unset a configuration setting. It only provides the key,
      description and the value type of the setting, but no implementation.
    
    - Use trait `ConfigSettingAccessor` to implemenent `query`, `update` and
      `unset` methods.
    
    - `TEdgeConfigRepository` implements functionality to read/write a
      `TEdgeConfig`. The `TEdgeConfig` struct itself just holds data.
    
    - Add `TEdgeConfigLocation` which holds all data that we need to resolve the defaults in
      TEdgeConfig, including the name of the config file (TODO: we might want to split these two apart). 
      The defaults differ depending on the location of the `tedge.toml`, whether it's located in `/etc` or a users `$HOME/.tedge`
      (or in tests).
    
    - Only add the `tedge_config` crate. `tedge` is currently not modified
      to use the new API.
    
    - Remove remaining `unwrap`s, also from tests
    
    - No longer create intermediate directories upon saving configuration
    
    # FOLLOW-UP TASKS
    
    - Refactor the `tedge` crate to use `tedge_config`.
    - Compute the `device.id` from the certificate stored in `device.cert.path`, if any.
    - Ensure that the config is updated only by `tedge config set/unset`.
    - Extend tedge_config_location to also manage the paths to the bridge configuration files.
    - Update `ConfigRepository::store()` to properly set file ownership.

---
+++ tedge_config/src/tedge_config.rs
@@ -0,0 +1,176 @@
+use crate::*;
+use std::path::PathBuf;
+
+/// Represents the complete configuration of a thin edge device.
+/// This configuration is a wrapper over the device specific configurations
+/// as well as the IoT cloud provider specific configurations.
+///
+#[derive(Debug)]
+pub struct TEdgeConfig {
+    pub(crate) data: TEdgeConfigDto,
+    pub(crate) config_location: TEdgeConfigLocation,
+}
+
+impl ConfigSettingAccessor<DeviceIdSetting> for TEdgeConfig {
+    fn query(&self, _setting: DeviceIdSetting) -> ConfigSettingResult<String> {
+        self.data
+            .device
+            .id
+            .clone()
+            .ok_or(ConfigSettingError::ConfigNotSet {
+                key: DeviceIdSetting::KEY,
+            })
+    }
+
+    fn update(&mut self, _setting: DeviceIdSetting, _value: String) -> ConfigSettingResult<()> {
+        Err(ConfigSettingError::ReadonlySetting)
+    }
+
+    fn unset(&mut self, _setting: DeviceIdSetting) -> ConfigSettingResult<()> {
+        Err(ConfigSettingError::ReadonlySetting)
+    }
+}
+
+impl ConfigSettingAccessor<AzureUrlSetting> for TEdgeConfig {
+    fn query(&self, _setting: AzureUrlSetting) -> ConfigSettingResult<ConnectUrl> {
+        self.data
+            .azure
+            .url
+            .clone()
+            .ok_or(ConfigSettingError::ConfigNotSet {
+                key: AzureUrlSetting::KEY,
+            })
+    }
+
+    fn update(&mut self, _setting: AzureUrlSetting, value: ConnectUrl) -> ConfigSettingResult<()> {
+        self.data.azure.url = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: AzureUrlSetting) -> ConfigSettingResult<()> {
+        self.data.azure.url = None;
+        Ok(())
+    }
+}
+
+impl ConfigSettingAccessor<C8yUrlSetting> for TEdgeConfig {
+    fn query(&self, _setting: C8yUrlSetting) -> ConfigSettingResult<ConnectUrl> {
+        self.data
+            .c8y
+            .url
+            .clone()
+            .ok_or(ConfigSettingError::ConfigNotSet {
+                key: C8yUrlSetting::KEY,
+            })
+    }
+
+    fn update(&mut self, _setting: C8yUrlSetting, value: ConnectUrl) -> ConfigSettingResult<()> {
+        self.data.c8y.url = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: C8yUrlSetting) -> ConfigSettingResult<()> {
+        self.data.c8y.url = None;
+        Ok(())
+    }
+}
+
+impl ConfigSettingAccessor<DeviceCertPathSetting> for TEdgeConfig {
+    fn query(&self, _setting: DeviceCertPathSetting) -> ConfigSettingResult<PathBuf> {
+        Ok(self
+            .data
+            .device
+            .cert_path
+            .clone()
+            .unwrap_or_else(|| self.config_location.default_device_cert_path.clone()))
+    }
+
+    fn update(
+        &mut self,
+        _setting: DeviceCertPathSetting,
+        value: PathBuf,
+    ) -> ConfigSettingResult<()> {
+        self.data.device.cert_path = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: DeviceCertPathSetting) -> ConfigSettingResult<()> {
+        self.data.device.cert_path = None;
+        Ok(())
+    }
+}
+
+impl ConfigSettingAccessor<DeviceKeyPathSetting> for TEdgeConfig {
+    fn query(&self, _setting: DeviceKeyPathSetting) -> ConfigSettingResult<PathBuf> {
+        Ok(self
+            .data
+            .device
+            .key_path
+            .clone()
+            .unwrap_or_else(|| self.config_location.default_device_key_path.clone()))
+    }
+
+    fn update(
+        &mut self,
+        _setting: DeviceKeyPathSetting,
+        value: PathBuf,
+    ) -> ConfigSettingResult<()> {
+        self.data.device.key_path = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: DeviceKeyPathSetting) -> ConfigSettingResult<()> {
+        self.data.device.key_path = None;
+        Ok(())
+    }
+}
+
+impl ConfigSettingAccessor<AzureRootCertPathSetting> for TEdgeConfig {
+    fn query(&self, _setting: AzureRootCertPathSetting) -> ConfigSettingResult<PathBuf> {
+        Ok(self
+            .data
+            .azure
+            .root_cert_path
+            .clone()
+            .unwrap_or_else(|| self.config_location.default_azure_root_cert_path.clone()))
+    }
+
+    fn update(
+        &mut self,
+        _setting: AzureRootCertPathSetting,
+        value: PathBuf,
+    ) -> ConfigSettingResult<()> {
+        self.data.azure.root_cert_path = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: AzureRootCertPathSetting) -> ConfigSettingResult<()> {
+        self.data.azure.root_cert_path = None;
+        Ok(())
+    }
+}
+
+impl ConfigSettingAccessor<C8yRootCertPathSetting> for TEdgeConfig {
+    fn query(&self, _setting: C8yRootCertPathSetting) -> ConfigSettingResult<PathBuf> {
+        Ok(self
+            .data
+            .c8y
+            .root_cert_path
+            .clone()
+            .unwrap_or_else(|| self.config_location.default_c8y_root_cert_path.clone()))
+    }
+
+    fn update(
+        &mut self,
+        _setting: C8yRootCertPathSetting,
+        value: PathBuf,
+    ) -> ConfigSettingResult<()> {
+        self.data.c8y.root_cert_path = Some(value);
+        Ok(())
+    }
+
+    fn unset(&mut self, _setting: C8yRootCertPathSetting) -> ConfigSettingResult<()> {
+        self.data.c8y.root_cert_path = None;
+        Ok(())
+    }
+}
