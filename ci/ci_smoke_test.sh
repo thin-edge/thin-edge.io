ec65290d58c9ee1702585bf8da9a29269e849fa6
Michael Abel <info@abel-ikt.de>
2021-04-07 12:12:54 +0200

    From now on, the bridge is disabled by default

--- ci/ci_smoke_test.sh
+++ ci/ci_smoke_test.sh
@@ -124,3 +124,5 @@ cp ./examples/sawtooth_publisher ~/
 # Uses thin-edge JSON for publishing
 ./ci/roundtrip_local_to_c8y.py -m JSON -pub ./examples/ -u $C8YUSERNAME -t $C8YTENANT -pass $C8YPASS -id $C8YDEVICEID
 
+# Disconnect the bridge
+sudo tedge disconnect c8y
