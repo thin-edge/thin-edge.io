67b21d4906c7fb7b088a32cbd61df9f47314c792
Michael Abel <info@abel-ikt.de>
2021-04-07 12:10:37 +0200

    use environment in smoketest

--- tests/PySys/smoketest_smartrest/run.py
+++ tests/PySys/smoketest_smartrest/run.py
@@ -2,12 +2,17 @@ import pysys
 from pysys.constants import *
 from pysys.basetest import BaseTest
 
+import sys
+sys.path.append('environments')
+from environment_c8y import EnvironmentC8y
+
 import time
 
 # This test relies on a already working bridge
 
-class PySysTest(BaseTest):
+class PySysTest(EnvironmentC8y):
     def setup(self):
+        super().setup()
         self.log.info("Setup")
         self.addCleanupFunction(self.mycleanup)
 
@@ -25,6 +30,7 @@ class PySysTest(BaseTest):
             self.abort(FAILED)
 
     def execute(self):
+        super().execute()
         self.log.info("Execute")
 
         script = self.project.tebasedir + "ci/roundtrip_local_to_c8y.py"
@@ -42,9 +48,11 @@ class PySysTest(BaseTest):
         )
 
     def validate(self):
+        super().validate()
         self.log.info("Validate")
         self.assertGrep('stdout.out', expr='Data verification PASSED', contains=True)
         self.assertGrep('stdout.out', expr='Timestamp verification PASSED', contains=True)
 
     def mycleanup(self):
+        super().mycleanup()
         self.log.info("MyCleanup")
