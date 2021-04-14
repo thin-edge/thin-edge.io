b2c04facdd0716dc681dd6bfe557ec6ef643562a
Michael Abel <info@abel-ikt.de>
2021-04-14 13:48:17 +0200

    Merge over feature tests runner

---
+++ ci/ci_run_all_feature_tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# Run all available feature-tests.
+# Note: Needs a bash shell to run
+#
+# Expected environment variables to be set:
+# C8YPASS : Cumulocity password
+# C8YUSERNAME : Cumolocity username
+# C8YTENANT : Cumolocity tenant
+# C8YDEVICE : The device name
+# C8YDEVICEID : The device ID in Cumolocity
+# TIMEZONE : Your timezone (temporary)
+# TEBASEDIR : Base directory for the Thin-Edge repo
+# EXAMPLEDIR : The direcory of the sawtooth example
+
+set -e
+
+cd $TEBASEDIR
+cd common/mqtt_client
+cargo test --features integration-test
+
+cd $TEBASEDIR
+cd mapper/cumulocity/c8y_translator_lib
+cargo test --features integration-test
