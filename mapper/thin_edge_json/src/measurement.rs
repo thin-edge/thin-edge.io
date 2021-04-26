c787310e54b2f6222a3a56107b1a28396ef213e9
Wenzek <diw@softwareag.com>
2021-04-26 17:19:42 +0100

    [CIT-309] Add doc comments

--- mapper/thin_edge_json/src/measurement.rs
+++ mapper/thin_edge_json/src/measurement.rs
@@ -1,82 +1,79 @@
 use chrono::offset::FixedOffset;
 use chrono::DateTime;
-/// The FlatMeasurementVisitor trait is used to group the measurements from descrete measurements.
-/// The descrete measurement that contains key, value or group, key, value.
-/// All these are grouped as one ThinEdge Json message
-/// For example the below example shows how the MeasurementGrouper implements FlatMeasurementVisitor to group
-/// the messages.
-/// ```
-/// use thin_edge_json::measurement::{FlatMeasurementVisitor, GroupedMeasurementVisitor};
-/// use thin_edge_json::serialize::ThinEdgeJsonSerializationError;
-/// use thin_edge_json::group::MeasurementGrouperError;
-/// use chrono::offset::FixedOffset;
-/// use chrono::DateTime;
-/// use chrono::TimeZone;
-/// use std::collections::HashMap;
-/// #[derive(Debug)]
-/// pub struct MeasurementGrouper {
-///     pub timestamp: DateTime<FixedOffset>,
-///     pub values: HashMap<String, Measurement>,
-/// }
-///
-/// #[derive(Debug)]
-/// pub enum Measurement {
-///     Single(f64),
-///     Multi(HashMap<String, f64>),
-/// }
-///
-/// impl FlatMeasurementVisitor for MeasurementGrouper {
-///     type Error = MeasurementGrouperError;
-///     fn timestamp(&mut self, timestamp: &DateTime<FixedOffset>) -> Result<(), Self::Error> {Ok(())}
-///     fn measurement(&mut self,group: Option<&str>,name: &str,value: f64,) -> Result<(), Self::Error> {Ok(())}
-/// }
-///
-/// fn test_timestamp() -> DateTime<FixedOffset> {
-///    FixedOffset::east(5 * 3600)
-///    .ymd(2021, 04, 08)
-///    .and_hms(0, 0, 0)
-///  }
-/// impl MeasurementGrouper {
-///   pub fn new(timestamp: DateTime<FixedOffset>) -> Self {
-///     Self {
-///        timestamp,
-///        values: HashMap::new(),
-///     }
-///   }
-///  }
-///
-/// fn tej_build_serialize() -> Result<(), MeasurementGrouperError> {
-///     let mut grp_msg = MeasurementGrouper::new(test_timestamp());
-///     grp_msg.timestamp(&test_timestamp())?;
-//     !measurement that does not belong to any group
-///     grp_msg.measurement(None, "temperature", 25.0)?;
-//     !measurement that belongs to a group
-///     grp_msg.measurement(Some("location"), "alti", 2100.4)?;
-///     grp_msg.measurement(Some("location"), "longi", 2100.4)?;
-///     grp_msg.measurement(Some("location"), "lati", 2100.4)?;
-///     grp_msg.measurement(Some("location"), "alti", 2100.5)?;
-///     Ok(())
-/// }
-/// ```
-/// output message looks like below
-///
-/// {
-///   "time": "2021-04-08T00:00:00+05:00",
-///   "temperature": 25.0,
-///    "location":
-///        {
-///            "lati": 2100.4,
-///            "alti": 2100.5,
-///            "longi": 2100.4,
-///        },
-/// }
-///
 
+/// The `FlatMeasurementVisitor` trait represents the capability to collect/visit a series of measurements.
+///
+/// An implementation of this trait is a consumer to which a producer forwards the measurements one after the other.
+///
+/// ```
+/// use thin_edge_json::measurement::*;
+/// use thin_edge_json::group::MeasurementGrouper;
+/// # fn main() -> Result<(), anyhow::Error> {
+///
+/// // A producer first needs a consumer to forward the source measurements.
+/// let mut consumer = MeasurementGrouper::new();
+///
+/// // Then the producer can forward the different measurements,
+/// // possibly attaching each measurement to a group
+/// consumer.measurement(Some("group-name"), "measurement-name", 42.0)?;
+/// consumer.measurement(None, "measurement-unrelated-to-any-group", 42.0)?;
+///
+/// // The measurements can be sourced in an order that is independent of the groups.
+/// consumer.measurement(Some("g1"), "x", 1.0)?;
+/// consumer.measurement(Some("g2"), "a", 2.0)?;
+/// consumer.measurement(Some("g1"), "y", 3.0)?;
+/// consumer.measurement(None, "k", 1.0)?;
+/// consumer.measurement(Some("g2"), "b", 4.0)?;
+///
+/// // A timestamp can be assigned to the whole measurement series
+/// consumer.timestamp(&current_timestamp())?;
+///
+/// // Note that the timestamp or a measurement can be pushed several times.
+/// // __However__, the behavior depends on the actual consumer.
+/// // Here, the consumer is a `MeasurementGrouper` that simply always peeks the latest value.
+/// consumer.timestamp(&current_timestamp())?;         // update the timestamp
+/// consumer.measurement(Some("g1"), "x", 2.0)?;       // update g1.x
+/// consumer.measurement(None, "k", 2.0)?;             // update k
+///
+/// # Ok(()) }
+/// ```
+///
+/// Here is an implementation that simply prints the series of measurements as they are produced.
+///
+/// ```
+/// # use thin_edge_json::measurement::*;
+/// # use chrono::*;
+/// struct MeasurementPrinter {}
+///
+/// impl FlatMeasurementVisitor for MeasurementPrinter {
+///    type Error = ();
+///
+///     fn timestamp(&mut self, value: &DateTime<FixedOffset>) -> Result<(), Self::Error> {
+///         Ok(println!("time = {}", value.to_rfc2822()))
+///     }
+///
+///     fn measurement(
+///         &mut self,
+///         group: Option<&str>,
+///         name: &str, value: f64
+///     ) -> Result<(), Self::Error> {
+///         if let Some(group_name) = group {
+///             Ok(println!("{}.{} = {}", group_name, name, value))
+///         } else {
+///             Ok(println!("{} = {}", name, value))
+///
+///         }
+///     }
+/// }
+/// ```
 pub trait FlatMeasurementVisitor {
+    /// Error type specific to this way of collecting measurements
     type Error;
 
+    /// Set the timestamp shared by all the measurements of this series
     fn timestamp(&mut self, value: &DateTime<FixedOffset>) -> Result<(), Self::Error>;
 
+    /// Add a new measurement, possibly attached to a group
     fn measurement(
         &mut self,
         group: Option<&str>,
@@ -85,38 +82,87 @@ pub trait FlatMeasurementVisitor {
     ) -> Result<(), Self::Error>;
 }
 
-/// The GroupedMeasurementVisitor trait manipulate the grouped messages.
-/// For example ThinEdgeJsonSerializer implements the GroupedMeasurementVisitor
-/// trait, to serialize the grouped Thinedge Json message.
+/// The `GroupedMeasurementVisitor` trait represents the capability
+/// to collect/visit a series of measurements that have *already* been arranged in groups.
+///
+/// This trait represents the interface between a source and a consumer of measurements
+/// with the assumptions that measurements of two different groups
+/// will never be produced in an interleaved order.
+/// Such a garanty, allows the implementation of streaming consumers.
+///
+/// Here is an implementation of the `GroupedMeasurementVisitor` trait that prints the measurememts.
+///
 /// ```
-/// use thin_edge_json::measurement::{FlatMeasurementVisitor, GroupedMeasurementVisitor};
-/// use thin_edge_json::serialize::ThinEdgeJsonSerializationError;
-/// use chrono::offset::FixedOffset;
-/// use chrono::DateTime;
-/// use std::io::Write;
+/// # use thin_edge_json::measurement::*;
+/// # use chrono::*;
+/// struct GroupedMeasurementPrinter {
+///     group: Option<String>,
+/// }
 ///
-/// pub struct ThinEdgeJsonSerializer {
-///        buffer: Vec<u8>,
-///        is_within_group: bool,
-///        needs_separator: bool,
-///  }
+/// #[derive(thiserror::Error, Debug)]
+/// pub enum MeasurementError {
+///     #[error("Unexpected time stamp within a group")]
+///     UnexpectedTimestamp,
 ///
-/// impl GroupedMeasurementVisitor for ThinEdgeJsonSerializer {
-///    type Error = ThinEdgeJsonSerializationError;
-///    fn timestamp(&mut self, value: DateTime<FixedOffset>) -> Result<(), Self::Error>{Ok(())}
-///    fn measurement(&mut self, name: &str, value: f64) -> Result<(), Self::Error>{Ok(())}
-///    fn start_group(&mut self, group: &str) -> Result<(), Self::Error>{Ok(())}
-///    fn end_group(&mut self) -> Result<(), Self::Error>{Ok(())}
+///     #[error("Unexpected end of group")]
+///     UnexpectedEndOfGroup,
+///
+///     #[error("Unexpected start of group")]
+///     UnexpectedStartOfGroup,
+/// }
+///
+/// impl GroupedMeasurementVisitor for GroupedMeasurementPrinter {
+///     type Error = MeasurementError;
+///
+///     fn timestamp(&mut self, value: DateTime<FixedOffset>) -> Result<(), Self::Error> {
+///         if self.group.is_none() {
+///             Ok(println!("time = {}", value.to_rfc2822()))
+///         } else {
+///             Err(MeasurementError::UnexpectedTimestamp)
+///         }
+///     }
+///
+///     fn measurement(&mut self, name: &str, value: f64) -> Result<(), Self::Error> {
+///         if let Some(group_name) = self.group.as_ref() {
+///             Ok(println!("{}.{} = {}", group_name, name, value))
+///         } else {
+///             Ok(println!("{} = {}", name, value))
+///         }
+///     }
+///
+///     fn start_group(&mut self, group: &str) -> Result<(), Self::Error> {
+///         if self.group.is_none() {
+///             self.group = Some(group.to_owned());
+///             Ok(())
+///         } else {
+///             Err(MeasurementError::UnexpectedStartOfGroup)
+///         }
+///     }
+///
+///     fn end_group(&mut self) -> Result<(), Self::Error> {
+///         if self.group.is_none() {
+///             Err(MeasurementError::UnexpectedEndOfGroup)
+///         } else {
+///             self.group = None;
+///             Ok(())
+///         }
+///     }
 /// }
 /// ```
-/// This produces vector of u8 bytes, that looks like below
-/// {\"time\":\"2021-04-08 00:00:00 +05:00\",\"temperature\":25,\"location\":{\"lati\":2100.4,\"alti\":2100.5,\"longi\":2100.4}}"
 pub trait GroupedMeasurementVisitor {
+    /// Error type specific to this way of collecting measurements
     type Error;
 
+    /// Set the timestamp shared by all the measurements of this serie
     fn timestamp(&mut self, value: DateTime<FixedOffset>) -> Result<(), Self::Error>;
+
+    /// Start to gather measurements for a group
     fn measurement(&mut self, name: &str, value: f64) -> Result<(), Self::Error>;
+
+    /// Definitely end to gather measurements for the current group
     fn start_group(&mut self, group: &str) -> Result<(), Self::Error>;
+
+    /// Add a new measurement, attached to the current group if any
     fn end_group(&mut self) -> Result<(), Self::Error>;
 }
 
