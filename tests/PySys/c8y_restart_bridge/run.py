a0cea54e21ff68bc0259ad4115f8c6bcc5777981
Michael Abel <75477722+abelikt@users.noreply.github.com>
2021-04-27 08:43:28 +0200

    [CIT-279 ]Add system tests that talk to c8y (#167)
    * Add system tests that talk to c8y
    
    - Add enviroment management enviroment_c8y.py
    - Add test that restarts the bridge
    - Add further tests that publish c8y and validate
    
    Signed-off-by: Michael Abel <info@abel-ikt.de>
    
    * Run black to format code
    
    Signed-off-by: Michael Abel <info@abel-ikt.de>
    
    * Fix typo
    
    * use systemctl instead of service
    
    * Add titles everywhere
    
    * Add additional info line
    
    * Avoid unused timezone property
    
    * Remove obsolete checks
    
    * Add experimental, additional enviroment
    
    * Checkout newer version from main
    
    * Add second environment to split logic from test-data
    
    * Decrease severity of output
    
    * Set class names and adjust title
    
    * Set test class names
    
    * Run black
    
    * Remove now obsolete code
    
    * Delete file that was acidentally commited
    
    * Do not register cleanup functions twice ...
    
    ... even when this does not hurt
    
    * Add missing pysystest.xml

---
+++ tests/PySys/c8y_restart_bridge/run.py
@@ -0,0 +1,40 @@
+import sys
+
+sys.path.append("environments")
+from environment_c8y import EnvironmentC8y
+
+"""
+Bridge restart
+
+Given a configured system with configured certificate
+When we setup EnvironmentC8y
+When we validate EnvironmentC8y
+When we cleanup EnvironmentC8y
+Then then the test has passed
+
+So far just use the EnvironmentC8y nothing else:
+see ../environment/environment_c8y
+"""
+
+
+class RestartBridge(EnvironmentC8y):
+    def setup(self):
+        super().setup()
+        self.log.info("Setup")
+        self.addCleanupFunction(self.mycleanup)
+
+    def execute(self):
+        super().execute()
+        self.log.info("Execute (empty in this case as we test the c8y enviroment)")
+
+    def validate(self):
+        super().validate()
+        self.log.info("Validate")
+        self.assertGrep(
+            "tedge_connect.out", "connection check is successful.", contains=True
+        )
+        fail = "Warning: Bridge has been configured, but Cumulocity connection check failed."
+        self.assertGrep("tedge_connect.out", fail, contains=False)
+
+    def mycleanup(self):
+        self.log.info("MyCleanup")
