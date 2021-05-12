620c1d5cb9a46f60e6662cdbb99131041c6d31f2
Michael Abel <info@abel-ikt.de>
2021-05-12 09:19:32 +0200

    Make system-test configure its environment (mod)

--- ci/ci_run_all_tests.sh
+++ ci/ci_run_all_tests.sh
@@ -13,18 +13,35 @@
 # TEBASEDIR : Base directory for the Thin-Edge repo
 # EXAMPLEDIR : The direcory of the sawtooth example
 
+# Adding sbin seems to be necessary for non Raspberry P OS systems as Debian or Ubuntu
+PATH=$PATH:/usr/sbin
+
+echo "Disconnect old bridge"
+
+# Disconnect - may fail if not there
+sudo tedge disconnect c8y
+
+# From now on exit if a command exits with a non-zero status.
+# Commands above are allowed to fail
 set -e
 
 cd $TEBASEDIR
 
-# Check if clients are installed
+# Check if clients are installed. If not, run:
+# sudo apt-get install mosquitto-clients
 dpkg -s mosquitto-clients
+
+./configure_bridge.sh
 
 # Run all PySys tests
 
 python3 -m venv ~/env-pysys
+
 source ~/env-pysys/bin/activate
+
 pip3 install -r tests/requirements.txt
+
 cd tests/PySys/
-pysys.py run
+
 deactivate
+
