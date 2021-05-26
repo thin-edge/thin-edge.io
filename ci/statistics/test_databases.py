eae5215d4b3f481be7098613795fa8b48e542ea3
Michael Abel <info@abel-ikt.de>
2021-05-26 15:26:55 +0200

    Move database related tests to test_database

---
+++ ci/statistics/test_databases.py
@@ -0,0 +1,224 @@
+
+import numpy as np
+import os
+from os.path import expanduser
+from pathlib import Path
+import pytest
+
+import databases as db
+
+class TestMemoryHistory:
+    def test_update_table_creates_attributes(self, mocker):
+        base = db.MemoryHistory( 3, None, None)
+        mocker.patch.object(base, "upload_table")
+
+        base.update_table()
+
+        assert base.job_config != None
+        assert base.json_data != None
+
+
+    def test_update_table_calls_upload(self, mocker):
+        metadata = db.MemoryHistory( 3, None, None)
+        mocker.patch.object(metadata, "upload_table")
+
+        metadata.update_table()
+
+        metadata.upload_table.assert_called_once()
+
+class TestCpuHistory:
+    def test_update_table_creates_attributes(self, mocker):
+        lake = os.path.expanduser("~/DataLakeTest")
+        base = db.CpuHistory("name", lake, 3, None, None, None)
+        mocker.patch.object(base, "upload_table")
+
+        base.update_table()
+
+        assert base.job_config != None
+        assert base.json_data != None
+
+
+    def test_update_table_calls_upload(self, mocker):
+        lake = os.path.expanduser("~/DataLakeTest")
+        base = db.CpuHistory("name", lake, 3, None, None, None)
+        mocker.patch.object(base, "upload_table")
+
+        base.update_table()
+
+        base.upload_table.assert_called_once()
+
+    def test_postprocess(self, mocker):
+        lake = os.path.expanduser("~/DataLakeTest")
+        base = db.CpuHistory("name", lake, 3, None, None, None)
+        mock = mocker.patch.object(base, "scrap_cpu_stats")
+        folders = [
+            "results_1_unpack",
+            "results_2_unpack",
+            "results_4_unpack",
+        ]
+        base.postprocess(folders, "publish_sawmill_record_statistics", "stat_mapper_stdout", "tedge_mapper")
+
+        #mock.assert_called_once()
+        assert mock.call_count == 3
+        #mock.assert_called_with
+
+
+class TestCpuHistoryStacked:
+    def test_stacked_update_table_calls_upload(self, mocker):
+        base = db.CpuHistoryStacked( 3, None, None)
+        mocker.patch.object(base, "upload_table")
+
+        base.update_table()
+
+        base.upload_table.assert_called_once()
+
+    def test_update_table_creates_attributes(self, mocker):
+        base = db.CpuHistoryStacked( 3, None, None)
+        mocker.patch.object(base, "upload_table")
+
+        base.update_table()
+
+        assert base.job_config != None
+        assert base.json_data != None
+        assert base.database != None
+
+class TestMetadata:
+    def test_update_table_calls_upload(self, mocker):
+        base = db.MeasurementMetadata( 1, None, None, None)
+        mocker.patch.object(base, "upload_table")
+        base.array = [[1,2,3,4,5,6]]
+
+        base.update_table()
+
+        base.upload_table.assert_called_once()
+
+    def test_update_table_creates_attributes(self, mocker):
+        base = db.MeasurementMetadata( 1, None, None, None)
+        mocker.patch.object(base, "upload_table")
+        base.array = [[1,2,3,4,5,6]]
+
+        base.update_table()
+
+        assert base.job_config != None
+        assert base.json_data != None
+
+
+    def test_show_metadata(self, mocker):
+        lake = os.path.expanduser("~/DataLakeTest")
+
+        folders = [
+            "results_1_unpack",
+            "results_2_unpack",
+            "results_4_unpack",
+        ]
+        client = 'google'
+        testmode = True
+        metadata = db.MeasurementMetadata(
+            len(folders), client, testmode, lake)
+
+        metadata.postprocess(folders)
+        metadata.show()
+
+    def test_upload_table_metadata(self, mocker):
+        """"""
+        lake = None
+        client = None
+        testmode = None
+        metadata = db.MeasurementMetadata(
+            3, client, testmode, lake)
+
+        metadata.json_data = {'nope':'nope'}
+        metadata.job_config = None
+
+        # With this we inject a mock chain
+        # load_table_from_json is called, it returns a load_job
+        # load_job.running() returns False
+        load_mock = mocker.MagicMock( name='load_job')
+        load_mock.running = mocker.MagicMock( name='running', return_value = False)
+        load_mock.errors = False
+        load_table_mock = mocker.MagicMock(name='load_table_from_json', return_value = load_mock)
+        mocker.patch.object(metadata, "client")
+        metadata.client.load_table_from_json = load_table_mock
+
+        metadata.upload_table()
+
+        metadata.client.load_table_from_json.assert_called_once()
+
+
+    def test_upload_table_errors(self, mocker):
+        """"""
+        lake = None
+        client = None
+        testmode = None
+        metadata = db.MeasurementMetadata(
+            3, client, testmode, lake)
+
+        metadata.json_data = {'nope':'nope'}
+        metadata.job_config = None
+
+        # With this we inject a mock chain
+        # load_table_from_json is called, it returns a load_job
+        # load_job.running() returns False
+        load_mock = mocker.MagicMock( name='load_job')
+        load_mock.running = mocker.MagicMock( name='running', return_value = False)
+
+        load_mock.errors = True
+        load_mock.error_results = "Error results"
+
+        load_table_mock = mocker.MagicMock(name='load_table_from_json', return_value = load_mock)
+        mocker.patch.object(metadata, "client")
+        metadata.client.load_table_from_json = load_table_mock
+
+        with pytest.raises(SystemError):
+            metadata.upload_table()
+
+    def test_upload_table_delayed(self, mocker):
+        """"""
+        lake = None
+        client = None
+        testmode = None
+        metadata = db.MeasurementMetadata(
+            3, client, testmode, lake)
+
+        metadata.json_data = {'nope':'nope'}
+        metadata.job_config = None
+
+        mocker.patch('time.sleep')
+
+        # With this we inject a mock chain
+        # load_table_from_json is called, it returns a load_job
+        # load_job.running() returns False
+        load_mock = mocker.MagicMock( name='load_job')
+        load_mock.running = mocker.MagicMock(
+            name='running', side_effect=[True, True, True, False])
+
+        load_mock.errors = False
+
+        load_table_mock = mocker.MagicMock(name='load_table_from_json', return_value = load_mock)
+        mocker.patch.object(metadata, "client")
+        metadata.client.load_table_from_json = load_table_mock
+
+        metadata.upload_table()
+        assert load_mock.running.call_count == 4
+
+    def test_upload_metadata_b(self, mocker):
+        """"""
+        lake = os.path.expanduser("~/DataLakeTest")
+
+        folders = [
+            "results_1_unpack",
+            "results_2_unpack",
+            "results_4_unpack",
+        ]
+        client = 'google'
+        testmode = True
+        metadata = db.MeasurementMetadata(
+            len(folders), client, testmode, lake)
+
+        metadata.postprocess(folders)
+
+        mocker.patch.object(metadata, "upload_table")
+
+        metadata.update_table()
+
+        metadata.upload_table.assert_called_once_with()
