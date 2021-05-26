06d258100e750727a7bf9419fc7e226727b119c2
Michael Abel <75477722+abelikt@users.noreply.github.com>
2021-05-26 17:50:10 +0200

    [CIT-293] Fix for system-tests: Fix tedge_mapper permissions in system test (#246)
    * Run tedge_mapper as tedge-mapper ...
    
    ... to avoid an issue in the rights management
    
    Signed-off-by: Michael Abel <info@abel-ikt.de>
    
    * Tweak timing, strangely we get a message ever two seconds
    
    Signed-off-by: Michael Abel <info@abel-ikt.de>
    
    * Format code with black
    
    * Revert unecessary timing change
    
    * Update call to tedge mapper with right username
    
    * Again, revert the user that runs tedge_mapper
    
    * Fix runtime id

--- tests/PySys/mapper_awaits_before_reconnect/run.py
+++ tests/PySys/mapper_awaits_before_reconnect/run.py
@@ -20,26 +20,46 @@ Then we validate output contains no more than 5 error messages
 class MapperReconnectAwait(BaseTest):
     def execute(self):
         tedge_mapper = "/usr/bin/tedge_mapper"
+        self.sudo = "/usr/bin/sudo"
 
         subber = self.startProcess(
-            command="mosquito_sub",
+            command="/usr/bin/mosquitto_sub",
             arguments=["-i", "tedge-mapper-c8y", "-t", "test"],
             stdouterr="mosquitto_sub",
-            background=True
+            background=True,
         )
 
         self.wait(0.1)
 
         mapper = self.startProcess(
-            command=tedge_mapper,
-            arguments=["c8y"],
+            command=self.sudo,
+            arguments=["-u", "tedge-mapper", tedge_mapper, "c8y"],
             stdouterr="tedge_mapper",
-            background=True
+            background=True,
         )
 
         self.wait(5)
 
+        # since the first mapper is running with different user rights the
+        # test runner can't kill it for us. So we need to kill it ourselves
+        kill = self.startProcess(
+            command=self.sudo,
+            arguments=["sh", "-c", "kill -9 $(pgrep -x tedge_mapper)"],
+            stdouterr="kill",
+            ignoreExitStatus=True,
+        )
+
     def validate(self):
         self.assertGrep("tedge_mapper.out", "ERROR", contains=True)
-        self.assertLineCount("tedge_mapper.out", condition='<=5', abortOnError=True, expr='tedge_mapper::mapper: MQTT connection error:')
-        self.assertLineCount("tedge_mapper.out", condition='>=4', abortOnError=True, expr='tedge_mapper::mapper: MQTT connection error:')
+        self.assertLineCount(
+            "tedge_mapper.out",
+            condition="<=5",
+            abortOnError=True,
+            expr="tedge_mapper::mapper: MQTT connection error:",
+        )
+        self.assertLineCount(
+            "tedge_mapper.out",
+            condition=">=2",
+            abortOnError=True,
+            expr="tedge_mapper::mapper: MQTT connection error:",
+        )
--- tests/PySys/runtime_start_two_mappers/run.py
+++ tests/PySys/runtime_start_two_mappers/run.py
@@ -18,10 +18,11 @@ Then we validate output appropriate error message
 class RuntimeMultiMappers(BaseTest):
     def execute(self):
         tedge_mapper = "/usr/bin/tedge_mapper"
+        self.sudo = "/usr/bin/sudo"
 
         tedge_mapper1 = self.startProcess(
-            command=tedge_mapper,
-            arguments=["c8y"],
+            command=self.sudo,
+            arguments=["-u", "tedge-mapper", tedge_mapper, "c8y"],
             stdouterr="tedge_mapper1",
             expectedExitStatus="==0",
             background=True
@@ -30,11 +31,20 @@ class RuntimeMultiMappers(BaseTest):
         self.wait(0.1)
 
         tedge_mapper2 = self.startProcess(
-            command=tedge_mapper,
-            arguments=["c8y"],
+            command=self.sudo,
+            arguments=["-u", "tedge-mapper", tedge_mapper, "c8y"],
             stdouterr="tedge_mapper2",
             expectedExitStatus="==1",
         )
+
+        # since the first mapper is running with different user rights the
+        # test runner can't kill it for us. So we need to kill it ourselves
+        kill = self.startProcess(
+            command=self.sudo,
+            arguments=["sh", "-c", "kill -9 $(pgrep -x tedge_mapper)"],
+            stdouterr="kill",
+            ignoreExitStatus=True
+            )
 
     def validate(self):
         self.assertGrep("tedge_mapper2.err", "Error: Couldn't acquire file lock.", contains=True)
