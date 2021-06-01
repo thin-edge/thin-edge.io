ec90f03cdabafe78de240a78a3eeb384e389de37
Pradeep K J <pradeekumar.kj@softwareag.com>
2021-06-01 16:26:21 +0530

    [CIT-382] Add a test with smaller interval

--- tests/PySys/monitoring/monitoring_smaller_interval/run.py
+++ tests/PySys/monitoring/monitoring_smaller_interval/run.py
@@ -11,14 +11,14 @@ Given a configured system
 When we start the tedge-dm-agent with sudo in the background
 When we start tedge sub with sudo in the background
 When we start two publishers to publish the simulated collectd messages
+Publish the messages in 100ms interval
 Wait for couple of seconds to publish couple of batch of messages
 Then we kill tedge sub with sudo as it is running with a different user account
 Then we validate the  messages in the output of tedge sub,
 
 """
 
-
-class MonitoringWithSimulatedMessages(BaseTest):
+class MonitoringSmallInterval(BaseTest):
     def execute(self):
         tedge = "/usr/bin/tedge"
         sudo = "/usr/bin/sudo"
@@ -42,19 +42,24 @@ class MonitoringWithSimulatedMessages(BaseTest):
         # runs.
         time.sleep(0.1)
 
-        pub = self.startProcess(
-            command=sudo,
-            arguments=[tedge, "mqtt", "pub",
-                       "collectd/host/temperature/temp", "123435445:25.5"],
-            stdouterr="tedge_temp",
-        )
+        for i in range(10) :
 
-        pub = self.startProcess(
-            command=sudo,
-            arguments=[tedge, "mqtt", "pub",
-                       "collectd/host/pressure/pres", "12345678:500.5"],
-            stdouterr="tedge_pres",
-        )
+            pub = self.startProcess(
+                command=sudo,
+                arguments=[tedge, "mqtt", "pub",
+                           "collectd/host/temperature/temp", "123435445:25.5"],
+                stdouterr="tedge_temp",
+            )
+
+            pub = self.startProcess(
+                command=sudo,
+                arguments=[tedge, "mqtt", "pub",
+                           "collectd/host/pressure/pres", "12345678:500.5"],
+                stdouterr="tedge_pres",
+            )
+
+            #publish every 100ms
+            time.sleep(0.1)
 
         # wait for tedge-dm-agent to batch messages
         time.sleep(1)
@@ -89,7 +94,6 @@ class MonitoringWithSimulatedMessages(BaseTest):
             return True
         else:
             return False
-
 
     def validate_temperature(self):
         if self.js_msg["temperature"]:
