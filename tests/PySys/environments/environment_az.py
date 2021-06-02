bcf6a9c0714495ce38e95b423f68dfba294a312e
Lukasz Woznicki <lukasz.woznicki@softwareag.com>
2021-06-02 09:01:54 +0100

    Use  separate services in system tests
    Signed-off-by: Lukasz Woznicki <lukasz.woznicki@softwareag.com>

--- tests/PySys/environments/environment_az.py
+++ tests/PySys/environments/environment_az.py
@@ -2,37 +2,38 @@ import pysys
 from pysys.basetest import BaseTest
 
 """
-Environment to manage automated connect and disconnect to c8y
+Environment to manage automated connect and disconnect to az
 
-Tests that derive from class EnvironmentC8y use automated connect and
+Tests that derive from class EnvironmentAz use automated connect and
 disconnect to Cumulocity. Additional checks are made for the status of
 service mosquitto and service tedge-mapper.
 """
 
 
-class EnvironmentC8y(BaseTest):
+class EnvironmentAz(BaseTest):
     def setup(self):
-        self.log.debug("EnvironmentC8y Setup")
+        self.log.debug("EnvironmentAz Setup")
 
         self.tedge = "/usr/bin/tedge"
-        self.tedge_mapper_c8y = "tedge-mapper-c8y"
+        self.tedge_mapper_az = "tedge-mapper-az"
         self.sudo = "/usr/bin/sudo"
         self.systemctl = "/usr/bin/systemctl"
-        self.log.info("EnvironmentC8y Setup")
+        self.log.info("EnvironmentAz Setup")
         self.addCleanupFunction(self.myenvcleanup)
 
         # Check if tedge-mapper is in disabled state
-        serv_mapper = self.startProcess(
+        serv_mapper_az = self.startProcess(
             command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
+            arguments=["status", self.tedge_mapper_az],
             stdouterr="serv_mapper1",
             expectedExitStatus="==3", # 3: disabled
         )
 
+
         # Connect the bridge
         connect = self.startProcess(
             command=self.sudo,
-            arguments=[self.tedge, "connect", "c8y"],
+            arguments=[self.tedge, "connect", "az"],
             stdouterr="tedge_connect",
         )
 
@@ -46,15 +47,15 @@ class EnvironmentC8y(BaseTest):
         # Check if tedge-mapper is active again
         serv_mapper = self.startProcess(
             command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
+            arguments=["status", self.tedge_mapper_az],
             stdouterr="serv_mapper3",
         )
 
     def execute(self):
-        self.log.debug("EnvironmentC8y Execute")
+        self.log.debug("EnvironmentAz Execute")
 
     def validate(self):
-        self.log.debug("EnvironmentC8y Validate")
+        self.log.debug("EnvironmentAz Validate")
 
         # Check if mosquitto is running well
         serv_mosq = self.startProcess(
@@ -66,24 +67,24 @@ class EnvironmentC8y(BaseTest):
         # Check if tedge-mapper is active
         serv_mapper = self.startProcess(
             command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
+            arguments=["status", self.tedge_mapper_az],
             stdouterr="serv_mapper4",
         )
 
     def myenvcleanup(self):
-        self.log.debug("EnvironmentC8y Cleanup")
+        self.log.debug("EnvironmentAz Cleanup")
 
         # Disconnect Bridge
         disconnect = self.startProcess(
             command=self.sudo,
-            arguments=[self.tedge, "disconnect", "c8y"],
+            arguments=[self.tedge, "disconnect", "az"],
             stdouterr="tedge_disconnect",
         )
 
         # Check if tedge-mapper is disabled
         serv_mosq = self.startProcess(
             command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
+            arguments=["status", self.tedge_mapper_az],
             stdouterr="serv_mapper5",
             expectedExitStatus="==3",
         )
