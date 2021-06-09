d7c086c24a56f3fb1ca8500d8ff90f3bd4eeea31
Michael Abel <info@abel-ikt.de>
2021-06-09 17:17:16 +0200

    Add bash script to run test

---
+++ ci/ci_smoke_test_az.sh
@@ -0,0 +1,24 @@
+
+
+set -e
+
+# The bridge should be already configured
+# lets avoid to create a new certifiate
+# ./ci/configure_bridge.sh
+
+
+
+
+THUMB=$(sudo tedge cert show | grep Thumb | cut -c13-)
+
+echo "DEVICE Thumbprint is " $THUMB
+
+./ci/az_upload_device_cert.py -d devpi3 -t $THUMB -u ThinEdgeHub -s iothubowner
+
+sudo tedge connect az
+
+./ci/roundtrip_local_to_az.py -p sas_policy -b thinedgebus -q testqueue
+
+sudo tedge disconnect az
+
+
