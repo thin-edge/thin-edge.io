bd4b685fc8120dc17edc94665ac38e8c8bc4315b
Michael Neumann <mneumann@ntecs.de>
2021-06-09 17:16:12 +0200

    Remove use of "build_path_for_sudo_or_user" (#282)
    - This information is already available through `TEdgeConfigLocation`.
    
    - Inject config location to commands that depend on this.
    
    - While there, refactor some `String`'s to `PathBuf` and dedup some
      other code.

--- tedge/src/cli/disconnect/cli.rs
+++ tedge/src/cli/disconnect/cli.rs
@@ -14,14 +14,16 @@ pub enum TEdgeDisconnectBridgeCli {
 }
 
 impl BuildCommand for TEdgeDisconnectBridgeCli {
-    fn build_command(self, _context: BuildContext) -> Result<Box<dyn Command>, crate::ConfigError> {
+    fn build_command(self, context: BuildContext) -> Result<Box<dyn Command>, crate::ConfigError> {
         let cmd = match self {
             TEdgeDisconnectBridgeCli::C8y => DisconnectBridgeCommand {
+                config_location: context.config_location,
                 config_file: C8Y_CONFIG_FILENAME.into(),
                 cloud: Cloud::C8y,
                 use_mapper: true,
             },
             TEdgeDisconnectBridgeCli::Az => DisconnectBridgeCommand {
+                config_location: context.config_location,
                 config_file: AZURE_CONFIG_FILENAME.into(),
                 cloud: Cloud::Azure,
                 use_mapper: true,
