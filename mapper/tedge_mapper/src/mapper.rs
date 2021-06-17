64c8e82af8d851d3dae359c7db5cf3091f3a664c
ChrisGreenaway <chris.greenaway@softwareag.com>
2021-06-17 16:42:10 +0100

    CIE-380: Migrate mappers to components (#298)

--- mapper/tedge_mapper/src/mapper.rs
+++ mapper/tedge_mapper/src/mapper.rs
@@ -1,14 +1,51 @@
 use crate::converter::*;
 use crate::error::*;
-use mqtt_client::{MqttClient, Topic};
+use flockfile::{Flockfile, FlockfileError};
+use mqtt_client::{Client, MqttClient, Topic};
+use tedge_config::ConfigSettingAccessor;
+use tedge_config::{MqttPortSetting, TEdgeConfig};
 use tokio::task::JoinHandle;
-use tracing::{debug, error, instrument};
+use tracing::{debug, error, info, instrument};
 
 #[derive(Debug)]
 pub struct MapperConfig {
     pub in_topic: Topic,
     pub out_topic: Topic,
     pub errors_topic: Topic,
+}
+
+pub fn make_valid_topic_or_panic(topic_name: &str) -> Topic {
+    Topic::new(topic_name).expect("This is a valid topic name")
+}
+
+pub async fn create_mapper<'a>(
+    app_name: &'a str,
+    tedge_config: &'a TEdgeConfig,
+    mapper_config: impl Into<MapperConfig>,
+    converter: Box<dyn Converter<Error = ConversionError>>,
+) -> Result<Mapper, anyhow::Error> {
+    let _flockfile = check_another_instance_is_not_running(app_name)?;
+
+    info!("{} starting", app_name);
+
+    let mqtt_config = mqtt_config(tedge_config)?;
+    let mqtt_client = Client::connect(app_name, &mqtt_config).await?;
+
+    Ok(Mapper::new(mqtt_client, mapper_config, converter))
+}
+
+fn mqtt_config(tedge_config: &TEdgeConfig) -> Result<mqtt_client::Config, anyhow::Error> {
+    Ok(mqtt_client::Config::default().with_port(tedge_config.query(MqttPortSetting)?.into()))
+}
+
+fn check_another_instance_is_not_running(app_name: &str) -> Result<Flockfile, FlockfileError> {
+    match flockfile::Flockfile::new_lock(format!("{}.lock", app_name)) {
+        Ok(file) => Ok(file),
+        Err(err) => {
+            error!("Another instance of {} is running.", app_name);
+            Err(err)
+        }
+    }
 }
 
 pub struct Mapper {
