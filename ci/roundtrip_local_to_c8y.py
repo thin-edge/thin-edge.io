322a44399ce48badb85d2e99ebc79bd6c53b85eb
Michael Abel <info@abel-ikt.de>
2021-06-28 17:10:32 +0200

    Run black
    Signed-off-by: Michael Abel <info@abel-ikt.de>

--- ci/roundtrip_local_to_c8y.py
+++ ci/roundtrip_local_to_c8y.py
@@ -226,23 +226,21 @@ def assert_values(
 
     print("Retrieved values:")
 
-    #del values[10] # to cause an error
+    # del values[10] # to cause an error
 
     for v in range(len(values)):
 
         if v >= 1:
-            if (values[v-1] + 1) != values[v]:
+            if (values[v - 1] + 1) != values[v]:
                 print("error!")
 
         print(f"{values[v]:5} ", end="")
-        if int(values[v]+1)%20 == 0: # use a new line when data is
+        if int(values[v] + 1) % 20 == 0:  # use a new line when data is
             print("")
-
-
 
     print("")
 
-    #print("Expected:", expected)
+    # print("Expected:", expected)
     print("Expected: ", expected[0], " ... ", expected[-1])
 
     if values == expected:
