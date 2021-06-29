93d49d59c7dacf68572f755d6f64c8fbb5317a81
Pradeep K J <Pradeep K J pradeekumar.kj@softwareag.com>
2021-06-29 21:34:38 +0530

    [CIT-418] fix the tests

--- tests/PySys/device_id/valid_device_id/run.py
+++ tests/PySys/device_id/valid_device_id/run.py
@@ -6,30 +6,42 @@ import time
 Validate certificate creation with valid characters and validate with cumulocity cloud
 
 Given a configured system
+When a temporary directory is created to store the certificate and key
 When we create a certificate using the valid characters using the sudo tedge
 When we upload the certificate onto the c8y cloud
 When we connect to the c8y cloud
 Then we check for the output of the tedge connect and find successfull
+Cleanup the Certificate and Key path and delete the temporary directory
 """
 
 
-class PySysTest(BaseTest):
+class ValidateValidDeviceId(BaseTest):
     def setup(self):
         self.tedge = "/usr/bin/tedge"
         self.sudo = "/usr/bin/sudo"
 
+        # disconnect the device from cloud
         c8y_disconnect = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "disconnect", "c8y"],
             stdouterr="c8y_disconnect",
         )
-
+        
+        # create a custom certiticate directory for testing purpose
         create_cert_dir = self.startProcess(
             command=self.sudo,
-            arguments=["mkdir test-device-certs"],
+            arguments=["mkdir", "/tmp/test-device-certs"],
             stdouterr="create_cert_dir",
         )
 
+        # change the ownership of the directory
+        change_owner_of_dir = self.startProcess(
+            command=self.sudo,
+            arguments=["chown", "mosquitto:mosquitto", "/tmp/test-device-certs"],
+            stdouterr="create_cert_dir",
+        )
+        
+        # set the custom certificate path
         set_cert_path = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "config", "set", "device.cert.path",
@@ -37,6 +49,7 @@ class PySysTest(BaseTest):
             stdouterr="set_cert_path",
         )
 
+        # set the custom key path
         set_key_path = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "config", "set", "device.key.path",
@@ -47,6 +60,7 @@ class PySysTest(BaseTest):
         self.addCleanupFunction(self.device_id_cleanup)
 
     def execute(self):
+        # create a certificate with a device id that contains all valid characters
         sub1 = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "cert", "create",
@@ -54,44 +68,51 @@ class PySysTest(BaseTest):
             stdouterr="cert_create",
         )
 
+        # upload the certificate
         c8y_connect = self.startProcess(
-            cert_upload=self.sudo,
+            command=self.sudo,
             arguments=[self.tedge, "cert", "upload",
                        "c8y", "--user", "pradeep"],
             stdouterr="cert_upload",
         )
 
+        # connect to the c8y cloud
         c8y_connect = self.startProcess(
-            c8y_connect=self.sudo,
+            command=self.sudo,
             arguments=[self.tedge, "connect", "c8y"],
             stdouterr="c8y_connect",
         )
 
     def validate(self):
+        # validate the connection is successfull
         self.assertGrep("c8y_connect.out", "Successfull", contains=True)
 
     def device_id_cleanup(self):
-        self.log.info("monitoring_cleanup")
+        
+        # disconnect the test
         c8y_disconnect = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "disconnect", "c8y"],
             stdouterr="c8y_disconnect",
         )
 
+        # unset the device certificate path
         unset_cert_path = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "config", "unset", "device.cert.path"],
             stdouterr="unset_cert_path",
         )
 
+        # unset the device key path
         unset_key_path = self.startProcess(
             command=self.sudo,
             arguments=[self.tedge, "config", "unset", "device.key.path"],
             stdouterr="unset_key_path",
         )
 
+        # delete the temporary directory
         remove_cert_dir = self.startProcess(
             command=self.sudo,
-            arguments=["rm -rf test-device-certs"],
+            arguments=["rm", "-rf", "/tmp/test-device-certs"],
             stdouterr="remove_cert_dir",
         )
