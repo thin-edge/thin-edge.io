cb52573d3ebed3ca767e31a65fcaf31d8e7dfd65
Michael Neumann <mneumann@ntecs.de>
2021-07-08 14:39:21 +0200

    Add example to measure memory consumption for JSON parsing
    For a JSON document of size 107 bytes these are the results:
    
    Total number of bytes allocated:
    
    * JSON parser: 2122
    
    * Stream parser: 1382
    
    Note that includes all allocated memory for running the whole program.

---
+++ mapper/thin_edge_json/examples/validate.rs
@@ -0,0 +1,65 @@
+use std::env;
+use chrono::prelude::*;
+use thin_edge_json::measurement::MeasurementVisitor;
+
+#[global_allocator]
+static GLOBAL: &stats_alloc::StatsAlloc<std::alloc::System> = &stats_alloc::INSTRUMENTED_SYSTEM;
+
+fn main() -> anyhow::Result<()> {
+    let region = stats_alloc::Region::new(&GLOBAL);
+
+    let mut args = env::args();
+    let _ = args.next();
+    let parser = args.next().expect("parser: json | stream");
+    let input_file = args.next().expect("input: file name");
+
+    let input = std::fs::read_to_string(input_file)?;
+
+    let mut builder = DummyVisitor;
+
+    let res: anyhow::Result<()> =
+    match parser.as_str() {
+        "json" => {
+            thin_edge_json::json::parse_str(&input, &mut builder)
+                .map_err(Into::into)
+        }
+        "stream" => {
+            thin_edge_json::stream::parse_str(&input, &mut builder)
+                .map_err(Into::into)
+        }
+        _ => {
+            anyhow::bail!("Invalid parser: either `json` or `stream`.");
+        }
+    };
+
+    if res.is_ok() {
+        println!("OK");
+    }
+
+    println!("{:?}", region.change());
+
+    res
+}
+
+#[derive(thiserror::Error, Debug)]
+enum DummyError {}
+
+struct DummyVisitor;
+
+impl MeasurementVisitor for DummyVisitor {
+    type Error = DummyError;
+
+    fn visit_timestamp(&mut self, _value: DateTime<FixedOffset>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn visit_measurement(&mut self, _name: &str, _value: f64) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn visit_start_group(&mut self, _group: &str) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn visit_end_group(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
