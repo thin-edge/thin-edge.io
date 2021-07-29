deba1d515387d7e4e9851a89e3fb8eafd70ec81e
Michael Abel <info@abel-ikt.de>
2021-07-29 15:25:26 +0200

    Skip tests techdemo
    Signed-off-by: Michael Abel <info@abel-ikt.de>

--- tests/PySys/apt_plugin/apt_fail_noarg/run.py
+++ tests/PySys/apt_plugin/apt_fail_noarg/run.py
@@ -12,8 +12,18 @@ Then we expect an error code from the plugin
 
 
 class AptPluginFailNoArg(AptPlugin):
+
+    # Static member that can be overriden by a command line argument
+    # pysys.py run apt_fail_noarg -XmyPlatform='vm'
+    myPlatform=None
+
     def setup(self):
+        if self.myPlatform != 'container':
+            self.skipTest('MyFeature is not supported on this platform')
+            self.log.info(self.myPlatform)
+
         super().setup()
+
 
     def execute(self):
         self.plugin_cmd("install", "outp_install", 1)
@@ -30,3 +40,6 @@ class AptPluginFailNoArg(AptPlugin):
         self.plugin_cmd("prepare", "outp_install", 1, "nonsense", "nonsense")
         self.plugin_cmd("list", "outp_install", 1, "nonsense", "nonsense")
         self.plugin_cmd("finalize", "outp_install", 1, "nonsense", "nonsense")
+
+        # self.skipTest('MyFeature is not supported on Windows')
+        #logging.info('')
