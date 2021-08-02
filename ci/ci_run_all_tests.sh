c0d4560f6e4a22aadeb301be48dab5c351ecb6ff
Michael Abel <info@abel-ikt.de>
2021-08-02 10:19:56 +0200

    Also run the apt tests

--- ci/ci_run_all_tests.sh
+++ ci/ci_run_all_tests.sh
@@ -40,5 +40,12 @@ python3 -m venv ~/env-pysys
 source ~/env-pysys/bin/activate
 pip3 install -r tests/requirements.txt
 cd tests/PySys/
+
 pysys.py run
+
+# Run the tests for the apt plugin
+# These are skipped by default, as the modify the operating system
+# TODO Ideally, as the keyword says we should run them in a container
+pysys.py run 'apt_*' -XmyPlatform='container'
+
 deactivate
