eccc614cf1ff6f20b81c51bb295b306285e5a56c
Pradeep Kumar K J <pradeepkumar.kj@sofwareag.com>
2021-08-02 21:16:42 +0530

    [CIT-374] check packet size in application

--- mapper/tedge_mapper/src/c8y_converter.rs
+++ mapper/tedge_mapper/src/c8y_converter.rs
@@ -1,11 +1,15 @@
 use crate::converter::*;
 use crate::error::*;
+use crate::size_threshold::SizeThreshold;
 
-pub struct CumulocityConverter;
+pub struct CumulocityConverter {
+    pub(crate) size_threshold: SizeThreshold,
+}
 
 impl Converter for CumulocityConverter {
     type Error = ConversionError;
     fn convert(&self, input: &str) -> Result<String, Self::Error> {
+        let () = self.size_threshold.validate(input)?;
         c8y_translator_lib::json::from_thin_edge_json(input).map_err(Into::into)
     }
 }
---
+++ tests/PySys/tedge_mapper_c8y/tedge_mapper_c8y_packet_threshold_size/run.py
@@ -0,0 +1,79 @@
+from pysys.basetest import BaseTest
+
+import time
+
+"""
+Validate message size that is published on tedge/measurements,
+by subscribing for error message on tedge/errors by tedge-mapper c8y
+
+Given a configured system
+When we start the tedge-mapper-c8y systemctl service
+When we start tedge sub with sudo in the background for error messages
+When we publish a bigger message
+Then we kill tedge sub with sudo as it is running with a different user account
+Then we validate the error message in the output of tedge sub
+Then we stop the tedge-mapper-c8y systemctl service
+
+"""
+
+
+class TedgeMapperC8yThresholdPacketSize(BaseTest):
+    def setup(self):
+        self.tedge = "/usr/bin/tedge"
+        self.sudo = "/usr/bin/sudo"
+        self.mosquitto_pub = "/usr/bin/mosquitto_pub"
+
+        mapper = self.startProcess(
+            command=self.sudo,
+            arguments=["systemctl", "start", "tedge-mapper-c8y"],
+            stdouterr="tedge_mapper_c8y",
+        )
+
+        self.addCleanupFunction(self.mapper_cleanup)
+
+    def execute(self):
+        sub = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "mqtt", "sub", "--no-topic", "tedge/errors"],
+            stdouterr="tedge_sub",
+            background=True,
+        )
+
+        # Wait for a small amount of time to give tedge sub time
+        # to initialize. This is a heuristic measure.
+        # Without an additional wait we observe failures in 1% of the test
+        # runs.
+        time.sleep(0.1)
+        
+        # Create a big file using the `dd` command
+        msg = self.startProcess(
+            command=self.sudo,
+            arguments=["dd", "if=/dev/zero", "of=/tmp/big_message.txt", "bs=10", "count=1", "seek=2048"],
+            stdouterr="tedge_msg",
+        )
+
+        pub = self.startProcess(
+            command=self.sudo,
+            arguments=[self.mosquitto_pub, "-t", "tedge/measurements",
+                      "-f", "/tmp/big_message.txt"],
+            stdouterr="tedge_pub",
+        )
+
+        # Kill the subscriber process explicitly with sudo as PySys does
+        # not have the rights to do it
+        kill = self.startProcess(
+            command=self.sudo,
+            arguments=["killall", "tedge"],
+            stdouterr="kill_out",
+        )
+
+    def validate(self):
+        self.assertGrep('tedge_sub.out', "The input size 20489 is too big. The threshold is 16384.", contains=True)
+
+    def mapper_cleanup(self):
+        self.log.info("mapper_cleanup")
+        mapper = self.startProcess(
+            command=self.sudo,
+            arguments=["systemctl", "stop", "tedge-mapper-c8y"],
+            stdouterr="tedge_mapper_c8y",
+        )
