c0ccf12f1f8d421f524094cb8be4fd031f35d8b2
Pradeep Kumar K J <pradeepkumar.kj@sofwareag.com>
2021-08-03 16:42:32 +0530

    [CIT-374] add test for c8y_converter

--- mapper/tedge_mapper/src/c8y_converter.rs
+++ mapper/tedge_mapper/src/c8y_converter.rs
@@ -13,3 +13,27 @@ impl Converter for CumulocityConverter {
         c8y_translator_lib::json::from_thin_edge_json(input).map_err(Into::into)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn check_c8y_threshold_packet_size() -> Result<(), anyhow::Error> {
+        let size_threshold = SizeThreshold(16 * 1024);
+        let converter = CumulocityConverter { size_threshold };
+        let buffer = create_packet(1024 * 20);
+        let err = converter.size_threshold.validate(&buffer).unwrap_err();
+        assert_eq!(err.to_string(), "The input size 20480 is too big. The threshold is 16384.");
+        Ok(())
+    }
+
+    fn create_packet(size: usize) -> String {
+        let data: String = "Some data!".into();
+        let loops = size / data.len();
+        let mut buffer = String::with_capacity(size);
+        for _ in 0..loops {
+            buffer.push_str("Some data!");
+        }
+        buffer
+    }
+}
