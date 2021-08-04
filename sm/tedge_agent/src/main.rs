e795cd9d4d9e7ba1d114f1bf7448b704c41bbace
Lukasz Woznicki <lukasz.woznicki@softwareag.com>
2021-08-04 16:47:27 +0100

    Remove user guard from main to allow run as system service and different user
    Signed-off-by: Lukasz Woznicki <lukasz.woznicki@softwareag.com>

--- sm/tedge_agent/src/main.rs
+++ sm/tedge_agent/src/main.rs
@@ -1,5 +1,4 @@
 use tedge_users::UserManager;
-use tedge_utils::paths::{home_dir, PathsError};
 
 mod agent;
 mod component;
@@ -9,15 +8,8 @@ mod state;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let user_manager = UserManager::new();
-    let _user_guard = user_manager.become_user(tedge_users::TEDGE_USER)?;
 
-    let tedge_config_location = if tedge_users::UserManager::running_as_root() {
-        tedge_config::TEdgeConfigLocation::from_default_system_location()
-    } else {
-        tedge_config::TEdgeConfigLocation::from_users_home_location(
-            home_dir().ok_or(PathsError::HomeDirNotFound)?,
-        )
-    };
+    let tedge_config_location = tedge_config::TEdgeConfigLocation::from_default_system_location();
 
     initialise_logging();
 
