b49a99b2cc945d6ad4f2305afef291a2ce6a2c3f
Michael Abel <info@abel-ikt.de>
2021-08-09 14:25:54 +0200

    Extend timespan once more

--- tests/PySys/apt_plugin/apt_prepare/run.py
+++ tests/PySys/apt_plugin/apt_prepare/run.py
@@ -32,7 +32,8 @@ class AptPluginPrepare(AptPlugin):
         self.assertThat("old != new", old=self.mtime_old, new=self.mtime_new)
         # make sure the cache was updated in the last N seconds
         # N=100 : Took 91s to update at mythic beasts
-        self.assertThat("(new +100) >= now", new=self.mtime_new, now=self.now)
+        # N=150 : Took 123s to update at mythic beasts (Why???)
+        self.assertThat("(new +150) >= now", new=self.mtime_new, now=self.now)
 
     def cleanup_prepare(self):
         pass
