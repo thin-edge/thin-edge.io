b8069bdff18b42f7dd25843ed8d61a59ef2a2e19
Michael Abel <info@abel-ikt.de>
2021-08-16 15:57:42 +0200

    Check for a failed installation

---
+++ tests/PySys/software-management-end-to-end/sm-apt-install-fail/run.py
@@ -0,0 +1,33 @@
+from pysys.basetest import BaseTest
+
+import time
+
+"""
+Validate ...
+
+"""
+
+import json
+import requests
+import time
+import sys
+
+sys.path.append("software-management-end-to-end")
+from environment_sm_management import SmManagement
+
+
+class PySysTest(SmManagement):
+    def setup(self):
+        super().setup()
+        self.assertThat("False == value", value=self.check_isinstalled("rolldice"))
+
+    def execute(self):
+
+        self.trigger_action("does_not_exist", "5446165", "::apt", "notanurl", "install")
+
+        self.wait_until_fail()
+
+
+    def validate(self):
+
+        self.assertThat("False == value", value=self.check_isinstalled("does_not_exist"))
