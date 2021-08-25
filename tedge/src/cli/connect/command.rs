f06f09af1f5d48bf9a031d74a364362ddbeb8a0d
Pradeep Kumar K J <pradeepkumar.kj@sofwareag.com>
2021-08-25 21:33:52 +0530

    address review comment

--- tedge/src/cli/connect/command.rs
+++ tedge/src/cli/connect/command.rs
@@ -94,6 +94,7 @@ impl Command for ConnectCommand {
                 self.cloud.as_str()
             );
         }
+
         Ok(())
     }
 }
@@ -338,14 +339,16 @@ fn new_bridge(
         }
     }
 
-    println!("Checking if tedge-agent and sm-c8y mapper are installed.\n");
-
-    if bridge_config.use_agent && which("tedge_agent").is_ok() {
-        service_manager.start_and_enable_service(SystemService::TEdgeSMAgent, std::io::stdout());
-        service_manager
-            .start_and_enable_service(SystemService::TEdgeSMMapperC8Y, std::io::stdout());
-    } else {
-        println!("Info: Software management is not installed. So, skipping enabling related components.\n");
+    if bridge_config.use_agent {
+        println!("Checking if tedge-agent and sm-c8y mapper are installed.\n");
+        if which("tedge_agent").is_ok() {
+            service_manager
+                .start_and_enable_service(SystemService::TEdgeSMAgent, std::io::stdout());
+            service_manager
+                .start_and_enable_service(SystemService::TEdgeSMMapperC8Y, std::io::stdout());
+        } else {
+            println!("Info: Software management is not installed. So, skipping enabling related components.\n");
+        }
     }
 
     Ok(())
