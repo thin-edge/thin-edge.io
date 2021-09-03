7320d70fc28a1d30c6cd1455e8d93ffdabf00739
Albin Suresh <albin.suresh@softwareag.com>
2021-09-03 15:45:48 +0530

    Addressing review comments

--- tedge/src/cli/connect/command.rs
+++ tedge/src/cli/connect/command.rs
@@ -27,9 +27,8 @@ pub struct ConnectCommand {
 }
 
 pub enum DeviceStatus {
-    New,
     AlreadyExists,
-    Unknown,
+    MightBeNew,
 }
 
 pub enum Cloud {
@@ -94,23 +93,29 @@ impl Command for ConnectCommand {
             &self.config_location,
         )?;
 
-        let connection_check_result = self.check_connection(&config);
-        if connection_check_result.is_err() {
-            println!(
-                "Warning: Bridge has been configured, but {} connection check failed.\n",
-                self.cloud.as_str()
-            );
+        match self.check_connection(&config) {
+            Ok(DeviceStatus::AlreadyExists) => {}
+            Ok(DeviceStatus::MightBeNew) => {
+                if let Cloud::C8y = self.cloud {
+                    println!("Restarting mosquitto to resubscribe to bridged inbound cloud topics after device creation");
+                    restart_mosquitto(
+                        &bridge_config,
+                        self.service_manager.as_ref(),
+                        &self.config_location,
+                    )?;
+
+                    enable_software_management(&bridge_config, self.service_manager.as_ref());
+                }
+            }
+            Err(_) => {
+                println!(
+                    "Warning: Bridge has been configured, but {} connection check failed.\n",
+                    self.cloud.as_str()
+                );
+            }
         }
 
         if let Cloud::C8y = self.cloud {
-            if let Ok(DeviceStatus::New) = connection_check_result {
-                println!("Restarting mosquitto to resubscribe to bridged inbound cloud topics after device creation");
-                restart_mosquitto(
-                    &bridge_config,
-                    self.service_manager.as_ref(),
-                    &self.config_location,
-                )?;
-            }
             enable_software_management(&bridge_config, self.service_manager.as_ref());
         }
 
@@ -157,8 +162,8 @@ impl ConnectCommand {
             WAIT_FOR_CHECK_SECONDS
         );
         match self.cloud {
-            Cloud::Azure => check_connection_azure(port),
-            Cloud::C8y => check_connection_c8y(port),
+            Cloud::Azure => check_device_status_azure(port),
+            Cloud::C8y => check_device_status_c8y(port),
         }
     }
 }
@@ -184,29 +189,32 @@ where
 // the check can finish in the second try as there is no error response in the first try.
 
 #[tokio::main]
-async fn check_connection_c8y(port: u16) -> Result<DeviceStatus, ConnectError> {
+async fn check_device_status_c8y(port: u16) -> Result<DeviceStatus, ConnectError> {
     for i in 0..2 {
         print!("Try {} / 2: Sending a message to Cumulocity. ", i + 1,);
 
         let create_device_result = create_device(port);
         match create_device_result.await {
-            Ok(DeviceStatus::New) => {
-                return Ok(DeviceStatus::New);
-            }
             Ok(DeviceStatus::AlreadyExists) => {
                 println!("Received expected response message, connection check is successful.\n",);
                 if i == 0 {
+                    // If the DeviceAlreadyExists response comes on the first attempt itself, the device definitely was created earlier
                     return Ok(DeviceStatus::AlreadyExists);
                 } else {
-                    return Ok(DeviceStatus::New);
+                    // If the DeviceAlreadyExists response comes on the second attempt only,
+                    // it may have been created on the first attempt or earlier.
+                    // The absence of the response on the first attempt can't be considered as definite proof of new device creation
+                    // due to the possibility of a DeviceAlreadyExists response from that first attempt getting lost in transit.
+                    // So, we could only say that the device might be new, but not entirely sure.
+                    return Ok(DeviceStatus::MightBeNew);
                 }
             }
-            Ok(DeviceStatus::Unknown) => {
+            Ok(DeviceStatus::MightBeNew) => {
                 if i == 0 {
                     println!("... No response. If the device is new, it's normal to get no response in the first try.");
                 } else {
                     println!("... No response. ");
-                    return Ok(DeviceStatus::Unknown);
+                    return Ok(DeviceStatus::MightBeNew);
                 }
             }
             Err(err) => {
@@ -214,7 +222,7 @@ async fn check_connection_c8y(port: u16) -> Result<DeviceStatus, ConnectError> {
             }
         }
     }
-    return Ok(DeviceStatus::Unknown);
+    return Ok(DeviceStatus::MightBeNew);
 }
 
 async fn create_device(port: u16) -> Result<DeviceStatus, ConnectError> {
@@ -261,7 +269,7 @@ async fn create_device(port: u16) -> Result<DeviceStatus, ConnectError> {
             println!("Received expected response message, connection check is successful.\n",);
             return Ok(DeviceStatus::AlreadyExists);
         }
-        _err => return Ok(DeviceStatus::Unknown),
+        _err => return Ok(DeviceStatus::MightBeNew),
     }
 }
 
@@ -273,7 +281,7 @@ async fn create_device(port: u16) -> Result<DeviceStatus, ConnectError> {
 // Here if the status is 200 then it's success.
 
 #[tokio::main]
-async fn check_connection_azure(port: u16) -> Result<DeviceStatus, ConnectError> {
+async fn check_device_status_azure(port: u16) -> Result<DeviceStatus, ConnectError> {
     const AZURE_TOPIC_DEVICE_TWIN_DOWNSTREAM: &str = r##"az/twin/res/#"##;
     const AZURE_TOPIC_DEVICE_TWIN_UPSTREAM: &str = r#"az/twin/GET/?$rid=1"#;
     const CLIENT_ID: &str = "check_connection_az";
@@ -312,7 +320,7 @@ async fn check_connection_azure(port: u16) -> Result<DeviceStatus, ConnectError>
     match fut.await {
         Ok(Ok(true)) => {
             println!("Received expected response message, connection check is successful.");
-            Ok(DeviceStatus::New)
+            Ok(DeviceStatus::AlreadyExists)
         }
         _err => Err(ConnectError::NoPacketsReceived {
             cloud: "Azure".into(),
