b22e14b76c9e376aef60510430b11abe66418602
Wenzek <diw@softwareag.com>
2021-09-17 17:01:01 +0100

    [CIT-564] Test error cases

--- sm/plugin_sm/src/logged_command.rs
+++ sm/plugin_sm/src/logged_command.rs
@@ -1,6 +1,6 @@
-use std::process::{Stdio, Output};
+use std::process::{Output, Stdio};
 use tokio::fs::File;
-use tokio::io::{BufWriter, AsyncWriteExt};
+use tokio::io::{AsyncWriteExt, BufWriter};
 use tokio::process::Command;
 
 pub struct LoggedCommand {
@@ -15,7 +15,6 @@ impl std::fmt::Display for LoggedCommand {
 }
 
 impl LoggedCommand {
-
     pub fn new(program: &str) -> LoggedCommand {
         let command_line = program.to_string();
         let mut command = Command::new(program);
@@ -25,7 +24,10 @@ impl LoggedCommand {
             .stdout(Stdio::piped())
             .stderr(Stdio::piped());
 
-        LoggedCommand { command_line, command }
+        LoggedCommand {
+            command_line,
+            command,
+        }
     }
 
     pub fn arg(&mut self, arg: &str) -> &mut LoggedCommand {
@@ -42,9 +44,7 @@ impl LoggedCommand {
     /// If the function fails to log the execution of the command,
     /// this is logged with `log::error!` without changing the return value.
     pub async fn execute(mut self, logger: &mut BufWriter<File>) -> Result<Output, std::io::Error> {
-        let outcome = self.command
-            .output()
-            .await;
+        let outcome = self.command.output().await;
 
         if let Err(err) = LoggedCommand::log_outcome(&self.command_line, &outcome, logger).await {
             log::error!("Fail to log the command execution: {}", err);
@@ -53,7 +53,7 @@ impl LoggedCommand {
         outcome
     }
 
-    async fn log_outcome (
+    async fn log_outcome(
         command_line: &str,
         result: &Result<Output, std::io::Error>,
         logger: &mut BufWriter<File>,
@@ -93,7 +93,8 @@ mod tests {
     use tokio::fs::File;
 
     #[tokio::test]
-    async fn on_execute_are_logged_command_line_exit_status_stdout_and_stderr() -> Result<(), anyhow::Error> {
+    async fn on_execute_are_logged_command_line_exit_status_stdout_and_stderr(
+    ) -> Result<(), anyhow::Error> {
         // Prepare a log file
         let tmp_dir = TempDir::new()?;
         let log_file_path = tmp_dir.path().join("operation.log");
@@ -102,9 +103,7 @@ mod tests {
 
         // Prepare a command
         let mut command = LoggedCommand::new("echo");
-        command
-            .arg("Hello")
-            .arg("World!");
+        command.arg("Hello").arg("World!");
 
         // Execute the command with logging
         let _ = command.execute(&mut logger).await;
@@ -121,6 +120,64 @@ EOF
 
 stderr <<EOF
 EOF
+"#
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn on_execute_with_error_stderr_is_logged() -> Result<(), anyhow::Error> {
+        // Prepare a log file
+        let tmp_dir = TempDir::new()?;
+        let log_file_path = tmp_dir.path().join("operation.log");
+        let log_file = File::create(log_file_path.clone()).await?;
+        let mut logger = BufWriter::new(log_file);
+
+        // Prepare a command that triggers some content on stderr
+        let mut command = LoggedCommand::new("ls");
+        command.arg("dummy-file");
+
+        // Execute the command with logging
+        let _ = command.execute(&mut logger).await;
+
+        // On expect the errors to be logged
+        let log_content = String::from_utf8(std::fs::read(&log_file_path)?)?;
+        assert_eq!(
+            log_content,
+            r#"----- $ ls "dummy-file"
+exit code: 2
+
+stdout <<EOF
+EOF
+
+stderr <<EOF
+ls: cannot access 'dummy-file': No such file or directory
+EOF
+"#
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn on_execution_error_are_logged_command_line_and_error() -> Result<(), anyhow::Error> {
+        // Prepare a log file
+        let tmp_dir = TempDir::new()?;
+        let log_file_path = tmp_dir.path().join("operation.log");
+        let log_file = File::create(log_file_path.clone()).await?;
+        let mut logger = BufWriter::new(log_file);
+
+        // Prepare a command that cannot be executed
+        let command = LoggedCommand::new("dummy-command");
+
+        // Execute the command with logging
+        let _ = command.execute(&mut logger).await;
+
+        // The fact that the command cannot be executed must be logged
+        let log_content = String::from_utf8(std::fs::read(&log_file_path)?)?;
+        assert_eq!(
+            log_content,
+            r#"----- $ dummy-command
+error: No such file or directory (os error 2)
 "#
         );
         Ok(())
