90cc2e1242f5443ade2cd155192cce464bcd5058
Albin Suresh <albin.suresh@softwareag.com>
2021-09-24 16:46:28 +0530

    Input argument validations

--- sm/plugins/tedge_docker_plugin/tedge_docker_plugin.sh
+++ sm/plugins/tedge_docker_plugin/tedge_docker_plugin.sh
@@ -1,48 +1,88 @@
 #!/bin/sh
 
+unsupported_args_check() {
+    if ! [ -z $1 ]; then
+        echo "Unsupported arguments: $@"
+        exit 1
+    fi
+}
+
+extract_image_tag_from_args() {
+    IMAGE_NAME="$1"
+    if [ -z "$IMAGE_NAME" ]; then
+        echo "Image name is a mandatory argument"
+        exit 1
+    fi
+    shift   # Pop image name from args list
+    IMAGE_TAG=$IMAGE_NAME
+    
+    if ! [ -z $1 ]; then
+        case "$1" in 
+            --module-version)
+                IMAGE_VERSION="$2"
+                IMAGE_TAG=$IMAGE_NAME:$IMAGE_VERSION
+                shift 2  # Pop --version and the version value from the args list
+                ;;
+            *)
+                echo "Unsupported argument option: $1"
+                exit 1
+                ;;
+        esac
+    fi
+    
+    unsupported_args_check $@
+
+}
+
 set -e
 
 COMMAND="$1"
-IMAGE_NAME="$2"
-IMAGE_TAG="$3"
+shift   # Pop the command from args list
 
 case "$COMMAND" in
     prepare)
+        unsupported_args_check $@
         # nothing to do here
         ;;
     list)
-        sudo docker image list --format '{"name":"{{.Repository}}","version":"{{.Tag}}"}'
+        unsupported_args_check $@
+        docker image list --format '{"name":"{{.Repository}}","version":"{{.Tag}}"}'
         ;;
     install)
+        # Extract the docker image tag into the IMAGE_TAG variable
+        extract_image_tag_from_args $@
+
         # Stop all containers using the provided image name
-        containers=sudo docker ps --format "{{.ID}} {{.Image}}" | grep $IMAGE_NAME | awk '{print $1}'
+        containers=$(docker ps --format "{{.ID}} {{.Image}}" | grep $IMAGE_TAG | awk '{print $1}')
         for container in $containers
         do
-            sudo docker stop $container
+            docker stop $container
         done
         # Spawn new containers with the provided image name and version to replace the stopped ones
-        sudo docker run $IMAGE_NAME:$IMAGE_TAG
+        docker run -d $IMAGE_TAG
         ;;
     remove)
-        containers=sudo docker ps --format "{{.ID}} {{.Image}}" | grep $IMAGE_NAME:$IMAGE_TAG | awk '{print $1}'
+        extract_image_tag_from_args $@
+
+        containers=$(docker ps --format "{{.ID}} {{.Image}}" | grep $IMAGE_TAG | awk '{print $1}')
         if [ -z $containers ]
         then
-            echo "No containers found for the image: $IMAGE_NAME:$IMAGE_TAG"
+            echo "No containers found for the image: $IMAGE_TAG"
         fi
         for container in $containers
         do
-            sudo docker stop $container
+            docker stop $container
         done
         ;;
     finalize)
+        unsupported_args_check $@
         # Prune all the unused containers. The --force command is used to avoid a [y/N] user prompt
-        sudo docker container prune --force
+        docker container prune --force
         # Prune all the unused images
-        sudo docker image prune --all --force
+        docker image prune --all --force
         ;;
     *)
-        echo "Unsupported argument: $COMMAND"
+        echo "Unsupported command: $COMMAND"
         exit 1
         ;;
 esac
-exit 0
