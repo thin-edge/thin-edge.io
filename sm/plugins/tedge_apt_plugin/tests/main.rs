71e3d791ffdc255d3d1f2001c7efd7272c95d021
Alex Solomes <alex.solomes@softwareag.com>
2021-10-07 13:54:08 +0100

    [CIT-611] refactor and spelling
    Signed-off-by: Alex Solomes <alex.solomes@softwareag.com>

--- sm/plugins/tedge_apt_plugin/tests/main.rs
+++ sm/plugins/tedge_apt_plugin/tests/main.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-mod apt_plugin_integration_tests {
+mod apt_plugin_integration_test {
     use curl::easy::Easy;
     use hamcrest2::prelude::*;
     use serial_test::serial;
@@ -14,34 +14,28 @@ mod apt_plugin_integration_tests {
 
     type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-    const ROLLDICE: &str = "rolldice";
+    const PACKAGE_NAME: &str = "rolldice";
+    const PACKAGE_VERSION: &str = "1.16-1+b3";
     const TEDGE_APT_COMMAND: &str = "/etc/tedge/sm-plugins/apt";
     const APT_COMMAND: &str = "/usr/bin/apt-get";
-    const ROLLDICE_URL: &str =
+    const PACKAGE_URL: &str =
         "http://ftp.br.debian.org/debian/pool/main/r/rolldice/rolldice_1.16-1+b3_amd64.deb";
-    const ROLLDICE_VERSION: &str = "1.16-1+b3";
-    const ROLLDICE_FILE_PATH: &str = "/tmp/rolldice_1.16-1+b3_amd64.deb";
-    static DOWNLOAD_ROLLDICE_BINARY: Once = Once::new();
+    const PACKAGE_FILE_PATH: &str = "/tmp/rolldice_1.16-1+b3_amd64.deb";
+    static DOWNLOAD_PACKAGE_BINARY: Once = Once::new();
 
-    pub fn download_rolldice_binary_once() {
-        DOWNLOAD_ROLLDICE_BINARY.call_once(|| {
-            let _ = fetch_url(ROLLDICE_URL).unwrap();
+    pub fn download_package_binary_once() {
+        DOWNLOAD_PACKAGE_BINARY.call_once_force(|_state| {
+            let _ = fetch_url(PACKAGE_URL).unwrap();
         });
     }
     /// converts a vector of u8 integers into utf8 String.
-    /// if the vec of u8 == "" (as a String) => None.
-    /// else Some
-    fn u8_to_string(vec: Vec<u8>) -> Option<String> {
-        let output = String::from_utf8(vec).unwrap();
-        if output == "" {
-            return None;
-        }
-        return Some(output);
+    fn u8_to_string(vec: Vec<u8>) -> String {
+        String::from_utf8(vec).unwrap()
     }
 
     /// executes a `cmd` with `args`
     /// returns the stdout, stderr and exit code
-    fn run_cmd(cmd: &str, args: &str) -> Result<(Option<String>, Option<String>, i32)> {
+    fn run_cmd(cmd: &str, args: &str) -> Result<(String, String, i32)> {
         let args: Vec<&str> = args.split_whitespace().collect();
         let output = Command::new(cmd)
             .args(args)
@@ -85,28 +79,28 @@ mod apt_plugin_integration_tests {
     }
 
     #[test_case(
-        &format!("install {} --file {}", ROLLDICE, "wrong_path"),                                               // input
-        &format!("ERROR: Parsing Debian package failed"),                                                       // expected stderr
-        5                                                                                                       // expected exit code
-        ; "wrong path"                                                                                          // description
+        &format!("install {} --file {}", PACKAGE_NAME, "wrong_path"),                                               // input
+        "ERROR: Parsing Debian package failed",                                                                     // expected stderr
+        5                                                                                                           // expected exit code
+        ; "wrong path"                                                                                              // description
     )]
     #[test_case(
-        &format!("install {} --file {} --module-version {}", ROLLDICE, "notapackagepath", ROLLDICE_VERSION),    // input
-        &format!("ERROR: Parsing Debian package failed"),                                                       // expected stderr
-        5                                                                                                       // expected exit code
-        ; "wrong path with right version"                                                                       // description
+        &format!("install {} --file {} --module-version {}", PACKAGE_NAME, "not/a/package/path", PACKAGE_VERSION),  // input
+        "ERROR: Parsing Debian package failed",                                                                     // expected stderr
+        5                                                                                                           // expected exit code
+        ; "wrong path with right version"                                                                           // description
     )]
     #[test_case(
-        &format!("install {} --file {} --module-version {}", ROLLDICE, ROLLDICE_FILE_PATH, "someversion"),      // input
-        &format!("ERROR: Parsing Debian package failed"),                                                       // expected stderr
-        5                                                                                                       // expcted exit code
-        ; "right path with wrong version"                                                                       // description
+        &format!("install {} --file {} --module-version {}", PACKAGE_NAME, PACKAGE_FILE_PATH, "some_version"),      // input
+        "ERROR: Parsing Debian package failed",                                                                     // expected stderr
+        5                                                                                                           // expected exit code
+        ; "right path with wrong version"                                                                           // description
     )]
     #[test_case(
-        &format!("install {} --file {} --module-version {}", ROLLDICE, "notapackagepath", "someversion"),       // input
-        &format!("ERROR: Parsing Debian package failed"),                                                       // expected stderr
-        5                                                                                                       // expcted exit code
-        ; "wrong path with wrong version"                                                                       // description
+        &format!("install {} --file {} --module-version {}", PACKAGE_NAME, "not/a/package/path", "some_version"),   // input
+        "ERROR: Parsing Debian package failed",                                                                     // expected stderr
+        5                                                                                                           // expected exit code
+        ; "wrong path with wrong version"                                                                           // description
     )]
     fn install_from_local_file_fail(
         input_command: &str,
@@ -117,49 +111,49 @@ mod apt_plugin_integration_tests {
         let (stdout, stderr, exit_code) = run_cmd(TEDGE_APT_COMMAND, input_command)?;
 
         // asserting command failed
-        assert_that!(stdout, is(none()));
-        assert_that!(stderr.unwrap(), match_regex(expected_stderr));
+        assert_that!(stdout.is_empty(), true);
+        assert_that!(stderr, match_regex(expected_stderr));
         assert_that!(exit_code, eq(expected_exit_code));
         Ok(())
     }
 
     #[test_case(
-        &format!("install {} --file {}", ROLLDICE, ROLLDICE_FILE_PATH),                                         // input
-        &format!("The following NEW packages will be installed\n  {}", ROLLDICE),                               // expected stdout
-        0                                                                                                       // expected exit code
-        ; "path"                                                                                                // description
+        &format!("install {} --file {}", PACKAGE_NAME, PACKAGE_FILE_PATH),                                          // input
+        &format!("The following NEW packages will be installed\n  {}", PACKAGE_NAME),                               // expected stdout
+        0                                                                                                           // expected exit code
+        ; "path"                                                                                                    // description
     )]
     #[serial]
     #[test_case(
-        &format!("install {} --file {} --module-version {}", ROLLDICE, ROLLDICE_FILE_PATH, ROLLDICE_VERSION),   // input
-        &format!("The following NEW packages will be installed\n  {}", ROLLDICE),                               // expected stdout 
-        0                                                                                                       // expected exit code
-        ; "path with version"                                                                                   // description
+        &format!("install {} --file {} --module-version {}", PACKAGE_NAME, PACKAGE_FILE_PATH, PACKAGE_VERSION),     // input
+        &format!("The following NEW packages will be installed\n  {}", PACKAGE_NAME),                               // expected stdout 
+        0                                                                                                           // expected exit code
+        ; "path with version"                                                                                       // description
     )]
     #[serial]
     #[test_case(
-        &format!("install {} --module-version {} --file {}", ROLLDICE,  ROLLDICE_VERSION, ROLLDICE_FILE_PATH),  // input
-        &format!("The following NEW packages will be installed\n  {}", ROLLDICE),                               // expected stdout 
-        0                                                                                                       // expected exit code
-        ; "version with path"                                                                                   // description
+        &format!("install {} --module-version {} --file {}", PACKAGE_NAME,  PACKAGE_VERSION, PACKAGE_FILE_PATH),    // input
+        &format!("The following NEW packages will be installed\n  {}", PACKAGE_NAME),                               // expected stdout 
+        0                                                                                                           // expected exit code
+        ; "version with path"                                                                                       // description
     )]
     #[serial]
-    fn install_from_local_file_sucess(
+    fn install_from_local_file_success(
         input_command: &str,
         expected_stdout: &str,
         expected_exit_code: i32,
     ) -> Result<()> {
         // fetching the debian package & removing rolldice in case it is already installed.
         // only executed once.
-        download_rolldice_binary_once();
-        let _ = run_cmd(APT_COMMAND, &format!("remove {} -y", ROLLDICE))?;
+        download_package_binary_once();
+        let _ = run_cmd(APT_COMMAND, &format!("remove {} -y", PACKAGE_NAME))?;
 
         // execute command to install from local file
         let (stdout, stderr, exit_code) = run_cmd(TEDGE_APT_COMMAND, input_command)?;
 
-        // asserting sucesss
-        assert_that!(stdout.unwrap(), match_regex(expected_stdout));
-        assert_that!(stderr, is(none()));
+        // asserting success
+        assert_that!(stdout, match_regex(expected_stdout));
+        assert_that!(stderr.is_empty(), true);
         assert_that!(exit_code, eq(expected_exit_code));
 
         Ok(())
