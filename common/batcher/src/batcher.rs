ea2cbe8b20ddb1b4d4af45ca17e939e134a00582
Wenzek <diw@softwareag.com>
2021-10-08 08:56:36 +0100

    [CIT-589] Improve naming in tests

--- common/batcher/src/batcher.rs
+++ common/batcher/src/batcher.rs
@@ -201,8 +201,8 @@ mod tests {
     fn multi_event_batch_with_long_delivery_jitter_and_delayed_message() {
         let mut test = BatcherTest::new(50, 50, 0);
 
-        let event1 = test.create_event(5, "b", 2);
-        let event2 = test.create_event(10, "a", 1);
+        let event1 = test.create_event(5, "a", 2);
+        let event2 = test.create_event(10, "b", 1);
 
         test.event(11, &event2);
         test.event(25, &event1); // late, but not too late
