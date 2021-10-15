c50aa1b5a21eb5a63311f29c9bd69ffb39cabfca
Wenzek <diw@softwareag.com>
2021-10-15 11:40:51 +0100

    Fix issue #495
    On `tedge connect` the configuration files are created in temporary
    files that are renamed once finalized. This rename cannot be done across
    file-systems, causing the issue #495. The fix is to create the temporary
    file along the target file in the same directory.
    
    Signed-off-by: Wenzek <diw@softwareag.com>

--- common/tedge_utils/src/paths.rs
+++ common/tedge_utils/src/paths.rs
@@ -4,6 +4,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use std::io::Write;
 use tempfile::{NamedTempFile, PersistError};
 
 #[derive(thiserror::Error, Debug)]
@@ -62,6 +63,54 @@ pub fn ok_if_not_found(err: std::io::Error) -> std::io::Result<()> {
     match err.kind() {
         std::io::ErrorKind::NotFound => Ok(()),
         _ => Err(err),
+    }
+}
+
+/// A DraftFile is a temporary file
+/// that can be populated using the `Write` trait
+/// then finally and atomically persisted to a target file.
+pub struct DraftFile {
+    file: NamedTempFile,
+    target: PathBuf,
+}
+
+impl DraftFile {
+    /// Create a draft for a file
+    pub fn new(target: impl AsRef<Path>) -> Result<DraftFile, PathsError> {
+        let target = target.as_ref();
+
+        // Since the persist method will rename the temp file into the target,
+        // one has to create the temp file in the same file system as the target.
+        let dir = target
+            .parent()
+            .ok_or_else(|| PathsError::ParentDirNotFound {
+                path: target.as_os_str().into(),
+            })?;
+        let file = NamedTempFile::new_in(dir)?;
+        let target = target.to_path_buf();
+
+        Ok(DraftFile { file, target })
+    }
+
+    /// Atomically persist the file into its target path
+    pub fn persist(self) -> Result<(), PathsError> {
+        let target = &self.target;
+        let _ = self
+            .file
+            .persist(target)
+            .map_err(|error| PathsError::FileCreationFailed(error, target.into()))?;
+
+        Ok(())
+    }
+}
+
+impl Write for DraftFile {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.file.write(buf)
+    }
+    #[inline]
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.file.flush()
     }
 }
 
