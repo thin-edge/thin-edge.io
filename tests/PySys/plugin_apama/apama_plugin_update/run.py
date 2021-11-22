6d2bfda39e4e0aa4ce52b618865995de13bd1709
Albin Suresh <albin.suresh@softwareag.com>
2021-11-22 12:41:28 +0530

    Apama project update test

--- tests/PySys/plugin_apama/apama_plugin_update/run.py
+++ tests/PySys/plugin_apama/apama_plugin_update/run.py
@@ -9,20 +9,22 @@ This project deploys a mon file named "TedgeDemoMonitor" into the apama correlat
 """
 
 
-class ApamaPluginRemoveTest(ApamaPlugin):
+class ApamaPluginUpdateTest(ApamaPlugin):
 
     def setup(self):
         super().setup()
         # Assert that an apama project is not installed on the machine before test
         self.assert_project_installed(negate=True, abortOnError=True)
 
-        # Install the project to be removed by this test
+        # Install the project to be updated by this test
         self.install_project(self.input + "/quickstart.zip")
         self.assert_project_installed(abortOnError=True)
+        self.assert_monitor_installed("TedgeDemoMonitor")
 
     def execute(self):
-        self.remove_project()
+        self.plugin_install(self.input + "/limitedbandwidth.zip")
 
     def validate(self):
-        # Assert project NOT installed
-        self.assert_project_installed(negate=True)
+        self.assert_project_installed()
+        self.assert_monitor_installed("TedgeDemoMonitor", negate=True)
+        self.assert_monitor_installed("ThinEdgeIoExample")
