a4ffeccf60090e4456755bc53a6e3b8c8038e855
Lukasz Woznicki <75632179+makr11st@users.noreply.github.com>
2021-11-24 20:54:56 +0000

    Restructure directories of the workspace (#559)
    * Restructure directories of the workspace
    * Rename c8y_translator_lib to c8y_translator
    * Update comment on how to get dummy plugin path
    
    Signed-off-by: Lukasz Woznicki <lukasz.woznicki@softwareag.com>

---
+++ crates/core/tedge_mapper/src/error.rs
@@ -0,0 +1,46 @@
+use crate::size_threshold::SizeThresholdExceeded;
+use mqtt_client::MqttClientError;
+use tedge_config::TEdgeConfigError;
+use thin_edge_json::serialize::ThinEdgeJsonSerializationError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum MapperError {
+    #[error(transparent)]
+    FromMqttClient(#[from] MqttClientError),
+
+    #[error("Home directory is not found.")]
+    HomeDirNotFound,
+
+    #[error(transparent)]
+    FromTEdgeConfig(#[from] TEdgeConfigError),
+
+    #[error(transparent)]
+    FromConfigSetting(#[from] tedge_config::ConfigSettingError),
+
+    #[error(transparent)]
+    FromFlockfile(#[from] flockfile::FlockfileError),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConversionError {
+    #[error(transparent)]
+    FromMapper(#[from] MapperError),
+
+    #[error(transparent)]
+    FromCumulocityJsonError(#[from] c8y_translator::json::CumulocityJsonError),
+
+    #[error(transparent)]
+    FromThinEdgeJsonSerialization(#[from] ThinEdgeJsonSerializationError),
+
+    #[error(transparent)]
+    FromThinEdgeJsonParser(#[from] thin_edge_json::parser::ThinEdgeJsonParserError),
+
+    #[error(transparent)]
+    FromSizeThresholdExceeded(#[from] SizeThresholdExceeded),
+
+    #[error("The given Child ID '{id}' is invalid.")]
+    InvalidChildId { id: String },
+
+    #[error(transparent)]
+    FromMqttClient(#[from] MqttClientError),
+}
