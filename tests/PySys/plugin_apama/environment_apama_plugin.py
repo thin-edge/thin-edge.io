527d8c9c978248e67996122f59922580d7bdec82
Albin Suresh <albin.suresh@softwareag.com>
2021-12-06 12:54:01 +0530

    Fixing tests again

--- tests/PySys/plugin_apama/environment_apama_plugin.py
+++ tests/PySys/plugin_apama/environment_apama_plugin.py
@@ -13,7 +13,7 @@ class ApamaPlugin(BaseTest):
     apama_env_cmd = "/opt/softwareag/Apama/bin/apama_env"
     apama_dir = "/etc/tedge/apama"
     apama_project_dir = "/etc/tedge/apama/project"
-    tmp_apama_dir = "/tmp/apama/project"
+    tmp_apama_dir = "/tmp/tedge_apama_project"
     sudo = "/usr/bin/sudo"
 
     def setup(self):
