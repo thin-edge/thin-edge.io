3ecb95a8fa2341677cade352347a3777441293c3
Michael Abel <info@abel-ikt.de>
2021-12-07 14:40:31 +0100

    Fix permissions of plugin

--- ci/ci_run_all_plugin_tests.sh
+++ ci/ci_run_all_plugin_tests.sh
@@ -37,6 +37,7 @@ set -e
 mv __pysys_junit_xml pysys_junit_xml_apt
 
 sudo cp ../../plugins/tedge_docker_plugin/tedge_docker_plugin.sh /etc/tedge/sm-plugins/docker
+sudo chmod +x /etc/tedge/sm-plugins/docker
 
 set +e
 pysys.py run --record -v DEBUG 'docker_*' -XmyPlatform='container' -Xdockerplugin='dockerplugin'
