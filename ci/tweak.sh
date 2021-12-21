a3a1be84baca23834085684844655fcfd9150629
Michael Abel <info@abel-ikt.de>
2021-12-21 17:27:51 +0100

    Add workaround for hosted pis

--- ci/tweak.sh
+++ ci/tweak.sh
@@ -2,8 +2,21 @@
 
 echo "Tweak Mosquitto configuration"
 
-sudo cp /usr/lib/systemd/system/mosquitto.service ci/mosquitto.service.bak
+# Rpi at Mythic Beasts
+if [ -f  /lib/systemd/system/mosquitto.service ]; then
+    echo "This is a hosted Pi"
+    SERVICEFILE=/usr/lib/systemd/system/mosquitto.service
+fi
 
-sudo cp ci/mosquitto.service /usr/lib/systemd/system/mosquitto.service
+# Regular Rpi and Debian
+if [ -f  /usr/lib/systemd/system/mosquitto.service ]; then
+    echo "This is a regular Debian"
+    SERVICEFILE=/usr/lib/systemd/system/mosquitto.service
+fi
+
+sudo rm -f ci/mosquitto.service.bak
+sudo cp $SERVICEFILE ci/mosquitto.service.bak
+sudo cp ci/mosquitto.service $SERVICEFILE
+
 
 sudo systemctl daemon-reload
