45277eb64d8112e0c952d2e6f3cfd0fca34574e9
initard <solo@softwareag.com>
2022-01-14 18:27:50 +0100

    creating inventory fragments and refactoring supported operations fragments (#686)
    Signed-off-by: initard <solo@softwareag.com>

--- crates/core/tedge_mapper/src/c8y_converter.rs
+++ crates/core/tedge_mapper/src/c8y_converter.rs
@@ -1,3 +1,4 @@
+use crate::c8y_fragments::C8yAgentFragment;
 use crate::error::*;
 use crate::size_threshold::SizeThreshold;
 use crate::{converter::*, operations::Operations};
@@ -6,18 +7,23 @@ use c8y_smartrest::smartrest_serializer::{SmartRestSerializer, SmartRestSetSuppo
 use c8y_translator::json;
 use mqtt_client::{Message, Topic};
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use thin_edge_json::alarm::ThinEdgeAlarm;
 
 const SMARTREST_PUBLISH_TOPIC: &str = "c8y/s/us";
+const INVENTORY_FRAGMENTS_FILE_LOCATION: &str = "/etc/tedge/devices/info.json";
 
 pub struct CumulocityConverter {
     pub(crate) size_threshold: SizeThreshold,
     children: HashSet<String>,
     pub(crate) mapper_config: MapperConfig,
+    device_name: String,
 }
 
 impl CumulocityConverter {
-    pub fn new(size_threshold: SizeThreshold) -> Self {
+    pub fn new(size_threshold: SizeThreshold, device_name: String) -> Self {
         let mut topic_fiter = make_valid_topic_filter_or_panic("tedge/measurements");
         let () = topic_fiter
             .add("tedge/measurements/+")
@@ -33,10 +39,12 @@ impl CumulocityConverter {
         };
 
         let children: HashSet<String> = HashSet::new();
+
         CumulocityConverter {
             size_threshold,
             children,
             mapper_config,
+            device_name,
         }
     }
 
@@ -108,16 +116,53 @@ impl Converter for CumulocityConverter {
     }
 
     fn try_init_messages(&self) -> Result<Vec<Message>, ConversionError> {
-        let ops = Operations::try_new("/etc/tedge/operations")?;
-        let ops = ops.get_operations_list("c8y");
+        let fragments_message = create_inventory_fragments_message(&self.device_name)?;
 
-        let ops_msg = SmartRestSetSupportedOperations::new(&ops);
-        let topic = Topic::new_unchecked("c8y/s/us");
-        let msg = Message::new(&topic, ops_msg.to_smartrest()?);
-        Ok(vec![msg])
+        let supported_operations_message = create_supported_operations_fragments()?;
+
+        Ok(vec![supported_operations_message, fragments_message])
     }
 }
 
+fn create_supported_operations_fragments() -> Result<Message, ConversionError> {
+    let ops = Operations::try_new("/etc/tedge/operations")?;
+    let ops = ops.get_operations_list("c8y");
+
+    let ops_msg = SmartRestSetSupportedOperations::new(&ops);
+    let topic = Topic::new_unchecked("c8y/s/us");
+    Ok(Message::new(&topic, ops_msg.to_smartrest()?))
+}
+
+fn create_inventory_fragments_message(device_name: &str) -> Result<Message, ConversionError> {
+    let ops_msg = get_inventory_fragments(INVENTORY_FRAGMENTS_FILE_LOCATION)?;
+
+    let topic = Topic::new_unchecked(&format!(
+        "c8y/inventory/managedObjects/update/{}",
+        device_name
+    ));
+    Ok(Message::new(&topic, ops_msg.to_string()))
+}
+
+fn read_json_from_file(file_path: &str) -> Result<serde_json::Value, ConversionError> {
+    let mut file = File::open(Path::new(file_path))?;
+    let mut data = String::new();
+    file.read_to_string(&mut data)?;
+    let json: serde_json::Value = serde_json::from_str(&data)?;
+    Ok(json)
+}
+
+fn get_inventory_fragments(file_path: &str) -> Result<serde_json::Value, ConversionError> {
+    let mut json = read_json_from_file(file_path)?;
+
+    let agent_fragment = C8yAgentFragment::new();
+    let json_fragment = agent_fragment.to_json()?;
+
+    json.as_object_mut()
+        .ok_or_else(|| return ConversionError::FromOptionToResultConversion)?
+        .insert("c8y_Agent".to_string(), json_fragment);
+
+    Ok(json)
+}
 fn get_child_id_from_topic(topic: &str) -> Result<Option<String>, ConversionError> {
     match topic.strip_prefix("tedge/measurements/").map(String::from) {
         Some(maybe_id) if maybe_id.is_empty() => {
