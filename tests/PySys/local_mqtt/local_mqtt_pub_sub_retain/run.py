7c75deb11758baaf9764007e7f0769ac34283e6b
Pradeep Kumar K J <pradeepkumar.kj@sofwareag.com>
2022-01-25 15:56:09 +0000

    add clear flag test as well

--- tests/PySys/local_mqtt/local_mqtt_pub_sub_retain/run.py
+++ tests/PySys/local_mqtt/local_mqtt_pub_sub_retain/run.py
@@ -10,7 +10,11 @@ When we publish two messages with retain flag on to test/alarms/temp_sensor
 When we start tedge sub in the background that subscribes to test/alarms/temp_sensor topic
 When we publish an empty message with retain flag on to test/alarms/temp_sensor
 Then we kill the subscriber
-Then we find the retained message in the output of the subscriber log
+Now publish an empty message with retain flag on to test/alarms/temp_sensor
+Start tedge sub in the background that subscribes to test/alarms/temp_sensor topic
+Then we kill the subscriber
+Then verify that there are retained message in the output of the first subscriber log
+No empty message is present in the second subscriber log
 """
 
 
@@ -38,7 +42,7 @@ class MqttPublishWithRetain(BaseTest):
 
         temp_sensor_sub = self.startProcess(
             command=self.sudo,
-            arguments=[self.tedge, "mqtt", "sub", "test/alarms/temp_sensor"],
+            arguments=[self.tedge, "mqtt", "sub", "test/alarms/#"],
             stdouterr="temp_sensor_sub",
             background=True,
         )
@@ -59,7 +63,36 @@ class MqttPublishWithRetain(BaseTest):
             arguments=["killall", "tedge"],
             stdouterr="kill_out",
         )
+
+        # Publish an empty message and start the subscriber, Now the subscriber will not receive any 
+        # message since it is an empty message.
+
+        pub_to_clear_alarm = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "mqtt", "pub", "--retain", "--qos", "1", "test/alarms/temp_sensor", ""],
+            stdouterr="pub_to_clear_alarm",
+        )
+
+        temp_sensor_empty_message = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "mqtt", "sub", "test/alarms/#"],
+            stdouterr="temp_sensor_empty_message",
+            background=True,
+        )
+
+         # wait for a while before killing the subscribers
+        time.sleep(1)
+
+        # Kill the subscriber process explicitly with sudo as PySys does
+        # not have the rights to do it
+        kill = self.startProcess(
+            command=self.sudo,
+            arguments=["killall", "tedge"],
+            stdouterr="kill_out",
+        )
      
     def validate(self):
         self.assertGrep("temp_sensor_sub.out", "\[test/alarms/temp_sensor\] alarm msg 2", contains=True)
         self.assertGrep("temp_sensor_sub.out", "\[test/alarms/temp_sensor\] ", contains=True)
+        self.assertGrep("temp_sensor_empty_message.out", "\[test/alarms/temp_sensor\] ", contains=False)
+
