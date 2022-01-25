5a6a0492afbbb3f57da241b70f28021adab507a0
Michael Abel <info@abel-ikt.de>
2022-01-25 18:11:26 +0100

    Add connection management methods to the TedgeEnvironment

--- tests/PySys/environments/environment.py
+++ tests/PySys/environments/environment.py
@@ -10,7 +10,7 @@ class TedgeEnvironment(BaseTest):
         """Make sure we do not restart mosqiotto too fast
         Systemd will become suspicios when whe restart faster than 5 seconds
         """
-        minimum_time = 15 #5
+        minimum_time = 5
         etimes = subprocess.check_output("/usr/bin/ps -o etimes $(pidof mosquitto)", shell=True)
         runtime = int(etimes.split()[1])
         if runtime <= minimum_time:
@@ -20,3 +20,36 @@ class TedgeEnvironment(BaseTest):
             self.log.info(f"Delaying execution by {delay} seconds" )
             time.sleep( delay)
 
+
+
+    def tedge_connect_c8y(self, expectedExitStatus="==0"):
+
+        self.wait_if_restarting_mosquitto_too_fast()
+
+        connect = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "connect", "c8y"],
+            stdouterr="tedge_connect_c8y",
+            expectedExitStatus = expectedExitStatus,
+        )
+        return connect
+
+    def tedge_disconnect_c8y(self, expectedExitStatus="==0"):
+
+        self.wait_if_restarting_mosquitto_too_fast()
+
+        connect = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "disconnect", "c8y"],
+            stdouterr="tedge_disconnect_c8y",
+            expectedExitStatus = expectedExitStatus,
+        )
+        return connect
+
+    def tedge_connect_c8y_test(self, expectedExitStatus="==0"):
+        connect_c8y = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "connect", "c8y", "--test"],
+            stdouterr="tedge_connect_c8y_test",
+            expectedExitStatus = expectedExitStatus,
+        )
