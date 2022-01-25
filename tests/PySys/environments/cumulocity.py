f832126cf82d95fef2ccb21f9643f177bbdf6fde
Michael Abel <info@abel-ikt.de>
2022-01-25 16:57:44 +0100

    Move Cumumlocity class to own file

--- tests/PySys/environments/cumulocity.py
+++ tests/PySys/environments/cumulocity.py
@@ -3,24 +3,12 @@ import json
 import re
 import requests
 
-import psutil
-
-from pysys.basetest import BaseTest
-from pysys.constants import FAILED
-
-from cumulocity import Cumulocity
-from environment import TedgeEnvironment
-
-"""
-Environment to manage automated connect and disconnect to c8y
-
-class EnvironmentC8y(TedgeEnvironment):
-    """
-    Pysys Environment to manage automated connect and disconnect to c8y
-
-    Tests that derive from class EnvironmentC8y use automated connect and
-    disconnect to Cumulocity. Additional checks are made for the status of
-    service mosquitto and service tedge-mapper.
+class Cumulocity(object):
+    """Class to retrieve information about Cumulocity.
+    TODO : Review if we download enough data -> pageSize
+    TODO : Documentation and test for all of these functions
+    TODO : Extract as separate package
+    TODO : Make this more bulletproof
     """
 
     c8y_url = ""
@@ -199,113 +187,3 @@ class EnvironmentC8y(TedgeEnvironment):
             url="{}/inventory/managedObjects/{}".format(self.c8y_url, internal_id), auth=self.auth)
         if res.status_code != 204 or res.status_code != 404:
             res.raise_for_status()
-
-class EnvironmentC8y(TedgeEnvironment):
-    cumulocity: Cumulocity
-
-    def setup(self):
-        self.log.debug("EnvironmentC8y Setup")
-
-        if self.project.c8yurl == "":
-            self.abort(
-                FAILED, "Cumulocity tenant URL is not set. Set with the env variable C8YURL")
-        if self.project.tenant == "":
-            self.abort(
-                FAILED, "Cumulocity tenant ID is not set. Set with the env variable C8YTENANT")
-        if self.project.c8yusername == "":
-            self.abort(
-                FAILED, "Cumulocity tenant username is not set. Set with the env variable C8YUSERNAME")
-        if self.project.c8ypass == "":
-            self.abort(
-                FAILED, "Cumulocity tenant password is not set. Set with the env variable C8YPASS")
-        if self.project.deviceid == "":
-            self.abort(
-                FAILED, "Device ID is not set. Set with the env variable C8YDEVICEID")
-
-        self.tedge = "/usr/bin/tedge"
-        self.tedge_mapper_c8y = "tedge-mapper-c8y"
-        self.sudo = "/usr/bin/sudo"
-        self.systemctl = "/usr/bin/systemctl"
-        self.log.info("EnvironmentC8y Setup")
-        self.addCleanupFunction(self.myenvcleanup)
-
-        # Check if tedge-mapper is in disabled state
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
-            stdouterr="serv_mapper1",
-            expectedExitStatus="==3",  # 3: disabled
-        )
-
-        self.wait_if_restarting_mosquitto_too_fast()
-
-        # Connect the bridge
-        connect = self.startProcess(
-            command=self.sudo,
-            arguments=[self.tedge, "connect", "c8y"],
-            stdouterr="tedge_connect",
-        )
-
-        # Test the bridge connection
-        connect = self.startProcess(
-            command=self.sudo,
-            arguments=[self.tedge, "connect", "c8y", "--test"],
-            stdouterr="tedge_connect_test",
-        )
-
-        # Check if mosquitto is running well
-        serv_mosq = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", "mosquitto"],
-            stdouterr="serv_mosq2",
-        )
-
-        # Check if tedge-mapper is active again
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
-            stdouterr="serv_mapper3",
-        )
-
-        self.cumulocity = Cumulocity(
-            self.project.c8yurl, self.project.tenant, self.project.c8yusername, self.project.c8ypass, self.log)
-
-    def execute(self):
-        self.log.debug("EnvironmentC8y Execute")
-
-    def validate(self):
-        self.log.debug("EnvironmentC8y Validate")
-
-        # Check if mosquitto is running well
-        serv_mosq = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", "mosquitto"],
-            stdouterr="serv_mosq",
-        )
-
-        # Check if tedge-mapper is active
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
-            stdouterr="serv_mapper4",
-        )
-
-    def myenvcleanup(self):
-        self.log.debug("EnvironmentC8y Cleanup")
-
-        self.wait_if_restarting_mosquitto_too_fast()
-        # Disconnect Bridge
-        disconnect = self.startProcess(
-            command=self.sudo,
-            arguments=[self.tedge, "disconnect", "c8y"],
-            stdouterr="tedge_disconnect",
-        )
-
-        # Check if tedge-mapper is disabled
-        serv_mosq = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
-            stdouterr="serv_mapper5",
-            expectedExitStatus="==3",
-        )
-
