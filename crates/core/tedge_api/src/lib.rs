32d66840213fd983b40c5000b1fe22963becd936
Marcel Müller <m.mueller@ifm.com>
2022-02-04 14:56:38 +0100

    Add the important types to the root of the crate
    Having the important types at the root allows users of the library to
    easier see what they should focus on.
    
    Signed-off-by: Marcel Müller <m.mueller@ifm.com>
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge_api/src/lib.rs
+++ crates/core/tedge_api/src/lib.rs
@@ -1,6 +1,13 @@
 #![allow(dead_code)]
 
 pub mod plugins;
-pub mod errors;
-pub mod messages;
+pub use plugins::{Plugin, PluginBuilder, PluginConfiguration};
+
 pub mod address;
+pub use address::Address;
+
+pub mod errors;
+pub use errors::{PluginConfigurationError, PluginError};
+
+pub mod messages;
+pub use messages::{CoreMessage, PluginMessage};
