103c5ff0c3d585d9b7177d88e9e66137fe875124
Matthias Beyer <matthias.beyer@ifm.com>
2022-02-04 12:12:59 +0100

    Replace single-character string constant with character
    This fixes clippy lint `clippy::single_char_pattern`.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge/src/cli/connect/c8y_direct_connection.rs
+++ crates/core/tedge/src/cli/connect/c8y_direct_connection.rs
@@ -24,7 +24,7 @@ pub fn create_device_with_direct_connection(
     const DEVICE_CREATE_ERROR_TOPIC: &str = "s/e";
 
     let address = bridge_config.address.clone();
-    let host: Vec<&str> = address.split(":").collect();
+    let host: Vec<&str> = address.split(':').collect();
 
     let mut mqtt_options = MqttOptions::new(bridge_config.remote_clientid.clone(), host[0], 8883);
     mqtt_options.set_keep_alive(std::time::Duration::from_secs(5));
