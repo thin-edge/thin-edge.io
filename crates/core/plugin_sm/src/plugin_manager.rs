d2dd361230ef5ce8cb777ac20cb6a45fad3691ee
Matthias Beyer <matthias.beyer@ifm.com>
2022-02-07 09:33:34 +0100

    Remove borrow that is immediately dereferenced
    This fixes clippy lint `clippy::needless_borrow`.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/plugin_sm/src/plugin_manager.rs
+++ crates/core/plugin_sm/src/plugin_manager.rs
@@ -199,7 +199,7 @@ impl ExternalPlugins {
 
         for (software_type, plugin) in self.plugin_map.iter() {
             match plugin.list(&mut logger).await {
-                Ok(software_list) => response.add_modules(&software_type, software_list),
+                Ok(software_list) => response.add_modules(software_type, software_list),
                 Err(_) => {
                     error_count += 1;
                 }
