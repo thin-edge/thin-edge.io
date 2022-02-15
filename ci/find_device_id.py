9a2f75b24657437986bb93c8879a14ffbc8805e7
Michael Abel <info@abel-ikt.de>
2022-02-15 15:44:35 +0100

    Use name keyword to get the right device

--- ci/find_device_id.py
+++ ci/find_device_id.py
@@ -18,9 +18,10 @@ from c8y_api import CumulocityApi
 
 def get_device_id(c8y, name):
     """retrive the current device ID"""
-    for dev in c8y.device_inventory.get_all():
-        if name == dev.name:
-            return dev.id
+
+    devices = c8y.device_inventory.get_all(name=name)
+    if len(devices) == 1:
+        return devices[0].id
     return None
 
 
