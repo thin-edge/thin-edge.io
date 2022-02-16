e0dcf6479a41dbefe72167160ae67856e76bd72f
Didier Wenzek <didier.wenzek@free.fr>
2022-02-16 15:06:28 +0000

    Use `read_line` instead of `exp_string`
    The former properly handles end of lines.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/tests/test-tedge-cli/src/lib.rs
+++ crates/tests/test-tedge-cli/src/lib.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests {
-    use rexpect::errors::*;
     use rexpect::*;
+    use rexpect::errors::*;
+    use rexpect::process::signal::Signal;
 
     const TIMEOUT_MS: Option<u64> = Some(5_000);
 
@@ -9,14 +10,14 @@ mod tests {
     fn it_works() -> Result<()> {
         let mut sub = spawn("tedge mqtt sub test/topic", TIMEOUT_MS)?;
 
-        execute(r#"tedge mqtt pub test/topic "hello thin-edge""#)?;
-        sub.exp_string("INFO: Connected\n")?;
-        sub.exp_string("[test/topic] hello thin-edge\n")?;
+        execute(r#"tedge mqtt pub test/topic hello-thin-edge"#)?;
+        assert_eq!(sub.read_line()?, "INFO: Connected");
+        assert_eq!(sub.read_line()?, "[test/topic] hello-thin-edge");
 
-        execute(r#"tedge mqtt pub test/topic "bye bye" --qos 2"#)?;
-        sub.exp_string("[test/topic] bye bye\n")?;
+        execute(r#"tedge mqtt pub test/topic bye-bye --qos 2"#)?;
+        assert_eq!(sub.read_line()?, "[test/topic] bye-bye");
 
-        sub.process.wait()?;
+        sub.process.kill(Signal::SIGTERM)?;
         Ok(())
     }
 
