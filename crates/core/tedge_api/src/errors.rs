ad626f5a70a16f809e00d3ebd5017d113cafd984
Marcel Müller <m.mueller@ifm.com>
2022-02-23 09:59:38 +0100

    Add anyhow and catchall error kind
    Signed-off-by: Marcel Müller <m.mueller@ifm.com>
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge_api/src/errors.rs
+++ crates/core/tedge_api/src/errors.rs
@@ -1,9 +1,11 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-#[error("An error occured while interacting with this plugin")]
 pub enum PluginError {
     #[error("The sender could not transfer the message to its receiving end. Did it get closed?")]
     CouldNotSendMessage(#[from] tokio::sync::mpsc::error::SendError<crate::Message>),
+    #[error("An error in the configuration was found")]
     Configuration(#[from] toml::de::Error),
+    #[error(transparent)]
+    Custom(#[from] anyhow::Error)
 }
