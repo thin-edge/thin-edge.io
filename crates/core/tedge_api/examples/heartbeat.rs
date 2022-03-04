thin-edge/thin-edge.io

commit ca5672eebbbdf61ee1994dd0ea13346d117cb755

Add Comms::new_message()
This patch adds the Comms::new_message() function, which can be used to
construct a new message with the origin automatically set correctly.

When the Comms object is passed to a plugin, the plugin can use this
function to automatically create a message that contains the right
origin.

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

crates/core/tedge_api/examples/heartbeat.rs
@@ -83,7 +83,7 @@ async fn main() {
     let hsb = HeartbeatServiceBuilder;
     let (sender, mut receiver) = tokio::sync::mpsc::channel(10);
 
-    let comms = Comms::new(sender);
+    let comms = Comms::new("heartbeat-service".to_string(), sender);
 
     let config = toml::from_str(
         r#"

crates/core/tedge_api/src/plugins.rs
@@ -17,16 +17,23 @@ type ReplyReceiver = tokio::sync::oneshot::Receiver<Message>;
 
 #[derive(Clone)]
 pub struct Comms {
+    plugin_name: String,
     sender: tokio::sync::mpsc::Sender<Message>,
     replymap: Arc<RwLock<HashMap<uuid::Uuid, ReplySender>>>,
 }
 
 impl Comms {
-    pub fn new(sender: tokio::sync::mpsc::Sender<Message>) -> Self {
+    pub fn new(plugin_name: String, sender: tokio::sync::mpsc::Sender<Message>) -> Self {
         Self {
+            plugin_name,
             sender,
             replymap: Arc::new(RwLock::new(HashMap::new())),
         }
+    }
+
+    pub fn new_message(&self, kind: crate::MessageKind) -> Message {
+        let addr = crate::Address::new(crate::address::EndpointKind::Plugin { id: self.plugin_name.clone() });
+        Message::new(addr, kind)
     }
 
     pub async fn send<T: Into<Message>>(&self, msg: T) -> Result<(), PluginError> {

commit 7103c78a6891019900ae04374a78564dab89a11e

Adapt example to new interface
`Message::new()` was made private, so we must use `Comms::new_message()`
now. This change was missing in the commit that changed that interface.

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>
Fixes: ca5672eebbbdf61ee1994dd0ea13346d117cb755 ("Add Comms::new_message()")
Signed-off-by: Marcel Müller <m.mueller@ifm.com>

crates/core/tedge_api/examples/heartbeat.rs
@@ -58,12 +58,10 @@ impl Plugin for HeartbeatService {
     async fn handle_message(&self, message: Message) -> Result<(), PluginError> {
         match message.kind() {
             MessageKind::CheckReadyness => {
-                let msg = Message::new(
-                    message.origin().clone(),
-                    MessageKind::SignalPluginState {
-                        state: String::from("Ok"),
-                    },
-                );
+                let kind = MessageKind::SignalPluginState {
+                    state: String::from("Ok"),
+                };
+                let msg = self.comms.new_message(message.origin().clone(), kind);
                 self.comms.send(msg).await?;
             }
             msg => println!("Does not handle: {:#?}", msg),