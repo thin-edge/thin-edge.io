43834776d7262b3906ffa99fb5b45997611e5591
Christoph Stoidner <christoph.stoidner@softwareag.com>
2022-03-09 08:45:48 +0000

    added bridge-scan and error-handling

--- crates/core/tedge_agent/src/bin/tedge-selfupdater.rs
+++ crates/core/tedge_agent/src/bin/tedge-selfupdater.rs
@@ -1,6 +1,9 @@
 
 use clap::Parser;
 use std::process::{Command, ExitStatus, Stdio};
+use std::path::Path;
+
+use tedge_config::DEFAULT_TEDGE_CONFIG_PATH;
 
 #[derive(Parser)]
 struct AptCli {
@@ -18,39 +21,8 @@ pub enum PluginOp {
 
 }
 
-// TODO: adapt enum below for self-updater purpose
-#[derive(thiserror::Error, Debug)]
-pub enum InternalError {
-    #[error("Fail to run `{cmd}`: {from}")]
-    ExecError { cmd: String, from: std::io::Error },
 
-    #[error(transparent)]
-    FromIo(#[from] std::io::Error),
-
-    #[error(transparent)]
-    FromUtf8(#[from] std::string::FromUtf8Error),
-
-    #[error("Parsing Debian package failed for `{file}`")]
-    ParsingError { file: String },
-
-    #[error("Validation of {package} failed with version mismatch. Installed version: {installed}, Expected version: {expected}")]
-    VersionMismatch {
-        package: String,
-        installed: String,
-        expected: String,
-    },
-}
-
-impl InternalError {
-    pub fn exec_error(cmd: impl Into<String>, from: std::io::Error) -> InternalError {
-        InternalError::ExecError {
-            cmd: cmd.into(),
-            from,
-        }
-    }
-}
-
-fn run(operation: PluginOp) -> Result<ExitStatus, InternalError> {
+fn run(operation: PluginOp) -> i32 {
     match operation {
         PluginOp::UpdateList {
             plugin_name,
@@ -58,76 +30,122 @@ fn run(operation: PluginOp) -> Result<ExitStatus, InternalError> {
 
             // identify all clouds
             let mut clouds : Vec<String> = Vec::new();
+            let mut failed = false;
 
-            // TODO: check for existing bridges 
-            clouds.push("c8y".to_string());
-            /*
-            foreach b in "/etc/tedge/mosquitto-conf\/\*-bridge.conf"
-              cloud += [ b ]
-            */
+            // check for existing bridges
+            // TODO: maybe better to have a tedge command as "tedge connected list", as here trying
+            //       to interpret files in mosquitto conf folder.
+            const TEDGE_BRIDGE_CONF_DIR_PATH: &str = "mosquitto-conf"; // NOTE: stolen from tedge/src/cli/connect/command.rs
+            const TEDGE_BRIDGE_CONF_POSTFIX : &str = "-bridge.conf";   // TODO: definition is just made here, might be better part of tedge CLI.
+
+            let config_location = DEFAULT_TEDGE_CONFIG_PATH; // NOTE: hardcoded here, maybe allow to set with option "--config-dir"
+
+            let bridge_cfg_dir = Path::new(config_location).join(TEDGE_BRIDGE_CONF_DIR_PATH);
+            let walker = globwalk::GlobWalkerBuilder::from_patterns(bridge_cfg_dir, &[format!("*{}", TEDGE_BRIDGE_CONF_POSTFIX)])
+                .max_depth(1)
+                .build().unwrap()
+                .into_iter()
+                .filter_map(Result::ok);
+
+            for img in walker {
+                let cloud_name = img.path()
+                                    .file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .strip_suffix(TEDGE_BRIDGE_CONF_POSTFIX)
+                                    .unwrap();
+                clouds.push(cloud_name.to_string());
+                println!("Found cloud connection: {}", cloud_name);
+            }
+
 
             // disconnect all clouds
-            // TODO: clone() below potentially not needed.
             for cloud in clouds.clone().into_iter() {
-                // TODO: what when some get disconnected but others fail!?
                 println!("Disconnecting {}", cloud);
-                run_cmd(
-                    "tedge",
-                    &format!("disconnect {}", cloud),
-                )?;
+                match run_tedge_cli(&format!("disconnect {}", cloud)) {
+                   Ok(_)  => (),
+                   Err(e) => {
+                       eprintln!("Error occured when calling tedge CLI: {}", e);
+                       failed = true; // one cloud failed to disconnect, so fail entire update
+                   }
+                }
             }
 
-            // exec plugin and forward STDIN
-            println!("plugin_name 22 update-list");
-            let status = Command::new(plugin_name)
-                .args(vec!["update-list"])
-                //.stdin(Stdio::piped())
-                //.stdout(Stdio::piped())
-                .status()
-                /*.map_err(|err| InternalError::exec_error("apt-get", err))*/?;
+            // NOTE: Here is point-of-no-return. SM Agent was stopped
 
+            let exitcode;
+            if failed {
+                // disconnect has completely or partially failed, so avoid to start update,
+                // instead report error to agent
+                eprintln!("Error on disconnect. At least one cloud could not be disconnected, so skipping update.");
+                exitcode = 2;
+            } else {
+                // exec plugin and forward STDIN
+                println!("Executing: {} update-list", plugin_name);
+                let status = Command::new(plugin_name)
+                    .args(vec!["update-list"])
+                    .status();
 
-            // TODO: handle error
-            store_exitcode(status_to_exitcode(Ok(status)));
+                exitcode = status_to_exitcode(status);
+            }
+
+            match store_exitcode(exitcode) {
+                Err(e) => eprintln!("Error writing exit-code '{}' to '/var/run/tedge_update/selfupdate-result': {}", exitcode, e),
+                Ok(_)  => println!("Stored exit-code '{}' to '/var/run/tedge_update/selfupdate-result'", exitcode),
+            };
 
             // connect all clouds
+            // NOTE: all clouds will be tried to connected here, even those that were not able to
+            //       be disconnect in loop on top.
             for cloud in clouds.into_iter() {
                 println!("Connectiong {}", cloud);
-                run_cmd(
-                    "tedge",
-                    &format!("connect {}", cloud),
-                )?;
+                match run_tedge_cli(&format!("connect {}", cloud)) {
+                    Ok(_)  => (),
+                    Err(e) => eprintln!("Error occured when calling tedge CLI {}", e),
+                }
             }
-
-            return Ok(status);
+            return exitcode;
         }
 
     };
 }
 
-
-fn run_cmd(cmd: &str, args: &str) -> Result<ExitStatus, InternalError> {
+fn run_tedge_cli(args: &str) -> std::result::Result<i32, String> {
     let args: Vec<&str> = args.split_whitespace().collect();
-    let status = Command::new(cmd)
-        .args(args)
-        .stdin(Stdio::null())
-        .status()
-        /*.map_err(|err| InternalError::exec_error(cmd, err))*/?;
-    Ok(status)
+
+    let status = Command::new("tedge")
+          .args(args)
+          .stdin(Stdio::null())
+          .status();
+
+    match status {
+        Ok(s) => match s.code() {
+            Some(code) => {
+                if code == 0 {
+                    Ok(0)
+                }
+                else {
+                    Err(format!("tedge exited with code: {}", code))
+                }
+            }
+            None => Err("tedge terminated by a signal".to_string())
+        },
+
+        Err(_) => Err("Could not exec tedge".to_string()),
+    }
 }
 
 fn store_exitcode(exit_code: i32) -> std::io::Result<()> {
     let filepath = "/var/run/tedge_update";
     let filename = "selfupdate-result";
 
-    // TODO: check existence of folder before creating 
-    println!("Storing final result ({}) in {}", exit_code, format!("{}/{}", filepath, filename)); 
-    std::fs::create_dir(filepath);
+    std::fs::create_dir_all(filepath)?;
     std::fs::write(format!("{}/{}", filepath, filename), exit_code.to_string())?;
     Ok(())
 }
 
-fn status_to_exitcode(status : Result<ExitStatus, InternalError>) -> i32 {
+fn status_to_exitcode(status : Result<ExitStatus, std::io::Error>) -> i32 {
     let exit_code;
 
     match status {
@@ -154,9 +172,9 @@ fn status_to_exitcode(status : Result<ExitStatus, InternalError>) -> i32 {
 
 fn main() {
 
-    // On usage error, the process exits with a status code of 1
     let apt = AptCli::parse();
-
-    run(apt.operation);
+    
+    let exitcode = run(apt.operation);
+    std::process::exit(exitcode);
 }
 
