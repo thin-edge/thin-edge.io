9be832bb86dbfc976c492b1c3fad69d875e7a6c2
Matthias Beyer <matthias.beyer@ifm.com>
2022-03-18 08:26:57 +0100

    Add doc: PluginError
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge_api/src/error.rs
+++ crates/core/tedge_api/src/error.rs
@@ -1,9 +1,14 @@
 use thiserror::Error;
 
+/// An error that a plugin might emit
 #[derive(Error, Debug)]
 pub enum PluginError {
+
+    /// Error kind if the configuration of the plugin was faulty
     #[error("An error in the configuration was found")]
     Configuration(#[from] toml::de::Error),
+
+    /// Error kind to report any `anyhow::Error` error
     #[error(transparent)]
     Custom(#[from] anyhow::Error),
 }
