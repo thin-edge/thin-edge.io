165a9832152b74bcde33554eca91d9c405ad6afd
Didier Wenzek <didier.wenzek@free.fr>
2022-04-02 10:46:04 +0100

    Remove the `AgentStateRepository` implementation
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge_agent/src/state.rs
+++ crates/core/tedge_agent/src/state.rs
@@ -1,16 +1,5 @@
-use crate::error::StateError;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, str::FromStr};
-use tedge_utils::fs::atomically_write_file_async;
-use tokio::fs;
-use tracing::error;
-
-#[derive(Debug)]
-pub struct AgentStateRepository {
-    state_repo_path: PathBuf,
-    state_repo_root: PathBuf,
-}
 
 #[async_trait]
 pub trait StateRepository {
@@ -21,75 +10,7 @@ pub trait StateRepository {
     async fn update(&self, status: &StateStatus) -> Result<(), Self::Error>;
 }
 
-#[async_trait]
-impl StateRepository for AgentStateRepository {
-    type Error = StateError;
-
-    async fn load(&self) -> Result<State, StateError> {
-        match fs::read(&self.state_repo_path).await {
-            Ok(bytes) => Ok(toml::from_slice::<State>(bytes.as_slice())?),
-
-            Err(err) => {
-                error!("Error reading: {:?}", &self.state_repo_path);
-                Err(StateError::FromIo(err))
-            }
-        }
-    }
-
-    async fn store(&self, state: &State) -> Result<(), StateError> {
-        let toml = toml::to_string_pretty(&state)?;
-
-        // Create `$HOME/.tedge` or `/etc/tedge` directory in case it does not exist yet
-        if !self.state_repo_root.exists() {
-            let () = fs::create_dir(&self.state_repo_root).await?;
-        }
-
-        let mut temppath = self.state_repo_path.clone();
-        temppath.set_extension("tmp");
-
-        let () =
-            atomically_write_file_async(temppath, &self.state_repo_path, toml.as_bytes()).await?;
-
-        Ok(())
-    }
-
-    async fn clear(&self) -> Result<State, Self::Error> {
-        let state = State {
-            operation_id: None,
-            operation: None,
-        };
-        let () = self.store(&state).await?;
-
-        Ok(state)
-    }
-
-    async fn update(&self, status: &StateStatus) -> Result<(), Self::Error> {
-        let mut state = self.load().await?;
-        state.operation = Some(status.to_owned());
-
-        self.store(&state).await?;
-
-        Ok(())
-    }
-}
-
-impl AgentStateRepository {
-    pub fn new(tedge_root: PathBuf) -> Self {
-        let mut state_repo_root = tedge_root;
-        state_repo_root.push(PathBuf::from_str(".agent").expect("infallible"));
-
-        let mut state_repo_path = state_repo_root.clone();
-        state_repo_path.push(PathBuf::from_str("current-operation").expect("infallible"));
-
-        Self {
-            state_repo_path,
-            state_repo_root,
-        }
-    }
-}
-
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-#[serde(untagged)]
 pub enum StateStatus {
     Software(SoftwareOperationVariants),
     Restart(RestartOperationStatus),
@@ -97,7 +18,6 @@ pub enum StateStatus {
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-#[serde(rename_all = "lowercase")]
 pub enum SoftwareOperationVariants {
     List,
     Update,
@@ -118,9 +38,9 @@ pub struct State {
 
 #[cfg(test)]
 mod tests {
+    use crate::journal::Journal;
     use crate::state::{
-        AgentStateRepository, RestartOperationStatus, SoftwareOperationVariants, State,
-        StateRepository, StateStatus,
+        RestartOperationStatus, SoftwareOperationVariants, State, StateRepository, StateStatus,
     };
 
     use tempfile::tempdir;
@@ -128,9 +48,7 @@ mod tests {
     #[tokio::test]
     async fn agent_state_repository_not_exists_fail() {
         let temp_dir = tempdir().unwrap();
-        let repo = AgentStateRepository::new(temp_dir.into_path());
-
-        repo.load().await.unwrap_err();
+        assert!(Journal::open(temp_dir.into_path()).await.is_err());
     }
 
     #[tokio::test]
@@ -140,11 +58,19 @@ mod tests {
         let _ = tokio::fs::create_dir(temp_dir.path().join(".agent/")).await;
         let destination_path = temp_dir.path().join(".agent/current-operation");
 
-        let content = "operation_id = \'1234\'\noperation = \"list\"";
+        let content = r#"(
+            state: (
+                operation_id: Some("1234"),
+                operation: Some(Software(List)),
+            ),
+            pending: [],
+        )"#;
 
-        let _ = tokio::fs::write(destination_path, content.as_bytes()).await;
+        let _ = tokio::fs::write(destination_path.clone(), content.as_bytes()).await;
 
-        let repo = AgentStateRepository::new(temp_dir.into_path());
+        let repo = Journal::open(destination_path)
+            .await
+            .expect("the previous journal");
 
         let data = repo.load().await.unwrap();
         assert_eq!(
@@ -162,12 +88,18 @@ mod tests {
 
         let _ = tokio::fs::create_dir(temp_dir.path().join(".agent/")).await;
         let destination_path = temp_dir.path().join(".agent/current-operation");
+        let content = r#"(
+            state: (
+                operation_id: Some("1234"),
+                operation: Some(Restart(Restarting)),
+            ),
+            pending: [],
+        )"#;
+        let _ = tokio::fs::write(destination_path.clone(), content.as_bytes()).await;
 
-        let content = "operation_id = \'1234\'\noperation = \"Restarting\"";
-
-        let _ = tokio::fs::write(destination_path, content.as_bytes()).await;
-
-        let repo = AgentStateRepository::new(temp_dir.into_path());
+        let repo = Journal::open(destination_path)
+            .await
+            .expect("the previous journal");
 
         let data = repo.load().await.unwrap();
         assert_eq!(
@@ -186,11 +118,9 @@ mod tests {
         let _ = tokio::fs::create_dir(temp_dir.path().join(".agent/")).await;
         let destination_path = temp_dir.path().join(".agent/current-operation");
 
-        let content = "";
-
-        let _ = tokio::fs::write(destination_path, content.as_bytes()).await;
-
-        let repo = AgentStateRepository::new(temp_dir.into_path());
+        let repo = Journal::open(destination_path)
+            .await
+            .expect("the previous journal");
 
         let data = repo.load().await.unwrap();
         assert_eq!(
@@ -209,7 +139,9 @@ mod tests {
         let _ = tokio::fs::create_dir(temp_dir.path().join(".agent/")).await;
         let destination_path = temp_dir.path().join(".agent/current-operation");
 
-        let repo = AgentStateRepository::new(temp_dir.into_path());
+        let repo = Journal::open(destination_path.clone())
+            .await
+            .expect("the previous journal");
 
         repo.store(&State {
             operation_id: Some("1234".into()),
@@ -220,6 +152,17 @@ mod tests {
 
         let data = tokio::fs::read_to_string(destination_path).await.unwrap();
 
-        assert_eq!(data, "operation_id = \'1234\'\noperation = \'list\'\n");
+        assert_eq!(
+            data.split_ascii_whitespace().collect::<String>(),
+            r#"(
+            state: (
+                operation_id: Some("1234"),
+                operation: Some(Software(List)),
+            ),
+            pending: [],
+        )"#
+            .split_ascii_whitespace()
+            .collect::<String>()
+        );
     }
 }
