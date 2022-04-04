f64d19f54c4b67936888ef4bd6842fe6de130221
Pradeep Kumar K J <pradeepkumar.kj@softwareag.com>
2022-04-04 07:00:18 -0700

    add config-dir

--- crates/core/tedge_watchdog/src/main.rs
+++ crates/core/tedge_watchdog/src/main.rs
@@ -1,69 +1,107 @@
+use clap::Parser;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
 use mqtt_channel::{Config, Message, MqttError, PubChannel, Topic};
 use nanoid::nanoid;
-use std::process::{self, Command, Stdio};
+use std::{
+    path::PathBuf,
+    process::{self, Command, Stdio},
+};
 
 use freedesktop_entry_parser::parse_entry;
 use serde::{Deserialize, Serialize};
 use tedge_config::{
     ConfigRepository, ConfigSettingAccessor, ConfigSettingError, MqttBindAddressSetting,
-    MqttPortSetting, TEdgeConfig, TEdgeConfigError,
+    MqttPortSetting, TEdgeConfigError, TEdgeConfigLocation, DEFAULT_TEDGE_CONFIG_PATH
 };
 use tokio::time::{error::Elapsed, Duration};
-
-pub const DEFAULT_TEDGE_CONFIG_PATH: &str = "/etc/tedge";
 
 #[derive(Serialize, Deserialize)]
 pub struct HealthStatus {
     status: String,
     pid: u32,
 }
-#[tokio::main]
-async fn main() {
-    let _ = start_watchdog().await;
+
+#[derive(Debug, clap::Parser)]
+#[clap(
+name = clap::crate_name!(),
+version = clap::crate_version!(),
+about = clap::crate_description!()
+)]
+pub struct WatchdogOpt {
+    /// Turn-on the debug log level.
+    ///
+    /// If off only reports ERROR, WARN, and INFO
+    /// If on also reports DEBUG and TRACE
+    #[clap(long)]
+    pub debug: bool,
+
+    /// Start the watchdog from custom path
+    ///
+    /// WARNING: This is mostly used in testing.
+    #[clap(long = "config-dir", default_value = DEFAULT_TEDGE_CONFIG_PATH)]
+    pub config_dir: PathBuf,
 }
 
-async fn start_watchdog() {
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let watchdog_opt = WatchdogOpt::parse();
+    tedge_utils::logging::initialise_tracing_subscriber(watchdog_opt.debug);
+
+    start_watchdog(watchdog_opt.config_dir).await?;
+    Ok(())
+}
+
+async fn start_watchdog(tedge_config_dir: PathBuf) -> Result<(), anyhow::Error> {
     // Send ready notification to systemd.
     let _ = notify_systemd(process::id(), "--ready");
 
-    loop {
-        let tedge_services = vec![
-            "tedge-mapper-c8y",
-            "tedge-mapper-az",
-            "tedge-mapper-collectd",
-            "tedge-agent",
-        ];
-        let watchdog_threads = FuturesUnordered::new();
+    let tedge_services = vec![
+        "tedge-mapper-c8y",
+        "tedge-mapper-az",
+        "tedge-mapper-collectd",
+        "tedge-agent",
+    ];
+    let watchdog_threads = FuturesUnordered::new();
 
-        for service in tedge_services {
-            let interval =
-                match get_watchdog_sec(&format!("/lib/systemd/system/{}.service", service)) {
-                    Ok(i) => i,
-                    Err(_e) => continue, // Watchdog not enabled for this service
-                };
-            let req_topic = format!("tedge/health-check/{}", service);
-            let res_topic = format!("tedge/health/{}", service);
-            if interval > 0 {
-                watchdog_threads.push(tokio::spawn(async move {
-                    monitor_tedge_service(service, &req_topic, &res_topic, interval / 2).await
-                }));
-            }
+    for service in tedge_services {
+        let interval = match get_watchdog_sec(&format!("/lib/systemd/system/{}.service", service)) {
+            Ok(i) => i,
+            Err(_e) => continue, // Watchdog not enabled for this service
+        };
+        let req_topic = format!("tedge/health-check/{}", service);
+        let res_topic = format!("tedge/health/{}", service);
+        let tedge_config_location =
+            tedge_config::TEdgeConfigLocation::from_custom_root(tedge_config_dir.clone());
+        if interval > 0 {
+            watchdog_threads.push(tokio::spawn(async move {
+                monitor_tedge_service(
+                    tedge_config_location,
+                    service,
+                    &req_topic,
+                    &res_topic,
+                    interval / 2,
+                )
+                .await
+            }));
         }
-
-        futures::future::join_all(watchdog_threads).await;
     }
+
+    futures::future::join_all(watchdog_threads).await;
+
+    Ok(())
 }
 
 async fn monitor_tedge_service(
+    tedge_config_location: TEdgeConfigLocation,
     name: &str,
     req_topic: &str,
     res_topic: &str,
     interval: u64,
 ) -> Result<(), WatchdogError> {
     let client_id: &str = &format!("{}_{}", name, nanoid!());
-    let mqtt_config = get_mqtt_config(client_id)?.with_subscriptions(res_topic.try_into()?);
+    let mqtt_config = get_mqtt_config(tedge_config_location, client_id)?
+        .with_subscriptions(res_topic.try_into()?);
     let client = mqtt_channel::Connection::new(&mqtt_config).await?;
     let mut received = client.received;
     let mut publisher = client.published;
@@ -102,8 +140,12 @@ async fn monitor_tedge_service(
     }
 }
 
-fn get_mqtt_config(client_id: &str) -> Result<Config, WatchdogError> {
-    let tedge_config = get_tedge_config()?;
+fn get_mqtt_config(
+    tedge_config_location: TEdgeConfigLocation,
+    client_id: &str,
+) -> Result<Config, WatchdogError> {
+    let config_repository = tedge_config::TEdgeConfigRepository::new(tedge_config_location.clone());
+    let tedge_config = config_repository.load()?;
     let mqtt_config = Config::default()
         .with_session_name(client_id)
         .with_host(tedge_config.query(MqttBindAddressSetting)?.to_string())
@@ -122,13 +164,6 @@ fn notify_systemd(pid: u32, status: &str) -> Result<(), WatchdogError> {
             from: err,
         })?;
     Ok(())
-}
-
-fn get_tedge_config() -> Result<TEdgeConfig, WatchdogError> {
-    let tedge_config_location =
-        tedge_config::TEdgeConfigLocation::from_custom_root(DEFAULT_TEDGE_CONFIG_PATH);
-    let config_repository = tedge_config::TEdgeConfigRepository::new(tedge_config_location.clone());
-    Ok(config_repository.load()?)
 }
 
 fn get_watchdog_sec(service_file: &str) -> Result<u64, WatchdogError> {
