9cd56d18ec342f685d32b75f4e927ec48c68c225
Michael Abel <info@abel-ikt.de>
2022-04-05 14:09:18 +0200

    black

--- ci/report/report_builder.py
+++ ci/report/report_builder.py
@@ -24,45 +24,105 @@ import subprocess
 
 
 runners_cfg = {
-    "michael":{     "repo":"abelikt",   "workflow":"system-test-workflow.yml",  "tests":["all", "apt", "apama", "docker", "sm", "analytics"] },
-    "offsitea":{    "repo":"abelikt",   "workflow":"system-test-workflow_A.yml",  "tests":["all", "apt", "apama", "docker", "sm", ] },
-    "offsiteb":{    "repo":"abelikt",   "workflow":"system-test-workflow_B.yml",  "tests":["all", "apt", "apama", "docker", "sm", ] },
-    "offsitec":{    "repo":"abelikt",   "workflow":"system-test-workflow_C.yml",  "tests":["all", "apt", "apama", "docker", "sm", ] },
-    "offsited":{    "repo":"abelikt",   "workflow":"system-test-workflow_D.yml",   "tests":["all", "apt", "apama", "docker", "sm", ] },
-    "sag":{         "repo":"thin-edge", "workflow":"system-test-workflow.yml",  "tests":["all" ] },
-    "offsite-sag":{ "repo":"thin-edge", "workflow":"system-test-offsite.yml",   "tests":["all", "apt", "docker", "sm", ] },
-        }
+    "michael": {
+        "repo": "abelikt",
+        "workflow": "system-test-workflow.yml",
+        "tests": ["all", "apt", "apama", "docker", "sm", "analytics"],
+    },
+    "offsitea": {
+        "repo": "abelikt",
+        "workflow": "system-test-workflow_A.yml",
+        "tests": [
+            "all",
+            "apt",
+            "apama",
+            "docker",
+            "sm",
+        ],
+    },
+    "offsiteb": {
+        "repo": "abelikt",
+        "workflow": "system-test-workflow_B.yml",
+        "tests": [
+            "all",
+            "apt",
+            "apama",
+            "docker",
+            "sm",
+        ],
+    },
+    "offsitec": {
+        "repo": "abelikt",
+        "workflow": "system-test-workflow_C.yml",
+        "tests": [
+            "all",
+            "apt",
+            "apama",
+            "docker",
+            "sm",
+        ],
+    },
+    "offsited": {
+        "repo": "abelikt",
+        "workflow": "system-test-workflow_D.yml",
+        "tests": [
+            "all",
+            "apt",
+            "apama",
+            "docker",
+            "sm",
+        ],
+    },
+    "sag": {
+        "repo": "thin-edge",
+        "workflow": "system-test-workflow.yml",
+        "tests": ["all"],
+    },
+    "offsite-sag": {
+        "repo": "thin-edge",
+        "workflow": "system-test-offsite.yml",
+        "tests": [
+            "all",
+            "apt",
+            "docker",
+            "sm",
+        ],
+    },
+}
+
 
 def cleanup(download_reports):
 
-    folders =     [
-    "*.xml",
-    "*.html",
-    "system-test-workflow",
-    "system-test-workflow_A",
-    "system-test-workflow_B",
-    "system-test-workflow_C",
-    "system-test-workflow_D",
-    "ci_system-test-workflow",
-    "ci_system-test-workflow_A",
-    "ci_system-test-workflow_B",
-    "ci_system-test-workflow_C",
-    "ci_system-test-workflow_D",
-    "sag_system-test-workflow",
-    "sag_system-test-offsite"]
+    folders = [
+        "*.xml",
+        "*.html",
+        "system-test-workflow",
+        "system-test-workflow_A",
+        "system-test-workflow_B",
+        "system-test-workflow_C",
+        "system-test-workflow_D",
+        "ci_system-test-workflow",
+        "ci_system-test-workflow_A",
+        "ci_system-test-workflow_B",
+        "ci_system-test-workflow_C",
+        "ci_system-test-workflow_D",
+        "sag_system-test-workflow",
+        "sag_system-test-offsite",
+    ]
 
     if download_reports:
         folders.append("*.zip")
         folders.append("*.json")
 
     for folder in folders:
-        cmd = "rm -rf "+ folder
+        cmd = "rm -rf " + folder
         print(cmd)
         sub = subprocess.run(cmd, shell=True)
 
-        #sub.check_returncode()
+        # sub.check_returncode()
         if sub.returncode != 0:
             print("Warning command failed:", cmd)
+
 
 def download(workflow, repo, simulate=False):
     # Download and unzip results from test workflows
@@ -71,58 +131,58 @@ def download(workflow, repo, simulate=False):
     # ./download_workflow_artifact.py abelikt system-test-workflow_B.yml -o ci_system-test-workflow_B;
     # unzip -q -o -d ci_system-test-workflow_B ci_system-test-workflow_B.zip
 
-    name = repo +"_"+ workflow.replace(".yml","")
-    filename = name+".zip"
+    name = repo + "_" + workflow.replace(".yml", "")
+    filename = name + ".zip"
 
     print(name)
-    cmd=f"./download_workflow_artifact.py {repo} {workflow} -o {name}"
+    cmd = f"./download_workflow_artifact.py {repo} {workflow} -o {name}"
     print(cmd)
 
     if not simulate:
-        sub=subprocess.run(cmd, shell=True)
+        sub = subprocess.run(cmd, shell=True)
         sub.check_returncode()
 
     assert os.path.exists(filename)
 
-    cmd =f"unzip -q -o -d {name} {filename}"
+    cmd = f"unzip -q -o -d {name} {filename}"
     print(cmd)
 
-    sub=subprocess.run(cmd, shell=True)
+    sub = subprocess.run(cmd, shell=True)
     sub.check_returncode()
 
 
 def postprocess_runner(runner):
     # Postprocess results
 
-
     # Postporcess results for the onsite runner onsite at Michael
-        workflow = runner ["workflow"]
-        repo = runner ["repo"]
-        tests = runner["tests"]
+    workflow = runner["workflow"]
+    repo = runner["repo"]
+    tests = runner["tests"]
 
-        print(f"Processing: {workflow} ")
+    print(f"Processing: {workflow} ")
 
-        tags = ["all", "apt", "apama", "docker", "sm", "analytics"]
-        files = ""
+    tags = ["all", "apt", "apama", "docker", "sm", "analytics"]
+    files = ""
 
-        name = repo +"_"+ workflow.replace(".yml","")
+    name = repo + "_" + workflow.replace(".yml", "")
 
-        for tag in tags:
-            if tag in tests:
-                folder = f"{name}/PySys/pysys_junit_xml_{tag}"
-                if os.path.exists(folder):
-                    files += f"{name}/PySys/pysys_junit_xml_{tag}/*.xml "
-                else:
-                    raise SystemError("Folder Expected", folder)
+    for tag in tags:
+        if tag in tests:
+            folder = f"{name}/PySys/pysys_junit_xml_{tag}"
+            if os.path.exists(folder):
+                files += f"{name}/PySys/pysys_junit_xml_{tag}/*.xml "
+            else:
+                raise SystemError("Folder Expected", folder)
 
-        cmd = f"junitparser merge {files} { name }.xml"
+    cmd = f"junitparser merge {files} { name }.xml"
 
-        print(cmd)
+    print(cmd)
 
-        sub=subprocess.run(cmd, shell=True)
-        sub.check_returncode()
+    sub = subprocess.run(cmd, shell=True)
+    sub.check_returncode()
 
-        print(f"junit2html {name}.xml")
+    print(f"junit2html {name}.xml")
+
 
 def postprocess():
 
@@ -169,6 +229,7 @@ def postprocess():
     # Zip everything
     # zip report.zip *.html *.json
 
+
 def main(runners, download_reports=True):
 
     cleanup(download_reports)
@@ -176,16 +237,16 @@ def main(runners, download_reports=True):
     simulate = not download_reports
 
     for key in runners.keys():
-        print( "Key", key, "Repo", runners[key]["repo"] )
-        download( runners[key]["workflow"], runners[key]["repo"], simulate=simulate)
+        print("Key", key, "Repo", runners[key]["repo"])
+        download(runners[key]["workflow"], runners[key]["repo"], simulate=simulate)
 
     print(runners.keys())
 
     for key in runners.keys():
-        postprocess_runner( runners[key] )
+        postprocess_runner(runners[key])
 
     postprocess()
 
-if __name__=="__main__":
-    main(runners_cfg, download_reports=False)
 
+if __name__ == "__main__":
+    main(runners_cfg, download_reports=False)
