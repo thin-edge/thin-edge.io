5c4e15f43e8b23af594a03ddd6b75c2de2ffe9cd
Michael Abel <info@abel-ikt.de>
2022-04-08 10:43:11 +0200

    Checkout build script for local builds from Michaels repo

---
+++ ci/report/build_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Call this file to generate a report locally
+
+set -e
+
+python3 -m venv ~/env-builder
+source ~/env-builder/bin/activate
+pip3 install junitparser
+pip3 install junit2html
+
+./ci/report/report_builder.py --folder ./results abelikt ci_pipeline.yml --download
