thin-edge/thin-edge.io

commit 50097a89079396f08f1302b621c7b5b3cc7f6091

Add send_with_timeout
Sending messages with a timeout ensures that if a plugin takes too long
to accept the message this method eventually returns.

Signed-off-by: Marcel Müller <m.mueller@ifm.com>

crates/core/tedge_api/src/address.rs
@@ -1,6 +1,6 @@
 use std::{marker::PhantomData, time::Duration};
 
-use tokio::sync::mpsc::error::TrySendError;
+use tokio::sync::mpsc::error::{SendTimeoutError, TrySendError};
 
 use crate::plugin::Message;
 
@@ -121,6 +121,41 @@ impl<RB: ReceiverBundle> Address<RB> {
             })
             .map_err(|msg| match msg {
                 TrySendError::Full(data) | TrySendError::Closed(data) => {
+                    *data.data.downcast::<M>().unwrap()
+                }
+            })?;
+
+        Ok(ReplyReceiver {
+            _pd: PhantomData,
+            reply_recv: receiver,
+        })
+    }
+
+    /// Send a message `M` to the address represented by the instance of this struct and wait for
+    /// them to accept it or timeout
+    ///
+    /// This method is identical to [`send_and_wait`] except a timeout can be specified after which
+    /// trying to send is aborted.
+    ///
+    /// If you do not wish to wait for a timeout see [`try_send`]
+    pub async fn send_with_timeout<M: Message>(
+        &self,
+        msg: M,
+        timeout: Duration,
+    ) -> Result<ReplyReceiver<M::Reply>, M> {
+        let (sender, receiver) = tokio::sync::oneshot::channel();
+
+        self.sender
+            .send_timeout(
+                InternalMessage {
+                    data: Box::new(msg),
+                    reply_sender: sender,
+                },
+                timeout,
+            )
+            .await
+            .map_err(|msg| match msg {
+                SendTimeoutError::Timeout(data) | SendTimeoutError::Closed(data) => {
                     *data.data.downcast::<M>().unwrap()
                 }
             })?;

commit a398c9783b54892aa1f625685beb7f153d7f7e6d

Fix doc comment references to send_* methods
Signed-off-by: Marcel Müller <m.mueller@ifm.com>
Fixes: 50097a89 ("Add send_with_timeout")
Fixes: 449d2f3b ("Add try_send method")
Fixes: 86530000 ("Rename send to send_and_wait")

crates/core/tedge_api/src/address.rs
@@ -134,10 +134,10 @@ impl<RB: ReceiverBundle> Address<RB> {
     /// Send a message `M` to the address represented by the instance of this struct and wait for
     /// them to accept it or timeout
     ///
-    /// This method is identical to [`send_and_wait`] except a timeout can be specified after which
+    /// This method is identical to [`Address::send_and_wait`] except a timeout can be specified after which
     /// trying to send is aborted.
     ///
-    /// If you do not wish to wait for a timeout see [`try_send`]
+    /// If you do not wish to wait for a timeout see [`Address::try_send`]
     pub async fn send_with_timeout<M: Message>(
         &self,
         msg: M,
@@ -168,7 +168,7 @@ impl<RB: ReceiverBundle> Address<RB> {
 }
 
 #[derive(Debug)]
-/// Listener that allows one to wait for a reply as sent through [`Address::send`]
+/// Listener that allows one to wait for a reply as sent through [`Address::send_and_wait`]
 pub struct ReplyReceiver<M> {
     _pd: PhantomData<fn(M)>,
     reply_recv: tokio::sync::oneshot::Receiver<AnySendBox>,