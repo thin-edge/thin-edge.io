86b8b9578dbac9da5a3094d2ec9904aba8f68382
PradeepKiruvale <pradeepkumar.kj@softwareag.com>
2022-04-28 16:00:22 +0530

    update tedge update script (#1113)
    Signed-off-by: Pradeep Kumar K J <pradeepkumar.kj@softwareag.com>

--- configuration/contrib/tedge_upgrade/upgrade_tedge_0.6.sh
+++ configuration/contrib/tedge_upgrade/upgrade_tedge_0.6.sh
@@ -4,7 +4,10 @@
 # as there is only `tedge` user exists.
 
 sudo chown tedge:tedge /etc/tedge/operations/c8y/c8y_*
+sudo chown tedge:tedge /etc/tedge/operations/az
 sudo chown tedge:tedge /etc/tedge/.agent
 sudo chown tedge:tedge /var/log/tedge/agent
 sudo chown tedge:tedge /run/lock/tedge_agent.lock
 sudo chown tedge:tedge /run/lock/tedge-mapper-c8y.lock
+sudo chown tedge:tedge /run/lock/tedge-mapper-az.lock
+sudo chown tedge:tedge /run/lock/tedge-mapper-collectd.lock
