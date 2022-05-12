8f4a0f5ecffbc5367ad557a2984e158436e76f2e
Didier Wenzek <didier.wenzek@free.fr>
2022-05-12 17:22:02 +0100

    Add producer and recipient modules
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge_actors/src/lib.rs
+++ crates/core/tedge_actors/src/lib.rs
@@ -1,11 +1,15 @@
 mod actor;
 mod error;
 mod message;
+mod producer;
+mod recipient;
 mod runtime;
 
 pub use actor::*;
 pub use error::*;
 pub use message::*;
+pub use producer::*;
+pub use recipient::*;
 pub use runtime::*;
 
 #[cfg(test)]
---
+++ crates/core/tedge_actors/src/producer.rs
@@ -0,0 +1,31 @@
+use crate::{Message, Recipient, RuntimeError};
+use async_trait::async_trait;
+use std::fmt::Debug;
+
+/// A source of messages
+#[async_trait]
+pub trait Producer<M: Message> {
+    /// Produce the messages of this source sending them to the given recipient
+    async fn produce_messages(self, output: impl Recipient<M>) -> Result<(), RuntimeError>;
+}
+
+/// Akin to `/dev/null`
+///
+/// - Produce no messages
+/// - Consume any message, silently dropping them
+#[derive(Clone, Debug)]
+pub struct DevNull;
+
+#[async_trait]
+impl<M: Message> Producer<M> for DevNull {
+    async fn produce_messages(self, _output: impl Recipient<M>) -> Result<(), RuntimeError> {
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl<M: Message> Recipient<M> for DevNull {
+    async fn send_message(&mut self, _message: M) -> Result<(), RuntimeError> {
+        Ok(())
+    }
+}
