51928a9fbb32c878659d7120cef148d35bd04f05
initard <solo@softwareag.com>
2022-05-24 14:07:26 +0100

    bugfix - fixing update failed status
    Signed-off-by: initard <solo@softwareag.com>

--- plugins/c8y_log_plugin/src/error.rs
+++ plugins/c8y_log_plugin/src/error.rs
@@ -5,4 +5,21 @@ pub enum LogRetrievalError {
 
     #[error(transparent)]
     FromConfigSetting(#[from] tedge_config::ConfigSettingError),
+
+    #[error(transparent)]
+    FromStdIo(#[from] std::io::Error),
+
+    #[error(transparent)]
+    FromGlobPatternError(#[from] glob::PatternError),
+
+    #[error(transparent)]
+    FromGlobError(#[from] glob::GlobError),
+
+    // NOTE: `MaxLines` is not a client-facing error. It is used
+    // to break out of `read_log_content`.
+    #[error("Log file has maximum number of lines.")]
+    MaxLines,
+
+    #[error("No logs available for log type: {log_type}. Hint: is your `path` key correct?")]
+    NoLogsAvailableForType { log_type: String },
 }
