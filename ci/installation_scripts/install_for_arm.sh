119add84be395e87abc9854673539058710e94b3
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-29 16:55:45 +0200

    Port installation step to bash script for arm
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

---
+++ ci/installation_scripts/install_for_arm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+PKG_DIR=$1
+
+# Load the package list as $EXTERNAL_ARM_PACKAGES, $RELEASE_PACKAGES
+source ./ci/package_list.sh
+
+# Install pre-required packages
+sudo apt-get --assume-yes install $(echo "${EXTERNAL_ARM_PACKAGES[*]}")
+
+# Install thin-edge packages
+for PACKAGE in "${RELEASE_PACKAGES[@]}"
+do
+    sudo dpkg -i ./"$PKG_DIR"/"$PACKAGE"_0.*_armhf.deb
+done
+
+# Configure collectd
+sudo cp "/etc/tedge/contrib/collectd/collectd.conf" "/etc/collectd/collectd.conf"
+
+# Change downloaded binaries to executable for testing
+chmod +x /home/pi/examples/sawtooth_publisher
+chmod +x /home/pi/tedge_dummy_plugin/tedge_dummy_plugin
