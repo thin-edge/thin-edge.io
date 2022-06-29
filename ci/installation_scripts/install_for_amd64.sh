406729d928879bc529b72e71c55ae5f809f43643
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-29 16:00:55 +0200

    Port installation step to bash script for amd64
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

---
+++ ci/installation_scripts/install_for_amd64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+PKG_DIR=$1
+
+# Load the package list as $EXTERNAL_AMD64_PACKAGES and $RELEASE_PACKAGES
+source ./ci/package_list.sh
+
+# Install pre-required packages
+sudo apt-get --assume-yes install $(echo "${EXTERNAL_AMD64_PACKAGES[*]}")
+
+# Install thin-edge packages
+for PACKAGE in "${RELEASE_PACKAGES[@]}"
+do
+    sudo dpkg -i ./"$PKG_DIR"/"$PACKAGE"_0.*_amd64.deb
+done
