46afe7fa9f0045a0dba8ebe6f8a9d2608275c818
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2022-06-29 16:53:10 +0200

    Port clean up step to bash script for RPis
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

---
+++ ci/installation_scripts/clean_up_rpi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# Stop services
+sudo systemctl stop tedge-mapper-collectd
+sudo tedge disconnect c8y
+sudo tedge disconnect az
+sudo systemctl stop apama
+
+# Load the release package list as $RELEASE_PACKAGES
+source ./ci/package_list.sh
+
+# Purge packages
+sudo apt --assume-yes purge $(echo "${RELEASE_PACKAGES[*]}")
+sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge $(echo "${EXTERNAL_ARM_PACKAGES[*]}")
