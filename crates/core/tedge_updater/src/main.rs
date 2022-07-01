759fac2648f7252cbffbeb5135e0b5ccf45a89c0
Christoph Stoidner <christoph.stoidner@softwareag.com>
2022-07-01 20:19:48 +0100

    fixed accidentally added bridge start/stop

--- crates/core/tedge_updater/src/main.rs
+++ crates/core/tedge_updater/src/main.rs
@@ -5,6 +5,8 @@ use std::{
     path::{Path, PathBuf},
     process::{Command, ExitStatus, Stdio},
 };
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 use tedge_config::DEFAULT_TEDGE_CONFIG_PATH;
 
@@ -33,7 +35,6 @@ fn exit_with_error<T>(code: i32, message: &str) -> T {
 fn run(operation: PluginOp) -> Result<i32, anyhow::Error> {
     match operation {
         PluginOp::UpdateList { plugin_name } => {
-            let mut clouds: Vec<String> = Vec::new();
             let mut failed = false;
 
             let mut buffer = String::new();
@@ -47,81 +48,20 @@ fn run(operation: PluginOp) -> Result<i32, anyhow::Error> {
                 "",
             );
 
-            // check for existing bridges
-            // TODO: maybe better to have a tedge command as "tedge connected list", as here trying
-            //       to interpret files in mosquitto conf folder.
-            const TEDGE_BRIDGE_CONF_DIR_PATH: &str = "mosquitto-conf"; // NOTE: stolen from tedge/src/cli/connect/command.rs
-            const TEDGE_BRIDGE_CONF_POSTFIX: &str = "-bridge.conf"; // TODO: definition is just made here, might be better part of tedge CLI.
+            // exec plugin and forward STDIN
+            println!("Executing: {} update-list", plugin_name);
 
-            let config_location = DEFAULT_TEDGE_CONFIG_PATH; // NOTE: hardcoded here, maybe allow to set with option "--config-dir"
-
-            let bridge_cfg_dir = Path::new(config_location).join(TEDGE_BRIDGE_CONF_DIR_PATH);
-            let walker = globwalk::GlobWalkerBuilder::from_patterns(
-                bridge_cfg_dir,
-                &[format!("*{}", TEDGE_BRIDGE_CONF_POSTFIX)],
-            )
-            .max_depth(1)
-            .build()
-            .unwrap_or_else(|e: globwalk::GlobError| {
-                exit_with_error(2, format!("Cannot build globwalk, error: '{}'", e).as_str())
-            })
-            .into_iter()
-            .filter_map(Result::ok);
-
-            for img in walker {
-                let cloud_name = img
-                    .path()
-                    .file_name()
-                    .unwrap_or_else(|| {
-                        exit_with_error(2, "Cannot read bridge-config filename from folder.")
-                    })
-                    .to_str()
-                    .unwrap_or_else(|| {
-                        exit_with_error(2, "Cannot handle bridge-config filename as string.")
-                    })
-                    .strip_suffix(TEDGE_BRIDGE_CONF_POSTFIX)
-                    .unwrap_or_else(|| {
-                        exit_with_error(2, "Cannot strip suffix from bridge-config filename.")
-                    });
-                clouds.push(cloud_name.to_string());
-                println!("Found cloud connection: {}", cloud_name);
-            }
-
-            // disconnect all clouds
-            for cloud in clouds.clone().into_iter() {
-                println!("Disconnecting {}", cloud);
-                match run_tedge_cli(&format!("disconnect {}", cloud)) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        eprintln!("Error occurred when calling tedge CLI: {}", e);
-                        failed = true; // one cloud failed to disconnect, so fail entire update
-                    }
-                }
-            }
-            // NOTE: Here is point-of-no-return. SM Agent was stopped
-
-            let exitcode = if failed {
-                // disconnect has completely or partially failed, so avoid to start update,
-                // instead report error to agent
-                eprintln!("Error on disconnect. At least one cloud could not be disconnected, so skipping update.");
-                2
-            } else {
-                // exec plugin and forward STDIN
-                println!("Executing: {} update-list", plugin_name);
-
-                let mut status = Command::new(format!("/etc/tedge/sm-plugins/{filename}"))
+            let mut status = Command::new(format!("/etc/tedge/sm-plugins/{filename}"))
                     .args(vec!["update-list"])
                     .stdin(std::process::Stdio::piped())
                     .spawn()?;
 
-                dbg!(&status);
-                let cmd_stdin = status.stdin.as_mut().unwrap();
-                let _ = cmd_stdin.write_all(buffer.as_bytes());
+            dbg!(&status);
+            let cmd_stdin = status.stdin.as_mut().unwrap();
+            let _ = cmd_stdin.write_all(buffer.as_bytes());
+            let status = status.wait();
 
-                let status = status.wait();
-
-                status_to_exitcode(status)
-            };
+            let exitcode = status_to_exitcode(status);
 
             match store_exitcode(exitcode) {
                 Err(e) => eprintln!(
@@ -216,7 +156,6 @@ fn status_to_exitcode(status: Result<ExitStatus, std::io::Error>) -> i32 {
 
 fn main() {
     let apt = AptCli::parse();
-
     let exitcode = run(apt.operation).unwrap();
     std::process::exit(exitcode);
 }
