1be984b4f03effdaaf64fb4fca2224c42d75b469
Matthias Beyer <matthias.beyer@ifm.com>
2022-08-06 09:13:55 +0200

    Fix: Assign and export variable seperately
    This fixes shellcheck SC2155
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- ci/ci_smoke_test_c8y.sh
+++ ci/ci_smoke_test_c8y.sh
@@ -77,7 +77,8 @@ PATH=$PATH:/usr/sbin
 python3 -m venv ~/env-c8y-api
 source ~/env-c8y-api/bin/activate
 pip3 install c8y-api retry-decorator
-export C8YDEVICEID=$(python3 ./ci/find_device_id.py --tenant "$C8YTENANT" --user "$C8YUSERNAME" --device "$C8YDEVICE" --url "$C8YURL")
+C8YDEVICEID=$(python3 ./ci/find_device_id.py --tenant "$C8YTENANT" --user "$C8YUSERNAME" --device "$C8YDEVICE" --url "$C8YURL")
+export C8YDEVICEID
 
 # after calling the script, the ID should be a numeric value
 if [[ "$C8YDEVICEID" =~ ^[0-9]+$ ]]; then
