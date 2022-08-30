thin-edge/thin-edge.io

commit c147d91f6aedfcef6574fd105557e6661220ee3d

Fix: Add missing quotes
This fixes shellcheck SC2046

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/install_for_arm.sh
@@ -8,7 +8,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install $(echo "${EXTERNAL_ARM_PACKAGES[*]}")
+sudo apt-get --assume-yes install "$(echo "${EXTERNAL_ARM_PACKAGES[*]}")"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"

commit 78d98e94d85c1130cb4b46813750dddfdc2434b5

Fix: Use @ to expand array instead of *
Fixes: c147d91f6aedfcef6574fd105557e6661220ee3d ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/install_for_arm.sh
@@ -9,7 +9,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install "${EXTERNAL_ARM_PACKAGES[*]}"
+sudo apt-get --assume-yes install "${EXTERNAL_ARM_PACKAGES[@]}"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"