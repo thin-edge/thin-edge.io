thin-edge/thin-edge.io

commit 12912211adc50d6ea99d4e2d1c4c1bf71a3cb5c0

Fix: Add missing quotes
This fixes shellcheck SC2046

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/install_for_amd64.sh
@@ -8,7 +8,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install $(echo "${EXTERNAL_AMD64_PACKAGES[*]}")
+sudo apt-get --assume-yes install "$(echo "${EXTERNAL_AMD64_PACKAGES[*]}")"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"

commit 3d16c341657564b639c5fb282aab4e94f38db6a8

Fix: Use @ to expand array instead of *
Fixes: 12912211adc50d6ea99d4e2d1c4c1bf71a3cb5c0 ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/install_for_amd64.sh
@@ -9,7 +9,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install "${EXTERNAL_AMD64_PACKAGES[*]}"
+sudo apt-get --assume-yes install "${EXTERNAL_AMD64_PACKAGES[@]}"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"