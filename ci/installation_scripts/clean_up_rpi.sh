thin-edge/thin-edge.io

commit b3f1da8cf607cb1c6668d30ae1121a19305cf9e9

Fix: Add missing quotes
This fixes shellcheck SC2046

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/clean_up_rpi.sh
@@ -10,5 +10,5 @@ sudo systemctl stop apama
 source ./ci/package_list.sh
 
 # Purge packages
-sudo apt --assume-yes purge $(echo "${RELEASE_PACKAGES[*]}")
-sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge $(echo "${EXTERNAL_ARM_PACKAGES[*]}")
+sudo apt --assume-yes purge "$(echo "${RELEASE_PACKAGES[*]}")"
+sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "$(echo "${EXTERNAL_ARM_PACKAGES[*]}")"

commit ce7c8ed45f57b2f0c8cf1eb6817cee756bde6f6b

Fix: Use @ to expand array instead of *
Fixes: b3f1da8cf607cb1c6668d30ae1121a19305cf9e9 ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/clean_up_rpi.sh
@@ -11,5 +11,5 @@ sudo systemctl stop apama
 source ./ci/package_list.sh
 
 # Purge packages
-sudo apt --assume-yes purge "${RELEASE_PACKAGES[*]}"
-sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "${EXTERNAL_ARM_PACKAGES[*]}"
+sudo apt --assume-yes purge "${RELEASE_PACKAGES[@]}"
+sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "${EXTERNAL_ARM_PACKAGES[@]}"