thin-edge/thin-edge.io

commit b3f1da8cf607cb1c6668d30ae1121a19305cf9e9

Fix: Add missing quotes
This fixes shellcheck SC2046

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/clean_up_rpi.sh
@@ -10,5 +10,5 @@ sudo systemctl stop apama
 source ./ci/package_list.sh
 
 # Purge packages
-sudo apt --assume-yes purge $(echo "${RELEASE_PACKAGES[*]}")
-sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge $(echo "${EXTERNAL_ARM_PACKAGES[*]}")
+sudo apt --assume-yes purge "$(echo "${RELEASE_PACKAGES[*]}")"
+sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "$(echo "${EXTERNAL_ARM_PACKAGES[*]}")"

commit 1f898e1459a1ab451aac7ef09bd1bf0585172124

Fix install array expansion (#1375)
* Fix: Use @ to expand array instead of *

Fixes: b3f1da8cf607cb1c6668d30ae1121a19305cf9e9 ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

* Fix: Use @ to expand array instead of *

Fixes: 12912211adc50d6ea99d4e2d1c4c1bf71a3cb5c0 ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

* Fix: Use @ to expand array instead of *

Fixes: c147d91f6aedfcef6574fd105557e6661220ee3d ("Fix: Add missing quotes")
Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

ci/installation_scripts/clean_up_rpi.sh
@@ -11,5 +11,5 @@ sudo systemctl stop apama
 source ./ci/package_list.sh
 
 # Purge packages
-sudo apt --assume-yes purge "${RELEASE_PACKAGES[*]}"
-sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "${EXTERNAL_ARM_PACKAGES[*]}"
+sudo apt --assume-yes purge "${RELEASE_PACKAGES[@]}"
+sudo DEBIAN_FRONTEND=noninteractive apt --assume-yes purge "${EXTERNAL_ARM_PACKAGES[@]}"

ci/installation_scripts/install_for_amd64.sh
@@ -9,7 +9,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install "${EXTERNAL_AMD64_PACKAGES[*]}"
+sudo apt-get --assume-yes install "${EXTERNAL_AMD64_PACKAGES[@]}"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"

ci/installation_scripts/install_for_arm.sh
@@ -9,7 +9,7 @@ PKG_DIR=$1
 source ./ci/package_list.sh
 
 # Install pre-required packages
-sudo apt-get --assume-yes install "${EXTERNAL_ARM_PACKAGES[*]}"
+sudo apt-get --assume-yes install "${EXTERNAL_ARM_PACKAGES[@]}"
 
 # Install thin-edge packages
 for PACKAGE in "${RELEASE_PACKAGES[@]}"