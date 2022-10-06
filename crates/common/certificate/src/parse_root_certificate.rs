d2a1a1a7d0e43b3fb493867adac5523d10b7576d
Didier Wenzek <didier.wenzek@free.fr>
2022-10-06 19:32:43 +0200

    Fix loading of root certificates
    Certificate under sub-directories must be loaded too.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/common/certificate/src/parse_root_certificate.rs
+++ crates/common/certificate/src/parse_root_certificate.rs
@@ -21,14 +21,31 @@ pub fn create_tls_config(
 
 fn new_root_store(cert_path: PathBuf) -> Result<RootCertStore, CertificateError> {
     let mut root_store = RootCertStore::empty();
-    if fs::metadata(&cert_path)?.is_dir() {
-        for file_entry in fs::read_dir(cert_path)? {
-            add_root_cert(&mut root_store, file_entry?.path())?;
-        }
-    } else {
-        add_root_cert(&mut root_store, cert_path)?;
-    }
+    rec_add_root_cert(&mut root_store, cert_path);
     Ok(root_store)
+}
+
+fn rec_add_root_cert(root_store: &mut RootCertStore, cert_path: PathBuf) {
+    if let Err(err) = try_rec_add_root_cert(root_store, cert_path.clone()) {
+        eprintln!("Ignoring certificates in {:?} due to: {}", cert_path, err)
+    }
+}
+
+fn try_rec_add_root_cert(
+    root_store: &mut RootCertStore,
+    cert_path: PathBuf,
+) -> Result<(), CertificateError> {
+    if fs::metadata(&cert_path)?.is_dir() {
+        for file_entry in fs::read_dir(cert_path)?.flatten() {
+            rec_add_root_cert(root_store, file_entry.path());
+        }
+    } else if let Err(err) = add_root_cert(root_store, cert_path.clone()) {
+        eprintln!(
+            "Ignoring certificates in file {:?} due to: {}",
+            cert_path, err
+        )
+    }
+    Ok(())
 }
 
 fn add_root_cert(
@@ -38,7 +55,10 @@ fn add_root_cert(
     let certificates = read_cert_chain(cert_path.clone())?;
     for certificate in certificates.iter() {
         if let Err(err) = root_store.add(certificate) {
-            eprintln!("Ignore invalid certificate {:?} : {}", cert_path, err)
+            eprintln!(
+                "Ignoring certificate in file {:?} due to: {}",
+                cert_path, err
+            )
         }
     }
     Ok(())
@@ -78,7 +98,7 @@ pub fn read_cert_chain(cert_file: PathBuf) -> Result<Vec<Certificate>, Certifica
 mod tests {
     use super::*;
     use std::io::Write;
-    use tempfile::NamedTempFile;
+    use tempfile::{NamedTempFile, TempDir};
 
     #[test]
     fn parse_private_rsa_key() {
@@ -132,5 +152,54 @@ mod tests {
         temp_file.write_all(key.as_bytes()).unwrap();
         let err = read_pvt_key(temp_file.path().into()).unwrap_err();
         assert!(matches!(err, CertificateError::UnknownPrivateKeyFormat));
+    }
+
+    #[test]
+    fn an_empty_directory_contains_no_root_certificate() {
+        let temp_dir = TempDir::new().unwrap();
+
+        let root_certs = new_root_store(temp_dir.into_path()).unwrap();
+        assert!(root_certs.is_empty());
+    }
+
+    #[test]
+    fn all_certificates_of_all_chains_are_root_certificates() {
+        let temp_dir = TempDir::new().unwrap();
+
+        // Add a first chain with 1 certificate
+        let mut cert_1 = File::create(temp_dir.path().join("cert_1")).unwrap();
+        cert_1
+            .write_all(include_str!("./test_root_cert_1.txt").as_bytes())
+            .unwrap();
+
+        // Add a second chain with 2 certificates
+        let mut cert_2 = File::create(temp_dir.path().join("cert_2")).unwrap();
+        cert_2
+            .write_all(include_str!("./test_root_cert_2.txt").as_bytes())
+            .unwrap();
+
+        let root_certs = new_root_store(temp_dir.path().to_path_buf()).unwrap();
+        assert_eq!(root_certs.len(), 3);
+    }
+
+    #[test]
+    fn all_certificates_are_loaded_even_under_sub_directories() {
+        let temp_dir = TempDir::new().unwrap();
+
+        // Add a first chain with 1 certificate
+        let mut cert_1 = File::create(temp_dir.path().join("cert_1")).unwrap();
+        cert_1
+            .write_all(include_str!("./test_root_cert_1.txt").as_bytes())
+            .unwrap();
+
+        // Add a second chain with 2 certificates in a sub directory
+        fs::create_dir(temp_dir.path().join("sub_certs")).unwrap();
+        let mut cert_2 = File::create(temp_dir.path().join("sub_certs/cert_2")).unwrap();
+        cert_2
+            .write_all(include_str!("./test_root_cert_2.txt").as_bytes())
+            .unwrap();
+
+        let root_certs = new_root_store(temp_dir.path().to_path_buf()).unwrap();
+        assert_eq!(root_certs.len(), 3);
     }
 }
