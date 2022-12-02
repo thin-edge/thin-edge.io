bf07b2ff53b536980a45169b0ac517066736b6d7
Marco Stoffel <marco.stoffel@softwareag.com>
2022-12-02 12:57:54 +0000

    First Working draft

--- configure-c8y.sh
+++ configure-c8y.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+
+if [ `id -u` != 0 ] ; 
+  then printf "Please run as sudo or root!\n"
+  exit
+fi
 printf "\n*********************************************"
 printf "\nThis script lets you setup a thinEdge connection to cumulocity IoT.\nWe assume that the installation was successful and all the necessary services are running.
 Just follow the instructions....."
 printf "\n*********************************************"
 
-
 printf "\n\n"
 read  -p 'Enter the Cumulocity tenant url e.g. [your-tenant.cumulocity.com]: ' tenant_url
-tenant_url=${tenant_url:-mstoffel.eu-latest.cumulocity.com}
+tenant_url=${tenant_url:-your-tenant.cumulocity.com}
 tedge config set c8y.url $tenant_url
 
 printf "\n\n"
-read -p 'Enter the path to your root certificates [/etc/ssl/certs]: ' path_root_cert
+read -p 'Enter the path to your root certificates. On most Linux systems that is [/etc/ssl/certs]: ' path_root_cert
 path_root_cert=${path_root_cert:-/etc/ssl/certs}
 tedge config set c8y.root.cert.path $path_root_cert
 
@@ -23,7 +27,7 @@ device_id=${device_id:-my-device}
     tedge cert create --device-id $device_id
 } ||
 printf "\n\n"
-read -r -p "\n\nWould you like to remove the old cert and create a new one? (y)Yes/(n)No: " response
+read -r -p "Would you like to remove the old cert and create a new one? (y)Yes/(n)No: " response
 case $response in
     [yY]) 
         printf "\nRemoving cert...."
@@ -31,7 +35,7 @@ case $response in
         tedge cert create --device-id $device_id
         ;;
     *) 
-        printf "\nOk bye, enjoy thingEdge.io!!!" 
+        printf "\nOk bye, enjoy thingEdge.io!!!\n" 
         exit 
         ;;    
 esac
@@ -41,15 +45,15 @@ printf "\n\nYour current configuration:\n"
 tedge config list
 
 
-printf "\n\n"
-read -p "\n\nEnter Cumulocity user [your@email.com]: " c8y_Username
-c8y_Username=${c8y_Username:-your@email.com}
+RET=1
+until [ ${RET} -eq 0 ]; do
+    printf "\n\n"
+    read -p "Enter Cumulocity user [your@email.com]: " c8y_Username
+    c8y_Username=${c8y_Username:-your@email.com}
+    tedge cert upload c8y --user $c8y_Username
+    RET=$?
+done
 
-#read -ps 'Enter Cumulocity password [secret]: ' c8y_Password
-#c8y_Password=${c8y_Username:-secret}
-
-printf "\n\nUsing $c8y_Username to authenticate...."
-tedge cert upload c8y --user $c8y_Username
 
 printf '\nCreated Certificate:'
 sudo tedge cert show
@@ -62,28 +66,28 @@ sudo tedge cert show
 
 
 printf "\n\n"
-read -r -p "\nLooks like tedge is already connected to Cumulocity. Would you like to reconnect? (y)Yes/(n)No: " response
+read -r -p "Looks like tedge is already connected to Cumulocity. Would you like to reconnect? (y)Yes/(n)No: " response
 case $response in
     [yY])  
         tedge disconnect c8y
         tedge connect c8y
         ;;
     *)  
-        printf "\nOk bye, enjoy thingEdge.io!!!" 
+        printf "\nOk bye, enjoy thingEdge.io!!!\n" 
         exit 
         ;;    
 esac
 
 printf "\n\n"
-read -r -p $'Would you like to send a test measurement to your new device? (y)Yes/(n)No: ' response
+read -r -p 'Would you like to send a test measurement to your new device? (y)Yes/(n)No: ' response
 case $response in
     [yY])  
         printf "\nCreating a 20 degree Temperature Measurement...."
         tedge mqtt pub c8y/s/us 211,20
-        printf "\nYou should now see a new measurement in your device. Enjoy...."
+        printf "\nYou should now see a new measurement in your device $device_id. Enjoy....\n"
         ;;
     *)  
-        printf "\nOk bye, enjoy thingEdge.io!!!" 
+        printf "\nOk bye, enjoy thingEdge.io!!!\n" 
         exit 
-        ;;    
+          
 esac
