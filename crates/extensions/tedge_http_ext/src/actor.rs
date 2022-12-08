1589517984754a1db90153ef71cd37dbf533da2a
Didier Wenzek <didier.wenzek@free.fr>
2022-12-08 22:38:45 +0100

    Combine mailbox and peer senders into a single abstraction
    The aim is to implement smarter helpers.
    Here is an example to control concurrency.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/extensions/tedge_http_ext/src/actor.rs
+++ crates/extensions/tedge_http_ext/src/actor.rs
@@ -1,4 +1,4 @@
-use crate::{HttpConfig, HttpError, HttpRequest, HttpResponse};
+use crate::{HttpConfig, HttpError, HttpRequest, HttpResponse, HttpResult};
 use async_trait::async_trait;
 use tedge_actors::{Actor, ChannelError, Mailbox, Recipient};
 
@@ -22,23 +22,100 @@ impl Actor for HttpActor {
 
     async fn run(
         self,
-        mut requests: Self::Mailbox,
-        mut client: Self::Peers,
+        requests: Self::Mailbox,
+        responses: Self::Peers,
     ) -> Result<(), ChannelError> {
-        while let Some((client_id, request)) = requests.next().await {
-            // Forward the request to the http server
-            let request = request.into();
+        let mut peers = HttpPeers::new(4, requests, responses);
 
-            // Await for a response
-            let response = match self.client.execute(request).await {
-                Ok(res) => Ok(res.into()),
-                Err(err) => Err(err.into()),
-            };
+        while let Some((client_id, request)) = peers.next_request().await {
+            let request = request.into();
+            let client = self.client.clone();
+
+            // Spawn the request
+            let pending_result = tokio::spawn(async move {
+                let response = match client.execute(request).await {
+                    Ok(res) => Ok(res.into()),
+                    Err(err) => Err(err.into()),
+                };
+                (client_id, response)
+            });
 
             // Send the response back to the client
-            client.send((client_id, response)).await?
+            peers.send_response_once_done(pending_result);
         }
 
         Ok(())
+    }
+}
+
+type PendingResult = tokio::task::JoinHandle<(usize, HttpResult)>;
+
+struct HttpPeers {
+    /// Max concurrent requests
+    max_concurrency: usize,
+
+    /// Requests received by this actor from its clients
+    requests: Mailbox<(usize, HttpRequest)>,
+
+    /// Responses sent by this actor to its clients
+    responses: Recipient<(usize, HttpResult)>,
+
+    /// Pending responses
+    pending_responses: futures::stream::FuturesUnordered<PendingResult>,
+}
+
+use futures::StreamExt;
+
+impl HttpPeers {
+    fn new(
+        max_concurrency: usize,
+        requests: Mailbox<(usize, HttpRequest)>,
+        responses: Recipient<(usize, HttpResult)>,
+    ) -> HttpPeers {
+        HttpPeers {
+            max_concurrency,
+            requests,
+            responses,
+            pending_responses: futures::stream::FuturesUnordered::new(),
+        }
+    }
+
+    async fn next_request(&mut self) -> Option<(usize, HttpRequest)> {
+        self.await_idle_processor().await;
+        loop {
+            tokio::select! {
+                Some(request) = self.requests.next() => {
+                    return Some(request);
+                }
+                Some(result) = self.pending_responses.next() => {
+                    self.send_result(result).await;
+                }
+                else => {
+                    return None
+                }
+            }
+        }
+    }
+
+    async fn await_idle_processor(&mut self) {
+        if self.pending_responses.len() >= self.max_concurrency {
+            if let Some(result) = self.pending_responses.next().await {
+                self.send_result(result).await;
+            }
+        }
+    }
+
+    fn send_response_once_done(&mut self, pending_result: PendingResult) {
+        self.pending_responses.push(pending_result);
+    }
+
+    async fn send_result(&mut self, result: Result<(usize, HttpResult), tokio::task::JoinError>) {
+        if let Ok(response) = result {
+            let _ = self.responses.send(response).await;
+        }
+        // TODO handle error cases:
+        // - cancelled task
+        // - task panics
+        // - send fails
     }
 }
