3e9b085f5f4945e8e9cebd828888daa2ea3774b6
Didier Wenzek <didier.wenzek@free.fr>
2023-01-09 16:54:50 +0100

    Add extension to handle signals
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

---
+++ crates/extensions/tedge_signal_ext/src/lib.rs
@@ -0,0 +1,98 @@
+use async_trait::async_trait;
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::Signals;
+use tedge_actors::Actor;
+use tedge_actors::ActorBuilder;
+use tedge_actors::ChannelError;
+use tedge_actors::DynSender;
+use tedge_actors::MessageBox;
+use tedge_actors::RuntimeAction;
+use tedge_actors::RuntimeError;
+use tedge_actors::RuntimeHandle;
+use tedge_actors::StreamExt;
+
+pub struct SignalActorBuilder;
+
+#[async_trait]
+impl ActorBuilder for SignalActorBuilder {
+    async fn spawn(self, runtime: &mut RuntimeHandle) -> Result<(), RuntimeError> {
+        let message_box = SignalMessageBox::new(runtime.clone());
+        runtime.run(SignalActor, message_box).await
+    }
+}
+
+pub struct SignalActor;
+
+impl SignalActor {
+    pub fn builder() -> SignalActorBuilder {
+        SignalActorBuilder
+    }
+}
+
+#[async_trait]
+impl Actor for SignalActor {
+    type MessageBox = SignalMessageBox;
+
+    fn name(&self) -> &str {
+        "Signal-Handler"
+    }
+
+    async fn run(self, mut messages: Self::MessageBox) -> Result<(), ChannelError> {
+        while let Some(signal) = messages.recv().await {
+            match signal {
+                SIGTERM | SIGINT | SIGQUIT => messages.send(RuntimeAction::Shutdown).await?,
+                _ => unreachable!(),
+            }
+        }
+        Ok(())
+    }
+}
+
+pub struct SignalMessageBox {
+    runtime: RuntimeHandle,
+    signals: Signals,
+}
+
+impl SignalMessageBox {
+    fn new(runtime: RuntimeHandle) -> Self {
+        let signals = Signals::new(&[SIGTERM, SIGINT, SIGQUIT]).unwrap(); // FIXME
+        SignalMessageBox { runtime, signals }
+    }
+}
+
+#[async_trait]
+impl MessageBox for SignalMessageBox {
+    type Input = i32;
+    type Output = RuntimeAction;
+
+    async fn recv(&mut self) -> Option<Self::Input> {
+        self.signals.next().await
+    }
+
+    async fn send(&mut self, message: Self::Output) -> Result<(), ChannelError> {
+        self.log_output(&message);
+        self.runtime.send(message).await
+    }
+
+    fn new_box(
+        _name: &str,
+        _capacity: usize,
+        _output: DynSender<Self::Output>,
+    ) -> (DynSender<Self::Input>, Self) {
+        todo!()
+
+        // FIXME, One needs first impl RuntimeHandle as a regular MessageBox
+    }
+
+    fn turn_logging_on(&mut self, _on: bool) {
+        todo!()
+    }
+
+    fn name(&self) -> &str {
+        "Signal-Handler"
+    }
+
+    fn logging_is_on(&self) -> bool {
+        true
+    }
+}
