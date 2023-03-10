b670f52bf16f3642d7c9d887b3630e5d519aef53
Pradeep Kumar K J <pradeepkumar.kj@softwareag.com>
2023-03-10 17:24:56 +0530

    Health status check actor
    Signed-off-by: Pradeep Kumar K J <pradeepkumar.kj@softwareag.com>

---
+++ crates/extensions/tedge_health_ext/src/actor.rs
@@ -0,0 +1,46 @@
+use async_trait::async_trait;
+use tedge_actors::Actor;
+use tedge_actors::ReceiveMessages;
+use tedge_actors::RuntimeError;
+use tedge_actors::SimpleMessageBox;
+use tedge_mqtt_ext::MqttMessage;
+
+use tedge_api::health::health_status_down_message;
+use tedge_api::health::health_status_up_message;
+
+pub struct HealthMonitorActor {
+    daemon_name: String,
+}
+
+impl HealthMonitorActor {
+    pub fn new(daemon_name: String) -> Self {
+        Self { daemon_name }
+    }
+
+    pub fn up_health_status(&self) -> MqttMessage {
+        health_status_up_message(&self.daemon_name)
+    }
+
+    pub fn down_health_status(&self) -> MqttMessage {
+        health_status_down_message(&self.daemon_name)
+    }
+}
+
+#[async_trait]
+impl Actor for HealthMonitorActor {
+    type MessageBox = SimpleMessageBox<MqttMessage, MqttMessage>;
+
+    fn name(&self) -> &str {
+        "HealthMonitorActor"
+    }
+
+    async fn run(mut self, mut messages: Self::MessageBox) -> Result<(), RuntimeError> {
+        messages.send(self.up_health_status()).await?;
+        while let Some(_message) = messages.recv().await {
+            {
+                messages.send(self.up_health_status()).await?;
+            }
+        }
+        Ok(())
+    }
+}
