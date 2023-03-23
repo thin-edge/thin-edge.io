7399ad76cad3cc941380c11c2da6316771b112c5
Pradeep Kumar K J <pradeepkumar.kj@softwareag.com>
2023-03-23 19:01:00 +0530

    aws actor

--- crates/extensions/aws_mapper_ext/src/mapper.rs
+++ crates/extensions/aws_mapper_ext/src/mapper.rs
@@ -1,20 +1,20 @@
 use std::path::Path;
 
 use crate::converter::AwsConverter;
-use tedge_mapper_core::component::TEdgeComponent;
-use tedge_mapper_core::mapper::create_mapper;
-use tedge_mapper_core::size_threshold::SizeThreshold;
-
+use crate::AwsMapperBuilder;
 use async_trait::async_trait;
-use clock::WallClock;
+use tedge_actors::Runtime;
+use tedge_actors::ServiceConsumer;
 use tedge_config::AwsMapperTimestamp;
 use tedge_config::ConfigSettingAccessor;
 use tedge_config::MqttClientHostSetting;
 use tedge_config::MqttClientPortSetting;
 use tedge_config::TEdgeConfig;
+use tedge_health_ext::HealthMonitorBuilder;
+use tedge_mapper_core::component::TEdgeComponent;
+use tedge_mqtt_ext::MqttActorBuilder;
+use tedge_mqtt_ext::MqttConfig;
 use tracing::info;
-use tracing::info_span;
-use tracing::Instrument;
 
 const AWS_MAPPER_NAME: &str = "tedge-mapper-aws";
 
@@ -50,22 +50,32 @@ impl TEdgeComponent for AwsMapper {
         tedge_config: TEdgeConfig,
         _config_dir: &Path,
     ) -> Result<(), anyhow::Error> {
+        let runtime_events_logger = None;
+        let mut runtime = Runtime::try_new(runtime_events_logger).await?;
+
         let add_timestamp = tedge_config.query(AwsMapperTimestamp)?.is_set();
         let mqtt_port = tedge_config.query(MqttClientPortSetting)?.into();
         let mqtt_host = tedge_config.query(MqttClientHostSetting)?;
-        let clock = Box::new(WallClock);
-        // Quotas at: https://docs.aws.amazon.com/general/latest/gr/iot-core.html#limits_iot
-        let size_threshold = SizeThreshold(128 * 1024);
 
-        let converter = Box::new(AwsConverter::new(add_timestamp, clock, size_threshold));
+        let mqtt_config = MqttConfig::default()
+            .with_host(mqtt_host.clone())
+            .with_port(mqtt_port);
+        let mut mqtt_actor =
+            MqttActorBuilder::new(mqtt_config.clone().with_session_name(self.session_name()));
 
-        let mut mapper = create_mapper(AWS_MAPPER_NAME, mqtt_host, mqtt_port, converter).await?;
+        let aws_actor = AwsMapperBuilder::new(self.session_name(), add_timestamp);
+        let aws_actor = aws_actor.with_connection(&mut mqtt_actor);
 
-        mapper
-            .run(None)
-            .instrument(info_span!(AWS_MAPPER_NAME))
-            .await?;
+        //Instantiate health monitor actor
+        let health_actor = HealthMonitorBuilder::new(self.session_name());
+        mqtt_actor.mqtt_config = health_actor.set_init_and_last_will(mqtt_actor.mqtt_config);
+        let health_actor = health_actor.with_connection(&mut mqtt_actor);
 
+        runtime.spawn(mqtt_actor).await?;
+        runtime.spawn(aws_actor).await?;
+        runtime.spawn(health_actor).await?;
+
+        runtime.run_to_completion().await?;
         Ok(())
     }
 }
