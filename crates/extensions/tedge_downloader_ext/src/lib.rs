bdcd39637c72821cc9f1bf5807a610966079bb16
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2023-03-31 18:06:46 +0200

    Add tedge-downloader-actor
    tedge-downloader-actor receives DownloadRequest, then try to download.
    After a try finish, tedge-downloader-actor responses DownloadResponse,
    which is the result of download attempt.
    
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

---
+++ crates/extensions/tedge_downloader_ext/src/lib.rs
@@ -0,0 +1,5 @@
+mod actor;
+pub use actor::*;
+
+#[cfg(test)]
+mod tests;
---
+++ crates/extensions/tedge_downloader_ext/src/tests.rs
@@ -0,0 +1,76 @@
+use super::*;
+use download::Auth;
+use mockito::mock;
+use std::time::Duration;
+use tedge_actors::ClientMessageBox;
+use tedge_actors::DynError;
+use tedge_test_utils::fs::TempTedgeDir;
+use tokio::time::timeout;
+
+const TEST_TIMEOUT: Duration = Duration::from_secs(5);
+
+#[tokio::test]
+async fn download_without_auth() -> Result<(), DynError> {
+    let ttd = TempTedgeDir::new();
+    let _mock = mock("GET", "/")
+        .with_status(201)
+        .with_header("content-type", "text/plain")
+        .with_body("without auth")
+        .create();
+
+    let target_path = ttd.path().join("downloaded_file");
+    let server_url = mockito::server_url();
+    let download_request = DownloadRequest::new(&server_url, &target_path, None);
+
+    let mut requester = spawn_downloader_actor().await;
+
+    let response = timeout(TEST_TIMEOUT, requester.await_response(download_request))
+        .await?
+        .expect("timeout");
+
+    assert!(response.is_ok());
+    assert_eq!(response.as_ref().unwrap().file_path, target_path.as_path());
+    assert_eq!(response.as_ref().unwrap().url, server_url);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn download_with_auth() -> Result<(), DynError> {
+    let ttd = TempTedgeDir::new();
+    let _mock = mock("GET", "/")
+        .with_status(201)
+        .with_header("content-type", "text/plain")
+        .match_header("authorization", "Bearer token")
+        .with_body("with auth")
+        .create();
+
+    let target_path = ttd.path().join("downloaded_file");
+    let server_url = mockito::server_url();
+    let download_request = DownloadRequest::new(
+        &server_url,
+        &target_path,
+        Some(Auth::Bearer("token".into())),
+    );
+
+    let mut requester = spawn_downloader_actor().await;
+
+    let response = timeout(TEST_TIMEOUT, requester.await_response(download_request))
+        .await?
+        .expect("timeout");
+
+    assert!(response.is_ok());
+    assert_eq!(response.as_ref().unwrap().file_path, target_path.as_path());
+    assert_eq!(response.as_ref().unwrap().url, server_url);
+
+    Ok(())
+}
+
+async fn spawn_downloader_actor() -> ClientMessageBox<DownloadRequest, DownloadResult> {
+    let mut downloader_actor_builder = DownloaderActor::new().builder();
+    let requester = ClientMessageBox::new("DownloadRequester", &mut downloader_actor_builder);
+
+    tokio::spawn(downloader_actor_builder.run());
+
+    requester
+}
