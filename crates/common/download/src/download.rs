b90d9625c96b5c54fac684ee13bb8fe451abe8e8
Marcel Guzik <marcel.guzik@inetum.com>
2023-05-16 17:46:15 +0200

    Remove test for stringified error values
    Signed-off-by: Marcel Guzik <marcel.guzik@inetum.com>

--- crates/common/download/src/download.rs
+++ crates/common/download/src/download.rs
@@ -414,14 +414,12 @@ mod tests {
     use anyhow::bail;
     use mockito::mock;
     use nix::sys::statvfs;
-    use std::error::Error;
     use std::io::Write;
     use std::path::Path;
     use std::path::PathBuf;
     use tempfile::tempdir;
     use tempfile::NamedTempFile;
     use tempfile::TempDir;
-    use test_case::test_case;
     use tokio::io::AsyncBufReadExt;
     use tokio::io::AsyncWriteExt;
     use tokio::io::BufReader;
@@ -597,104 +595,6 @@ mod tests {
                 bail!("failed")
             }
         }
-    }
-
-    #[test_case(
-        200,
-        false,
-        Some("not_a_url"),
-        "builder error: relative URL without a base"
-        ; "builder error"
-    )]
-    #[test_case(
-        200,
-        true,
-        Some("not_a_url"),
-        "builder error: relative URL without a base"
-        ; "builder error with auth"
-    )]
-    #[test_case(
-        200,
-        false,
-        Some("http://not_a_url"),
-        "dns error: failed to lookup address information"
-        ; "dns error"
-    )]
-    #[test_case(
-        200,
-        true,
-        Some("http://not_a_url"),
-        "dns error: failed to lookup address information"
-        ; "dns error with auth"
-    )]
-    #[test_case(
-        404,
-        false,
-        None,
-        "404 Not Found"
-        ; "client error"
-    )]
-    #[test_case(
-        404,
-        true,
-        None,
-        "404 Not Found"
-        ; "client error with auth"
-    )]
-    // #[tokio::test]
-    async fn downloader_download_processing_error(
-        status_code: usize,
-        with_token: bool,
-        url: Option<&str>,
-        expected_err: &str,
-    ) -> anyhow::Result<()> {
-        let name = "test_download";
-        let version = Some("test1".to_string());
-        let target_dir_path = TempDir::new()?;
-
-        // bearer/no bearer setup
-        let _mock1 = {
-            if with_token {
-                mock("GET", "/some_file.txt")
-                    .match_header("authorization", "Bearer token")
-                    .with_status(status_code)
-                    .create()
-            } else {
-                mock("GET", "/some_file.txt")
-                    .with_status(status_code)
-                    .create()
-            }
-        };
-
-        // url/no url setup
-        let url = {
-            if let Some(url) = url {
-                DownloadInfo::new(url)
-            } else {
-                let mut target_url = mockito::server_url();
-                target_url.push_str("/some_file.txt");
-                DownloadInfo::new(&target_url)
-            }
-        };
-
-        // applying token if `with_token` = true
-        let url = {
-            if with_token {
-                url.with_auth(Auth::Bearer(String::from("token")))
-            } else {
-                url
-            }
-        };
-
-        let downloader = Downloader::new_sm(name, &version, target_dir_path.path());
-        match downloader.download(&url).await {
-            Ok(_success) => anyhow::bail!("Expected client error."),
-            Err(err) => {
-                dbg!(err.source());
-                assert!(dbg!(err.to_string()).contains(expected_err));
-            }
-        };
-        Ok(())
     }
 
     #[tokio::test]
