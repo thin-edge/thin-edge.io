6e023058439b8a49778388a66e7a963519437fef
Rina Fujino <18257209+rina23q@users.noreply.github.com>
2023-06-26 15:02:42 +0200

    Fix AzConverter not being killed by serialize parse error
    Signed-off-by: Rina Fujino <18257209+rina23q@users.noreply.github.com>

--- crates/extensions/az_mapper_ext/src/converter.rs
+++ crates/extensions/az_mapper_ext/src/converter.rs
@@ -1,6 +1,8 @@
 use crate::error::ConversionError;
 use crate::size_threshold::SizeThreshold;
 use clock::Clock;
+use log::error;
+use std::convert::Infallible;
 use tedge_actors::Converter;
 use tedge_api::serialize::ThinEdgeJsonSerializer;
 use tedge_mqtt_ext::MqttMessage;
@@ -52,14 +54,8 @@ impl AzureConverter {
             .try_into()
             .unwrap()
     }
-}
 
-impl Converter for AzureConverter {
-    type Error = ConversionError;
-    type Input = MqttMessage;
-    type Output = MqttMessage;
-
-    fn convert(&mut self, input: &Self::Input) -> Result<Vec<Self::Output>, Self::Error> {
+    fn try_convert(&mut self, input: &MqttMessage) -> Result<Vec<MqttMessage>, ConversionError> {
         self.size_threshold.validate(input)?;
         let default_timestamp = self.add_timestamp.then(|| self.clock.now());
         let mut serializer = ThinEdgeJsonSerializer::new_with_timestamp(default_timestamp);
@@ -70,19 +66,37 @@ impl Converter for AzureConverter {
             (MqttMessage::new(&self.mapper_config.out_topic, payload)),
         ])
     }
+
+    fn wrap_errors(
+        &self,
+        messages_or_err: Result<Vec<MqttMessage>, ConversionError>,
+    ) -> Vec<MqttMessage> {
+        messages_or_err.unwrap_or_else(|error| vec![self.new_error_message(error)])
+    }
+
+    fn new_error_message(&self, error: ConversionError) -> MqttMessage {
+        error!("Mapping error: {}", error);
+        MqttMessage::new(&self.mapper_config.errors_topic, error.to_string())
+    }
+}
+
+impl Converter for AzureConverter {
+    type Input = MqttMessage;
+    type Output = MqttMessage;
+    type Error = Infallible;
+
+    fn convert(&mut self, input: &Self::Input) -> Result<Vec<Self::Output>, Self::Error> {
+        let messages_or_err = self.try_convert(input);
+        Ok(self.wrap_errors(messages_or_err))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::converter::AzureConverter;
-    use crate::converter::*;
-
+    use super::*;
     use assert_json_diff::*;
     use assert_matches::*;
-    use clock::Clock;
     use serde_json::json;
-    use tedge_mqtt_ext::MqttMessage;
-    use tedge_mqtt_ext::Topic;
     use time::macros::datetime;
 
     struct TestClock;
@@ -93,22 +107,56 @@ mod tests {
         }
     }
 
-    #[test]
-    fn converting_invalid_json_is_invalid() {
-        let mut converter = AzureConverter::new(false, Box::new(TestClock));
-
-        let input = "This is not Thin Edge JSON";
-        let result = converter.convert(&new_tedge_message(input));
-
-        assert_matches!(result, Err(ConversionError::FromThinEdgeJsonParser(_)))
-    }
-
     fn new_tedge_message(input: &str) -> MqttMessage {
         MqttMessage::new(&Topic::new_unchecked("tedge/measurements"), input)
     }
 
     fn extract_first_message_payload(mut messages: Vec<MqttMessage>) -> String {
         messages.pop().unwrap().payload_str().unwrap().to_string()
+    }
+
+    #[test]
+    fn convert_error() {
+        let mut converter = AzureConverter::new(true, Box::new(TestClock));
+
+        let input = "Invalid JSON";
+
+        let output = converter.convert(&new_tedge_message(input)).unwrap();
+
+        assert_eq!(output.first().unwrap().topic.name, "tedge/errors");
+        assert_eq!(
+            extract_first_message_payload(output),
+            "Invalid JSON: expected value at line 1 column 1: `Invalid JSON\n`"
+        );
+    }
+
+    #[test]
+    fn try_convert_invalid_json_returns_error() {
+        let mut converter = AzureConverter::new(false, Box::new(TestClock));
+
+        let input = "This is not Thin Edge JSON";
+        let result = converter.try_convert(&new_tedge_message(input));
+
+        assert_matches!(result, Err(ConversionError::FromThinEdgeJsonParser(_)))
+    }
+
+    #[test]
+    fn try_convert_exceeding_threshold_returns_error() {
+        let mut converter =
+            AzureConverter::new(false, Box::new(TestClock)).with_threshold(SizeThreshold(1));
+
+        let _topic = "tedge/measurements".to_string();
+        let input = "ABC";
+        let result = converter.try_convert(&new_tedge_message(input));
+
+        assert_matches!(
+            result,
+            Err(ConversionError::SizeThresholdExceeded {
+                topic: _topic,
+                actual_size: 3,
+                threshold: 1
+            })
+        );
     }
 
     #[test]
@@ -201,25 +249,6 @@ mod tests {
             serde_json::from_str::<serde_json::Value>(&extract_first_message_payload(output))
                 .unwrap(),
             expected_output
-        );
-    }
-
-    #[tokio::test]
-    async fn exceeding_threshold_returns_error() {
-        let mut converter =
-            AzureConverter::new(false, Box::new(TestClock)).with_threshold(SizeThreshold(1));
-
-        let _topic = "tedge/measurements".to_string();
-        let input = "ABC";
-        let result = converter.convert(&new_tedge_message(input));
-
-        assert_matches!(
-            result,
-            Err(ConversionError::SizeThresholdExceeded {
-                topic: _topic,
-                actual_size: 3,
-                threshold: 1
-            })
         );
     }
 }
