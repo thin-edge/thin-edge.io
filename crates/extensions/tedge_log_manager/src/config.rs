75b4ee475832609aea406ca13380f495ec9d49b0
Krzysztof Piotrowski <krzysztof.piotrowski@inetum.com>
2023-09-29 08:43:11 +0000

    Parse topic using new MqttSchema
    Signed-off-by: Krzysztof Piotrowski <krzysztof.piotrowski@inetum.com>

--- crates/extensions/tedge_log_manager/src/config.rs
+++ crates/extensions/tedge_log_manager/src/config.rs
@@ -1,6 +1,11 @@
 use std::collections::HashSet;
 use std::path::PathBuf;
-use std::sync::Arc;
+use tedge_api::mqtt_topics::Channel;
+use tedge_api::mqtt_topics::ChannelFilter;
+use tedge_api::mqtt_topics::EntityFilter;
+use tedge_api::mqtt_topics::EntityTopicId;
+use tedge_api::mqtt_topics::MqttSchema;
+use tedge_api::mqtt_topics::OperationType;
 use tedge_config::ReadError;
 use tedge_mqtt_ext::Topic;
 use tedge_mqtt_ext::TopicFilter;
@@ -12,8 +17,6 @@ pub const DEFAULT_PLUGIN_CONFIG_DIR_NAME: &str = "plugins/";
 #[derive(Clone, Debug)]
 pub struct LogManagerConfig {
     pub config_dir: PathBuf,
-    pub mqtt_topic_root: String,
-    pub mqtt_device_topic_id: String,
     pub plugin_config_dir: PathBuf,
     pub plugin_config_path: PathBuf,
     pub logtype_reload_topic: Topic,
@@ -23,40 +26,35 @@ pub struct LogManagerConfig {
 
 pub struct LogManagerOptions {
     pub config_dir: PathBuf,
-    pub mqtt_topic_root: Arc<str>,
-    pub mqtt_device_topic_id: Arc<str>,
+    pub mqtt_schema: MqttSchema,
+    pub mqtt_device_topic_id: EntityTopicId,
 }
 
 impl LogManagerConfig {
     pub fn from_options(cliopts: LogManagerOptions) -> Result<Self, ReadError> {
         let config_dir = cliopts.config_dir;
-        let mqtt_topic_root = cliopts.mqtt_topic_root;
+        let mqtt_schema = cliopts.mqtt_schema;
         let mqtt_device_topic_id = cliopts.mqtt_device_topic_id;
 
         let plugin_config_dir = config_dir.join(DEFAULT_PLUGIN_CONFIG_DIR_NAME);
         let plugin_config_path = plugin_config_dir.join(DEFAULT_PLUGIN_CONFIG_FILE_NAME);
 
-        // TODO: move topic parsing to tedge_api
-        let logtype_reload_topic = Topic::new_unchecked(
-            format!(
-                "{}/{}/cmd/log_upload",
-                mqtt_topic_root, mqtt_device_topic_id
-            )
-            .as_str(),
+        let logtype_reload_topic = mqtt_schema.topic_for(
+            &mqtt_device_topic_id,
+            &Channel::CommandMetadata {
+                operation: OperationType::LogUpload,
+            },
         );
-        let logfile_request_topic = TopicFilter::new_unchecked(
-            format!(
-                "{}/{}/cmd/log_upload/+",
-                mqtt_topic_root, mqtt_device_topic_id
-            )
-            .as_str(),
+
+        let logfile_request_topic = mqtt_schema.topics(
+            EntityFilter::Entity(&mqtt_device_topic_id),
+            ChannelFilter::Command(OperationType::LogUpload),
         );
+
         let current_operations = HashSet::new();
 
         Ok(Self {
             config_dir,
-            mqtt_topic_root: mqtt_topic_root.to_string(),
-            mqtt_device_topic_id: mqtt_device_topic_id.to_string(),
             plugin_config_dir,
             plugin_config_path,
             logtype_reload_topic,
