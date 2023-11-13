eafe1b37f3d2a7ba2d04735db6b78e7312a56288
Marcel Guzik <marcel.guzik@inetum.com>
2023-11-13 12:40:05 +0000

    fix(#2445): add c8y proxy config settings
    This commit adds `c8y.proxy.client.host` and `c8y.proxy.client.port`
    used to connect to running C8y HTTP proxy.

--- crates/extensions/c8y_mapper_ext/src/config.rs
+++ crates/extensions/c8y_mapper_ext/src/config.rs
@@ -3,7 +3,6 @@ use c8y_api::smartrest::error::OperationsError;
 use c8y_api::smartrest::operations::Operations;
 use c8y_api::smartrest::topic::C8yTopic;
 use camino::Utf8PathBuf;
-use std::net::IpAddr;
 use std::path::Path;
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -37,7 +36,7 @@ pub struct C8yMapperConfig {
     pub tedge_http_host: Arc<str>,
     pub topics: TopicFilter,
     pub capabilities: Capabilities,
-    pub auth_proxy_addr: IpAddr,
+    pub auth_proxy_addr: Arc<str>,
     pub auth_proxy_port: u16,
     pub mqtt_schema: MqttSchema,
 }
@@ -56,7 +55,7 @@ impl C8yMapperConfig {
         tedge_http_host: Arc<str>,
         topics: TopicFilter,
         capabilities: Capabilities,
-        auth_proxy_addr: IpAddr,
+        auth_proxy_addr: Arc<str>,
         auth_proxy_port: u16,
         mqtt_schema: MqttSchema,
     ) -> Self {
@@ -97,8 +96,8 @@ impl C8yMapperConfig {
         let tedge_http_address = tedge_config.http.client.host.clone();
         let tedge_http_port = tedge_config.http.client.port;
         let mqtt_schema = MqttSchema::with_root(tedge_config.mqtt.topic_root.clone());
-        let auth_proxy_addr = tedge_config.c8y.proxy.bind.address;
-        let auth_proxy_port = tedge_config.c8y.proxy.bind.port;
+        let auth_proxy_addr = tedge_config.c8y.proxy.client.host.clone();
+        let auth_proxy_port = tedge_config.c8y.proxy.client.port;
 
         let tedge_http_host = format!("{}:{}", tedge_http_address, tedge_http_port).into();
 
--- crates/extensions/c8y_mapper_ext/src/tests.rs
+++ crates/extensions/c8y_mapper_ext/src/tests.rs
@@ -2566,7 +2566,7 @@ pub(crate) async fn spawn_c8y_mapper_actor(
     let c8y_host = "test.c8y.io".into();
     let tedge_http_host = "localhost:8888".into();
     let mqtt_schema = MqttSchema::default();
-    let auth_proxy_addr = [127, 0, 0, 1].into();
+    let auth_proxy_addr = "127.0.0.1".into();
     let auth_proxy_port = 8001;
     let mut topics = C8yMapperConfig::default_internal_topic_filter(config_dir.path()).unwrap();
     topics.add_all(crate::log_upload::log_upload_topic_filter(&mqtt_schema));
