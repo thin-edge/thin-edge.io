de2fb4083548a456e5b8df466a5bd866c36e9ce7
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-03 12:00:45 +0100

    fix(build): only import `AsRawFd` trait when used under linux target
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- crates/common/download/src/download.rs
+++ crates/common/download/src/download.rs
@@ -18,6 +18,7 @@ use std::fs::File;
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::Write;
+#[cfg(target_os = "linux")]
 use std::os::unix::prelude::AsRawFd;
 use std::path::Path;
 use std::path::PathBuf;
