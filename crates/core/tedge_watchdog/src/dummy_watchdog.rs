7f05343c38efd0f3060b9bc97c5cb22408353478
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-05 09:06:45 +0100

    refactor: use target_os specific error enum definition
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- crates/core/tedge_watchdog/src/dummy_watchdog.rs
+++ crates/core/tedge_watchdog/src/dummy_watchdog.rs
@@ -1,7 +1,11 @@
 use std::path::PathBuf;
 
 pub async fn start_watchdog(_config_dir: PathBuf) -> Result<(), anyhow::Error> {
-    Err(anyhow::Error::from(
-        crate::error::WatchdogError::WatchdogNotAvailable,
-    ))
+    Err(anyhow::Error::from(WatchdogError::WatchdogNotAvailable))
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum WatchdogError {
+    #[error("The watchdog is not available on this platform")]
+    WatchdogNotAvailable,
 }
