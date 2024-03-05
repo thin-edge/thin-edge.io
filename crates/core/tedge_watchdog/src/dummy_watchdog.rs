210997adf453a7bff12c7a5ed42f6b6ddd7e636f
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-05 08:58:38 +0100

    refactor: fix function signal for dummy watchdog used on non linux targets
    Signed-off-by: Reuben Miller <reuben.d.miller@gmail.com>

--- crates/core/tedge_watchdog/src/dummy_watchdog.rs
+++ crates/core/tedge_watchdog/src/dummy_watchdog.rs
@@ -1,3 +1,7 @@
+use std::path::PathBuf;
+
 pub async fn start_watchdog(_config_dir: PathBuf) -> Result<(), anyhow::Error> {
-    anyhow::Error::from(crate::error::WatchdogError::WatchdogNotAvailable)
+    Err(anyhow::Error::from(
+        crate::error::WatchdogError::WatchdogNotAvailable,
+    ))
 }
