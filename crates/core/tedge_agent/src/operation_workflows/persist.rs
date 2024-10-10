b417ae19dd8f2dbbaf69502a1ef16b8417204e4e
Didier Wenzek <didier.wenzek@free.fr>
2024-10-10 10:23:05 +0200

    The agent WorkflowRepository adds durability to the WorkflowSupervisor engine
    The WorkflowRepository acts as a facade to WorkflowSupervisor adding all
    disk related features: loading definitions from disk, caching
    definitions in-use, reloading definitions on changes.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge_agent/src/operation_workflows/persist.rs
+++ crates/core/tedge_agent/src/operation_workflows/persist.rs
@@ -2,20 +2,34 @@ use anyhow::Context;
 use camino::Utf8PathBuf;
 use std::ffi::OsStr;
 use std::path::Path;
+use tedge_api::mqtt_topics::EntityTopicId;
+use tedge_api::mqtt_topics::MqttSchema;
+use tedge_api::mqtt_topics::OperationType;
+use tedge_api::workflow::CommandBoard;
+use tedge_api::workflow::GenericCommandState;
 use tedge_api::workflow::IllFormedOperationWorkflow;
+use tedge_api::workflow::OperationAction;
 use tedge_api::workflow::OperationName;
 use tedge_api::workflow::OperationWorkflow;
+use tedge_api::workflow::WorkflowExecutionError;
 use tedge_api::workflow::WorkflowSupervisor;
 use tedge_api::workflow::WorkflowVersion;
+use tedge_mqtt_ext::MqttMessage;
 use tracing::error;
 use tracing::info;
 
 /// Persist the workflow definitions.
+///
+/// The WorkflowRepository acts as a facade to WorkflowSupervisor
+/// adding all disk related features:
+/// - loading definitions from disk,
+/// - caching definitions in-use,
+/// - reloading definitions on changes.
 pub struct WorkflowRepository {
     builtin_workflows: Vec<OperationName>,
     custom_workflows_dir: Utf8PathBuf,
     state_dir: Utf8PathBuf,
-    pub(crate) workflows: WorkflowSupervisor,
+    workflows: WorkflowSupervisor,
 }
 
 impl WorkflowRepository {
@@ -90,6 +104,73 @@ impl WorkflowRepository {
                 error!("Fail to register built-in workflow for {capability} operation: {err}");
             }
         }
+    }
+
+    pub fn load_pending_commands(&mut self, commands: CommandBoard) -> Vec<GenericCommandState> {
+        self.workflows.load_pending_commands(commands)
+    }
+
+    pub fn pending_commands(&self) -> &CommandBoard {
+        self.workflows.pending_commands()
+    }
+
+    pub fn capability_messages(
+        &self,
+        schema: &MqttSchema,
+        target: &EntityTopicId,
+    ) -> Vec<MqttMessage> {
+        self.workflows.capability_messages(schema, target)
+    }
+
+    pub fn apply_external_update(
+        &mut self,
+        operation: &OperationType,
+        command_state: GenericCommandState,
+    ) -> Result<Option<GenericCommandState>, WorkflowExecutionError> {
+        self.workflows
+            .apply_external_update(operation, command_state)
+    }
+
+    pub fn apply_internal_update(
+        &mut self,
+        new_command_state: GenericCommandState,
+    ) -> Result<(), WorkflowExecutionError> {
+        self.workflows.apply_internal_update(new_command_state)
+    }
+
+    pub fn get_action(
+        &self,
+        command_state: &GenericCommandState,
+    ) -> Result<OperationAction, WorkflowExecutionError> {
+        self.workflows.get_action(command_state)
+    }
+
+    pub fn root_invoking_command_state(
+        &self,
+        leaf_command: &GenericCommandState,
+    ) -> Option<&GenericCommandState> {
+        self.workflows.root_invoking_command_state(leaf_command)
+    }
+
+    pub fn invoking_command_state(
+        &self,
+        sub_command: &GenericCommandState,
+    ) -> Option<&GenericCommandState> {
+        self.workflows.invoking_command_state(sub_command)
+    }
+
+    pub fn sub_command_state(
+        &self,
+        command_state: &GenericCommandState,
+    ) -> Option<&GenericCommandState> {
+        self.workflows.sub_command_state(command_state)
+    }
+
+    pub fn adapt_builtin_response(
+        &self,
+        command_state: GenericCommandState,
+    ) -> GenericCommandState {
+        self.workflows.adapt_builtin_response(command_state)
     }
 }
 
