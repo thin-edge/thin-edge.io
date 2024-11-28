5e9a3f12a69e63207270d88320628d99d15f52ed
Didier Wenzek <didier.wenzek@free.fr>
2024-11-28 15:30:00 +0100

    fixup! Impl tedge cert renew c8y

--- crates/core/tedge/src/cli/certificate/c8y/mod.rs
+++ crates/core/tedge/src/cli/certificate/c8y/mod.rs
@@ -3,18 +3,18 @@ mod renew;
 mod upload;
 
 use crate::cli::certificate::create_csr::CreateCsrCmd;
+use crate::override_public_key;
 use crate::read_cert_to_string;
 use crate::CertError;
 use camino::Utf8PathBuf;
 use certificate::NewCertificateConfig;
 pub use download::DownloadCertCmd;
 pub use renew::RenewCertCmd;
-use std::fs::OpenOptions;
-use std::io::Write;
-use tedge_utils::paths::set_permission;
 pub use upload::UploadCertCmd;
 
 /// Create a device private key and CSR
+///
+/// Return the CSR in the format expected by c8y CA
 fn create_device_csr(
     common_name: String,
     key_path: Utf8PathBuf,
@@ -29,19 +29,23 @@ fn create_device_csr(
         group: "tedge".to_string(),
     };
     create_cmd.create_certificate_signing_request(&config)?;
-    read_cert_to_string(&csr_path)
+
+    let csr = read_cert_to_string(&csr_path)?;
+    let csr = csr
+        .strip_prefix("-----BEGIN CERTIFICATE-----\n")
+        .unwrap_or(&csr);
+    let csr = csr
+        .strip_suffix("-----END CERTIFICATE-----\n")
+        .unwrap_or(csr)
+        .to_string();
+    Ok(csr)
 }
 
-/// Store a device certificate
+/// Store a device certificate received from c8y CA
+///
+/// Notably this adds PEM header and trailer.
 fn store_device_cert(cert_path: &Utf8PathBuf, cert: String) -> Result<(), CertError> {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(cert_path)?;
-
-    file.write_all(cert.as_bytes())?;
-    file.sync_all()?;
-
-    set_permission(&file, 0o444)?;
-    Ok(())
+    let pem_string =
+        String::new() + "-----BEGIN CERTIFICATE-----\n" + &cert + "-----END CERTIFICATE-----\n";
+    override_public_key(cert_path, pem_string)
 }
