0a449243c3b9861a3961233c8f0f71fffc5ac982
Didier Wenzek <didier.wenzek@free.fr>
2025-03-05 19:56:25 +0100

    Ability to provide the CSR to tedge cert download and renew
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge/src/cli/certificate/mod.rs
+++ crates/core/tedge/src/cli/certificate/mod.rs
@@ -1,6 +1,6 @@
 pub use self::cli::TEdgeCertCli;
-use std::io::Read;
 use std::path::Path;
+use tokio::io::AsyncReadExt;
 
 mod c8y;
 mod cli;
@@ -15,13 +15,13 @@ pub use self::cli::*;
 pub use self::create::*;
 pub use self::error::*;
 
-pub(crate) fn read_cert_to_string(path: impl AsRef<Path>) -> Result<String, CertError> {
-    let mut file = std::fs::File::open(path.as_ref()).map_err(|err| {
+pub(crate) async fn read_cert_to_string(path: impl AsRef<Path>) -> Result<String, CertError> {
+    let mut file = tokio::fs::File::open(path.as_ref()).await.map_err(|err| {
         let path = path.as_ref().display().to_string();
         CertError::CertificateReadFailed(err, path)
     })?;
     let mut content = String::new();
-    file.read_to_string(&mut content)?;
+    file.read_to_string(&mut content).await?;
 
     Ok(content)
 }
