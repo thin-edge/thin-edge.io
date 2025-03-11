4f56c086ab166208c172cc9f4c57f5c94779cb94
Didier Wenzek <didier.wenzek@free.fr>
2025-03-11 14:18:46 +0100

    Prepare refactoring of tedge cli to async
    Introduce a wrapper to smooth the refactoring,
    with co-existing sync and async sub-commands.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge/src/lib.rs
+++ crates/core/tedge/src/lib.rs
@@ -1,4 +1,5 @@
 pub use cli::*;
+use std::future::Future;
 pub mod bridge;
 pub mod cli;
 pub mod command;
@@ -8,3 +9,7 @@ mod system_services;
 pub type ConfigError = crate::error::TEdgeError;
 const BROKER_USER: &str = "mosquitto";
 const BROKER_GROUP: &str = "mosquitto";
+
+pub fn block_on<T>(future: impl Future<Output = T>) -> T {
+    tokio::runtime::Runtime::new().unwrap().block_on(future)
+}
