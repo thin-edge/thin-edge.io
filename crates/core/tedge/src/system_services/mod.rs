0c111b382d43498fefe1ece98a2cf62603bedbb5
James Rhodes <jarhodes314@gmail.com>
2025-03-13 12:00:54 +0000

    Remove SystemServiceManagerExt to avoid extra async hassle

--- crates/core/tedge/src/system_services/mod.rs
+++ crates/core/tedge/src/system_services/mod.rs
@@ -11,13 +11,11 @@
 mod command_builder;
 mod error;
 mod manager;
-mod manager_ext;
 mod managers;
 mod services;
 
 pub use self::command_builder::*;
 pub use self::error::*;
 pub use self::manager::*;
-pub use self::manager_ext::*;
 pub use self::managers::*;
 pub use self::services::*;
