63a25f03f5565b2bbbb3652d9f897b398b1c464a
Didier Wenzek <didier.wenzek@free.fr>
2025-03-27 17:45:38 +0100

    Integrate wasmtime
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/extensions/tedge_wasm_mapper/src/lib.rs
+++ crates/extensions/tedge_wasm_mapper/src/lib.rs
@@ -1,14 +1,18 @@
 mod actor;
 mod config;
+mod engine;
 mod pipeline;
 mod wasm;
 
 use crate::actor::WasmMapper;
+use crate::config::PipelineConfig;
+use crate::engine::HostEngine;
 use crate::pipeline::Pipeline;
 use camino::Utf8Path;
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::path::Path;
+use std::path::PathBuf;
 use tedge_actors::Builder;
 use tedge_actors::DynSender;
 use tedge_actors::MessageSink;
@@ -25,20 +29,22 @@ use tracing::error;
 use tracing::info;
 
 pub struct WasmMapperBuilder {
+    engine: HostEngine,
     message_box: SimpleMessageBoxBuilder<MqttMessage, MqttMessage>,
+    pipeline_specs: HashMap<String, PipelineConfig>,
     pipelines: HashMap<String, Pipeline>,
 }
 
-impl Default for WasmMapperBuilder {
-    fn default() -> Self {
-        WasmMapperBuilder {
-            message_box: SimpleMessageBoxBuilder::new("WasmMapper", 16),
-            pipelines: HashMap::default(),
-        }
-    }
-}
-
 impl WasmMapperBuilder {
+    pub fn try_new() -> Result<Self, LoadError> {
+        Ok(WasmMapperBuilder {
+            engine: HostEngine::try_new()?,
+            message_box: SimpleMessageBoxBuilder::new("WasmMapper", 16),
+            pipeline_specs: HashMap::default(),
+            pipelines: HashMap::default(),
+        })
+    }
+
     pub async fn load(&mut self, config_dir: impl AsRef<Path>) {
         let config_dir = config_dir.as_ref();
         let Ok(mut entries) = read_dir(config_dir).await.map_err(|err|
@@ -53,21 +59,39 @@ impl WasmMapperBuilder {
                 continue;
             };
             if let Ok(file_type) = entry.file_type().await {
-                if file_type.is_file() && path.extension() == Some("toml") {
-                    info!(target: "WASM", "Loading pipeline: {path}");
-                    if let Err(err) = self.load_pipeline(path).await {
-                        error!(target: "WASM", "Failed to load pipeline: {err}");
+                if file_type.is_file() {
+                    match path.extension() {
+                        Some("toml") => {
+                            info!(target: "WASM", "Loading pipeline: {path}");
+                            if let Err(err) = self.load_pipeline(path).await {
+                                error!(target: "WASM", "Failed to load pipeline: {err}");
+                            }
+                        }
+                        Some("wasm") => {
+                            info!(target: "WASM", "Loading filter: {path}");
+                            if let Err(err) = self.engine.load_component(path).await {
+                                error!(target: "WASM", "Failed to load filter: {err}");
+                            }
+                        }
+                        _ => {
+                            info!(target: "WASM", "Skipping file which type is unknown: {path}");
+                        }
                     }
                 }
             }
         }
+
+        // FIXME This should be done when the actor is built
+        // Done here for now to ease the computation of the topics to subscribe to
+        // as these topics have to be known when connect is called
+        self.instantiate()
     }
 
     async fn load_pipeline(&mut self, file: impl AsRef<Utf8Path>) -> Result<(), LoadError> {
         if let Some(name) = file.as_ref().file_name() {
             let specs = read_to_string(file.as_ref()).await?;
-            let pipeline: Pipeline = toml::from_str(&specs)?;
-            self.pipelines.insert(name.to_string(), pipeline);
+            let pipeline: PipelineConfig = toml::from_str(&specs)?;
+            self.pipeline_specs.insert(name.to_string(), pipeline);
         }
 
         Ok(())
@@ -79,6 +103,19 @@ impl WasmMapperBuilder {
     ) {
         mqtt.connect_sink(self.topics(), &self.message_box);
         self.message_box.connect_sink(NoConfig, mqtt);
+    }
+
+    fn instantiate(&mut self) {
+        for (name, specs) in self.pipeline_specs.drain() {
+            match specs.instantiate(&self.engine) {
+                Ok(pipeline) => {
+                    let _ = self.pipelines.insert(name, pipeline);
+                }
+                Err(err) => {
+                    error!(target: "WASM", "Failed to load pipeline: {err}")
+                }
+            }
+        }
     }
 
     fn topics(&self) -> TopicFilter {
@@ -113,9 +150,18 @@ impl Builder<WasmMapper> for WasmMapperBuilder {
 
 #[derive(thiserror::Error, Debug)]
 pub enum LoadError {
+    #[error("File not found: {path}")]
+    FileNotFound { path: PathBuf },
+
     #[error(transparent)]
     IoError(#[from] std::io::Error),
 
     #[error(transparent)]
     TomlError(#[from] toml::de::Error),
+
+    #[error(transparent)]
+    WasmError(#[from] wasmtime::Error),
+
+    #[error(transparent)]
+    ConfigError(#[from] config::ConfigError),
 }
--- crates/extensions/tedge_wasm_mapper/src/wasm.rs
+++ crates/extensions/tedge_wasm_mapper/src/wasm.rs
@@ -1,17 +1,34 @@
+use crate::engine::HostState;
+use crate::pipeline;
 use crate::pipeline::Filter;
-use crate::pipeline::FilterError;
-use std::path::PathBuf;
+use std::ops::DerefMut;
+use std::sync::Arc;
+use std::sync::Mutex;
 use tedge_mqtt_ext::MqttMessage;
+use time::format_description;
 use time::OffsetDateTime;
 use tracing::debug;
+use wasmtime::component::TypedFunc;
+use wasmtime::Store;
 
-pub struct WasmFilter {}
+wasmtime::component::bindgen!();
+
+pub type TransformedMessages = Result<Vec<Message>, FilterError>;
+pub type ProcessFunc = TypedFunc<(Timestamp, Message), (TransformedMessages,)>;
+
+pub struct WasmFilter {
+    store: Arc<Mutex<Store<HostState>>>,
+    process_func: ProcessFunc,
+}
 
 impl WasmFilter {
-    pub fn new(path: impl Into<PathBuf>) -> Self {
-        let path = path.into();
-        debug!(target: "WASM", "new({path:?})");
-        WasmFilter {}
+    pub fn new(store: Store<HostState>, process_func: ProcessFunc) -> Self {
+        let store = Arc::new(Mutex::new(store));
+
+        Self {
+            store,
+            process_func,
+        }
     }
 }
 
@@ -20,18 +37,89 @@ impl Filter for WasmFilter {
         &mut self,
         timestamp: OffsetDateTime,
         message: &MqttMessage,
-    ) -> Result<Vec<MqttMessage>, FilterError> {
+    ) -> Result<Vec<MqttMessage>, pipeline::FilterError> {
         debug!(target: "WASM", "process({timestamp}, {message:?})");
-        Ok(vec![message.clone()])
+        let now_utc = timestamp
+            .format(&format_description::well_known::Rfc3339)
+            .map_err(|err| {
+                pipeline::FilterError::IncorrectSetting(format!(
+                    "failed to format timestamp: {}",
+                    err
+                ))
+            })?;
+        let message = message.try_into()?;
+
+        let mut store = self.store.lock().unwrap();
+        let (result,) = self
+            .process_func
+            .call(store.deref_mut(), (now_utc, message))
+            .map_err(|err| {
+                pipeline::FilterError::IncorrectSetting(format!(
+                    "failed to call the process function: {}",
+                    err
+                ))
+            })?;
+        self.process_func
+            .post_return(store.deref_mut())
+            .map_err(|err| {
+                pipeline::FilterError::IncorrectSetting(format!(
+                    "failed to clean up the process function call: {}",
+                    err
+                ))
+            })?;
+
+        result
+            .map_err(pipeline::FilterError::from)?
+            .into_iter()
+            .map(MqttMessage::try_from)
+            .collect()
     }
 
-    fn update_config(&mut self, config: &MqttMessage) -> Result<(), FilterError> {
+    fn update_config(&mut self, config: &MqttMessage) -> Result<(), pipeline::FilterError> {
         debug!(target: "WASM", "update_config({config:?})");
         Ok(())
     }
 
-    fn tick(&mut self, timestamp: OffsetDateTime) -> Result<Vec<MqttMessage>, FilterError> {
+    fn tick(
+        &mut self,
+        timestamp: OffsetDateTime,
+    ) -> Result<Vec<MqttMessage>, pipeline::FilterError> {
         debug!(target: "WASM", "tick({timestamp})");
         Ok(vec![])
+    }
+}
+
+impl TryFrom<&MqttMessage> for Message {
+    type Error = pipeline::FilterError;
+
+    fn try_from(message: &MqttMessage) -> Result<Self, Self::Error> {
+        let topic = message.topic.to_string();
+        let payload = message
+            .payload_str()
+            .map_err(|_| {
+                pipeline::FilterError::UnsupportedMessage("Not an UTF8 payload".to_string())
+            })?
+            .to_string();
+        Ok(Message { topic, payload })
+    }
+}
+
+impl TryFrom<Message> for MqttMessage {
+    type Error = pipeline::FilterError;
+
+    fn try_from(message: Message) -> Result<Self, Self::Error> {
+        let topic = message.topic.as_str().try_into().map_err(|_| {
+            pipeline::FilterError::UnsupportedMessage(format!("invalid topic {}", message.topic))
+        })?;
+        Ok(MqttMessage::new(&topic, message.payload))
+    }
+}
+
+impl From<FilterError> for pipeline::FilterError {
+    fn from(error: FilterError) -> Self {
+        match error {
+            FilterError::UnsupportedMessage(err) => pipeline::FilterError::UnsupportedMessage(err),
+            FilterError::IncorrectSetting(err) => pipeline::FilterError::IncorrectSetting(err),
+        }
     }
 }
