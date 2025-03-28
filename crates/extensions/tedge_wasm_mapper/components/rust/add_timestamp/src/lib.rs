1e07f24919a01abe76dec6b6b5521a6100862fbd
Didier Wenzek <didier.wenzek@free.fr>
2025-03-28 16:32:24 +0100

    Use Datetime for timestamps
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/extensions/tedge_wasm_mapper/components/rust/add_timestamp/src/lib.rs
+++ crates/extensions/tedge_wasm_mapper/components/rust/add_timestamp/src/lib.rs
@@ -1,4 +1,6 @@
 use serde_json::Value;
+use time::format_description::well_known::Rfc3339;
+use time::OffsetDateTime;
 
 wit_bindgen::generate!({
     world: "tedge",
@@ -7,8 +9,19 @@ wit_bindgen::generate!({
 
 pub struct Component;
 
+impl Component {
+    fn format_timestamp(timestamp: Datetime) -> Result<String, String> {
+        OffsetDateTime::from_unix_timestamp(timestamp.seconds as i64)
+            .map_err(|e| e.to_string())?
+            .replace_nanosecond(timestamp.nanoseconds)
+            .map_err(|e| e.to_string())?
+            .format(&Rfc3339)
+            .map_err(|e| e.to_string())
+    }
+}
+
 impl Guest for Component {
-    fn process(timestamp: Timestamp, message: Message) -> Result<Vec<Message>, FilterError> {
+    fn process(timestamp: Datetime, message: Message) -> Result<Vec<Message>, FilterError> {
         let Ok(Value::Object(mut json)) = serde_json::from_str(&message.payload) else {
             return Err(FilterError::UnsupportedMessage(
                 "Expect JSON input".to_string(),
@@ -19,7 +32,11 @@ impl Guest for Component {
             return Ok(vec![message]);
         }
 
-        json.insert("time".to_string(), Value::String(timestamp));
+        let now_utc = Self::format_timestamp(timestamp).map_err(|err| {
+            FilterError::IncorrectSetting(format!("failed to format current timestamp: {}", err))
+        })?;
+
+        json.insert("time".to_string(), Value::String(now_utc));
 
         let updated_message = Message {
             payload: Value::Object(json).to_string(),
@@ -33,7 +50,7 @@ impl Guest for Component {
         Ok(())
     }
 
-    fn tick(_timestamp: Timestamp) -> Result<Vec<Message>, FilterError> {
+    fn tick(_timestamp: Datetime) -> Result<Vec<Message>, FilterError> {
         Ok(vec![])
     }
 }
