63035756eed49b9d4d5170e77d6fafe7500dc720
reubenmiller <reuben.d.miller@gmail.com>
2025-04-03 17:12:09 +0200

    fixup! add certificate renewal service
    Signed-off-by: reubenmiller <reuben.d.miller@gmail.com>

--- configuration/contrib/tedge-cert-renewer.sh
+++ configuration/contrib/tedge-cert-renewer.sh
@@ -11,16 +11,45 @@ set -e
 # * if a backup exists, then the renewal process was interrupted
 # * remove any backups after the new certificate has been verified
 #
+usage() {
+    cat <<EOT
+Reliable certificate renewer to check if a certificate should be renewed
+and to renew the certificate in a reliable manner by backing up the previous
+certificate, and rolling back if the new certificate does not pass the validation
+check.
 
-if [ $# -lt 1 ]; then
-    echo "missing required position argument" >&2
+The scripts relies on the Cumulocity certificate-authority feature to work.
+
+Usage
+
+  $0 needs-renewal <cloud>      Check if the certificate needs renewal (exit code 0 if it does)
+  $0 renew <cloud>              Renew the certificate for the given cloud
+
+Examples
+
+$0 needs-renewal c8y 
+# Check if the Cumulocity certificate needs renewal
+
+$0 renew c8y
+# Renew the Cumulocity certificate
+
+EOT
+}
+
+if [ $# -lt 2 ]; then
+    echo "ERROR: missing required positional arguments" >&2
+    usage
     exit 1
 fi
 
-CLOUD="$1"
+COMMAND="$1"
+CLOUD="$2"
 CERT_PATH=$(tedge config get "${CLOUD}.device.cert_path")
 BACKUP_CERTIFICATE="${CERT_PATH}.bak"
 
+#
+# Helpers
+#
 verify_certificate_or_rollback() {
     if tedge reconnect "$CLOUD"; then
         echo "Successfully reconnected to $CLOUD. Removing backup certificate" >&2
@@ -50,34 +79,63 @@ is_backup_same() {
     fi   
 }
 
-# If a backup file already exists, than the script may of been interrupted, so check if the
-# current connection is ok or not, and revert to the backup if necessary
-# If we don't do this check, then it could result in the backup replacing a potentially good certificate
-if [ -f "$BACKUP_CERTIFICATE" ]; then
-    if is_backup_same; then
-        echo "Back file is the same so removing it" >&2
-        rm -f "$BACKUP_CERTIFICATE"
-    else
-        echo "Warning: backup certificate exists and is different to current file. path=$BACKUP_CERTIFICATE" >&2
-        verify_certificate_or_rollback
-    fi
-fi
-
-echo "Backup up certificate file. $BACKUP_CERTIFICATE" >&2
-cp "$CERT_PATH" "$BACKUP_CERTIFICATE"
-
-if ! tedge cert renew "$CLOUD"; then
-    # Check if the existing certificate has been overwritten by
-    # the renew command, if so, then the backup should be restored
-    if ! is_backup_same; then
-        echo "Certificate has been changed by the renewal process even though it was not successful, so restoring the backup" >&2
-        mv "$BACKUP_CERTIFICATE" "$CERT_PATH"
-    else
-        rm -f "$BACKUP_CERTIFICATE"
+#
+# Commands
+#
+needs_renewal() {
+    if [ -f "$BACKUP_CERTIFICATE" ]; then
+        echo "Found a left-over certificate backup file which is a sign that the renewal did not fully complete" >&2
+        exit 0
     fi
 
-    echo "Failed to renew certificate" >&2
-    exit 1
-fi
+    /usr/bin/tedge cert needs-renewal "$CLOUD"
+}
 
-verify_certificate_or_rollback
+renew() {
+    # If a backup file already exists, than the script may of been interrupted, so check if the
+    # current connection is ok or not, and revert to the backup if necessary
+    # If we don't do this check, then it could result in the backup replacing a potentially good certificate
+    if [ -f "$BACKUP_CERTIFICATE" ]; then
+        if is_backup_same; then
+            echo "Back file is the same so removing it" >&2
+            rm -f "$BACKUP_CERTIFICATE"
+        else
+            echo "Warning: backup certificate exists and is different to current file. path=$BACKUP_CERTIFICATE" >&2
+            verify_certificate_or_rollback
+        fi
+    fi
+
+    echo "Backup up certificate file. $BACKUP_CERTIFICATE" >&2
+    cp "$CERT_PATH" "$BACKUP_CERTIFICATE"
+
+    if ! tedge cert renew "$CLOUD"; then
+        # Check if the existing certificate has been overwritten by
+        # the renew command, if so, then the backup should be restored
+        if is_backup_same; then
+            echo "Certificate and backup are the same file so removing the backup" >&2
+            rm -f "$BACKUP_CERTIFICATE"
+        else
+            echo "Certificate has been changed by the renewal process even though it was not successful, so restoring the backup" >&2
+            mv "$BACKUP_CERTIFICATE" "$CERT_PATH" 
+        fi
+
+        echo "Failed to renew certificate" >&2
+        exit 1
+    fi
+
+    verify_certificate_or_rollback
+}
+
+
+#
+# Main
+#
+case "$COMMAND" in
+    needs-renewal)
+        needs_renewal
+        ;;
+    renew)
+        renew
+        ;;
+esac
+
