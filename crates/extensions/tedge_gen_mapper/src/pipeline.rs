8d195b98fd10c3080433208791072fb9a2110e2a
Didier Wenzek <didier.wenzek@free.fr>
2025-05-19 12:25:44 +0200

    Avoid to deserialize MQTT at each pipeline stage.
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/extensions/tedge_gen_mapper/src/pipeline.rs
+++ crates/extensions/tedge_gen_mapper/src/pipeline.rs
@@ -1,5 +1,8 @@
 use crate::js_filter::JsFilter;
 use crate::js_filter::JsRuntime;
+use crate::LoadError;
+use serde_json::json;
+use serde_json::Value;
 use tedge_mqtt_ext::MqttMessage;
 use tedge_mqtt_ext::TopicFilter;
 use time::OffsetDateTime;
@@ -17,6 +20,18 @@ pub struct Pipeline {
 pub struct Stage {
     pub filter: JsFilter,
     pub config_topics: TopicFilter,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize, Eq, PartialEq)]
+pub struct DateTime {
+    pub(crate) seconds: u64,
+    pub(crate) nanoseconds: u32,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize, Eq, PartialEq)]
+pub struct Message {
+    pub(crate) topic: String,
+    pub(crate) payload: String,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -38,12 +53,12 @@ impl Pipeline {
     }
 
     pub fn update_config(
-        &mut self,
+        &self,
         js_runtime: &JsRuntime,
-        message: &MqttMessage,
+        message: &Message,
     ) -> Result<(), FilterError> {
-        for stage in self.stages.iter_mut() {
-            if stage.config_topics.accept(message) {
+        for stage in self.stages.iter() {
+            if stage.config_topics.accept_topic_name(&message.topic) {
                 stage.filter.update_config(js_runtime, message)?
             }
         }
@@ -53,16 +68,16 @@ impl Pipeline {
     pub async fn process(
         &mut self,
         js_runtime: &JsRuntime,
-        timestamp: OffsetDateTime,
-        message: &MqttMessage,
-    ) -> Result<Vec<MqttMessage>, FilterError> {
+        timestamp: &DateTime,
+        message: &Message,
+    ) -> Result<Vec<Message>, FilterError> {
         self.update_config(js_runtime, message)?;
-        if !self.input_topics.accept(message) {
+        if !self.input_topics.accept_topic_name(&message.topic) {
             return Ok(vec![]);
         }
 
         let mut messages = vec![message.clone()];
-        for stage in self.stages.iter_mut() {
+        for stage in self.stages.iter() {
             let mut transformed_messages = vec![];
             for message in messages.iter() {
                 let filter_output = stage.filter.process(js_runtime, timestamp, message).await;
@@ -76,10 +91,10 @@ impl Pipeline {
     pub async fn tick(
         &mut self,
         js_runtime: &JsRuntime,
-        timestamp: OffsetDateTime,
-    ) -> Result<Vec<MqttMessage>, FilterError> {
+        timestamp: &DateTime,
+    ) -> Result<Vec<Message>, FilterError> {
         let mut messages = vec![];
-        for stage in self.stages.iter_mut() {
+        for stage in self.stages.iter() {
             // Process first the messages triggered upstream by the tick
             let mut transformed_messages = vec![];
             for message in messages.iter() {
@@ -95,4 +110,67 @@ impl Pipeline {
         }
         Ok(messages)
     }
+}
+
+impl DateTime {
+    pub fn now() -> Self {
+        DateTime::try_from(OffsetDateTime::now_utc()).unwrap()
+    }
+}
+
+impl TryFrom<OffsetDateTime> for DateTime {
+    type Error = FilterError;
+
+    fn try_from(value: OffsetDateTime) -> Result<Self, Self::Error> {
+        let seconds = u64::try_from(value.unix_timestamp()).map_err(|err| {
+            FilterError::UnsupportedMessage(format!("failed to convert timestamp: {}", err))
+        })?;
+
+        Ok(DateTime {
+            seconds,
+            nanoseconds: value.nanosecond(),
+        })
+    }
+}
+
+impl Message {
+    #[cfg(test)]
+    pub(crate) fn new(topic: &str, payload: &str) -> Self {
+        Message {
+            topic: topic.to_string(),
+            payload: payload.to_string(),
+        }
+    }
+
+    pub fn json(&self) -> Value {
+        json!({"topic": self.topic, "payload": self.payload})
+    }
+}
+
+impl TryFrom<MqttMessage> for Message {
+    type Error = FilterError;
+
+    fn try_from(message: MqttMessage) -> Result<Self, Self::Error> {
+        let topic = message.topic.to_string();
+        let payload = message
+            .payload_str()
+            .map_err(|_| FilterError::UnsupportedMessage("Not an UTF8 payload".to_string()))?
+            .to_string();
+        Ok(Message { topic, payload })
+    }
+}
+
+impl TryFrom<Message> for MqttMessage {
+    type Error = FilterError;
+
+    fn try_from(message: Message) -> Result<Self, Self::Error> {
+        let topic = message.topic.as_str().try_into().map_err(|_| {
+            FilterError::UnsupportedMessage(format!("invalid topic {}", message.topic))
+        })?;
+        Ok(MqttMessage::new(&topic, message.payload))
+    }
+}
+
+pub fn error_from_js(err: LoadError) -> FilterError {
+    FilterError::IncorrectSetting(format!("{}", err))
 }
