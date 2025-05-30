56480dd22d609055de375ef010cabdb6b39af3f1
James Rhodes <jarhodes314@gmail.com>
2025-05-30 16:24:02 +0100

    Tidy JS example
    Signed-off-by: James Rhodes <jarhodes314@gmail.com>

--- crates/extensions/tedge_gen_mapper/pipelines/collectd-to-te.js
+++ crates/extensions/tedge_gen_mapper/pipelines/collectd-to-te.js
@@ -1,19 +1,14 @@
-export function process (timestamp, message, config) {
-    let groups = message.topic.split( '/')
+export function process(_timestamp, message, config) {
+    let groups = message.topic.split('/')
     let data = message.payload.split(':')
 
     let group = groups[2]
-	let measurement = groups[3]
-	let time = data[0]
-	let value = data[1]
+    let measurement = groups[3]
+    let time = data[0]
+    let value = data[1]
 
-    var topic = "te/device/main///m/collectd"
-    if (config && config.topic) {
-        topic = config.topic
-    }
-
-    return [ {
-        topic: topic,
+    return [{
+        topic: config?.topic || "te/device/main///m/collectd",
         payload: `{"time": ${time}, "${group}": {"${measurement}": ${value}}}`
     }]
 }
