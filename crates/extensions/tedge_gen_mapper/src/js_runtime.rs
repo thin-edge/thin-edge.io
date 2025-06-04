e177b0844c104c6d285e5687c259ca524fec8dbf
Didier Wenzek <didier.wenzek@free.fr>
2025-06-04 19:12:08 +0200

    Fix memory leak
    The first design was naive (i.e. loading the javascript module
    for each and every function call) and leading to a memory leak
    (loading a module with the same name keeps the previous version of the
    module in memory).
    
    The new design fixes the issue. The memory is stable while translating to c8y
    600 tedge measurements per second during 20 minutes (mode debug)
    
    ```shell
    $ watch ps -p 1382888 -o args,%cpu,etimes,times,%mem,rss,vsz
    COMMAND                     %CPU ELAPSED     TIME %MEM   RSS    VSZ
    tedge-mapper gen            47.4    1322      627  0.0 41440 1359476
    
    $ tedge mqtt sub 'test/output' --duration 1000 | pv | wc -l
    89.3MiB 0:16:40 [91.4KiB/s] [  <=>                                                                                                                            ]
    586311
    ```
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

---
+++ crates/extensions/tedge_gen_mapper/src/js_runtime.rs
@@ -0,0 +1,204 @@
+use crate::js_filter::JsonValue;
+use crate::LoadError;
+use anyhow::anyhow;
+use rquickjs::module::Evaluated;
+use rquickjs::Ctx;
+use rquickjs::Module;
+use std::collections::HashMap;
+use std::path::Path;
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
+use tracing::debug;
+
+pub struct JsRuntime {
+    runtime: rquickjs::AsyncRuntime,
+    worker: mpsc::Sender<JsRequest>,
+}
+
+impl JsRuntime {
+    pub async fn try_new() -> Result<Self, LoadError> {
+        let runtime = rquickjs::AsyncRuntime::new()?;
+        let context = rquickjs::AsyncContext::full(&runtime).await?;
+        let worker = JsWorker::spawn(context).await;
+        Ok(JsRuntime { runtime, worker })
+    }
+
+    pub async fn load_file(&mut self, path: impl AsRef<Path>) -> Result<(), LoadError> {
+        let path = path.as_ref();
+        let source = tokio::fs::read_to_string(path).await?;
+        self.load_js(path, source).await
+    }
+
+    pub async fn load_js(
+        &mut self,
+        path: impl AsRef<Path>,
+        source: impl Into<Vec<u8>>,
+    ) -> Result<(), LoadError> {
+        let (sender, receiver) = oneshot::channel();
+        let path = path.as_ref().to_path_buf();
+        let name = path.display().to_string();
+        let source = source.into();
+        self.worker
+            .send(JsRequest::LoadModule {
+                name,
+                source,
+                sender,
+            })
+            .await
+            .map_err(|err| anyhow!(err))?;
+        receiver.await.map_err(|err| anyhow!(err))?
+    }
+
+    pub async fn call_function(
+        &self,
+        module: &Path,
+        function: &str,
+        args: Vec<JsonValue>,
+    ) -> Result<JsonValue, LoadError> {
+        let (sender, receiver) = oneshot::channel();
+        self.worker
+            .send(JsRequest::CallFunction {
+                module: module.display().to_string(),
+                function: function.to_string(),
+                args,
+                sender,
+            })
+            .await
+            .map_err(|err| anyhow!(err))?;
+        receiver.await.map_err(|err| anyhow!(err))?
+    }
+
+    pub async fn dump_memory_stats(&self) {
+        let usage = self.runtime.memory_usage().await;
+        tracing::info!(target: "gen-mapper", "Memory usage:");
+        tracing::info!(target: "gen-mapper", "  - malloc size: {}", usage.malloc_size);
+        tracing::info!(target: "gen-mapper", "  - used memory size: {}", usage.memory_used_size);
+        tracing::info!(target: "gen-mapper", "  - function count: {}", usage.js_func_count);
+        tracing::info!(target: "gen-mapper", "  - object count: {}", usage.obj_count);
+        tracing::info!(target: "gen-mapper", "  - array count: {}", usage.array_count);
+        tracing::info!(target: "gen-mapper", "  - string count: {}", usage.str_count);
+        tracing::info!(target: "gen-mapper", "  - atom count: {}", usage.atom_count);
+    }
+}
+
+enum JsRequest {
+    LoadModule {
+        name: String,
+        source: Vec<u8>,
+        sender: oneshot::Sender<Result<(), LoadError>>,
+    },
+    CallFunction {
+        module: String,
+        function: String,
+        args: Vec<JsonValue>,
+        sender: oneshot::Sender<Result<JsonValue, LoadError>>,
+    },
+}
+
+struct JsWorker {
+    context: rquickjs::AsyncContext,
+    requests: mpsc::Receiver<JsRequest>,
+}
+
+impl JsWorker {
+    pub async fn spawn(context: rquickjs::AsyncContext) -> mpsc::Sender<JsRequest> {
+        let (sender, requests) = mpsc::channel(100);
+        tokio::spawn(async move {
+            let worker = JsWorker { context, requests };
+            worker.run().await
+        });
+        sender
+    }
+
+    async fn run(mut self) {
+        rquickjs::async_with!(self.context => |ctx| {
+            let mut modules = JsModules::new();
+            while let Some(request) = self.requests.recv().await {
+                match request {
+                    JsRequest::LoadModule{name, source, sender} => {
+                        let result = modules.load_module(ctx.clone(), name, source).await;
+                        let _ = sender.send(result);
+                    }
+                    JsRequest::CallFunction{module, function, args, sender} => {
+                        let result = modules.call_function(ctx.clone(), module, function, args).await;
+                        let _ = sender.send(result);
+                    }
+                }
+            }
+        })
+        .await
+    }
+}
+
+struct JsModules<'js> {
+    modules: HashMap<String, Module<'js, Evaluated>>,
+}
+
+impl<'js> JsModules<'js> {
+    fn new() -> Self {
+        JsModules {
+            modules: HashMap::new(),
+        }
+    }
+
+    async fn load_module(
+        &mut self,
+        ctx: Ctx<'js>,
+        name: String,
+        source: Vec<u8>,
+    ) -> Result<(), LoadError> {
+        debug!(target: "MAPPING", "compile({name})");
+        let module = Module::declare(ctx, name.clone(), source)?;
+        let (module, p) = module.eval()?;
+        let () = p.finish()?;
+        self.modules.insert(name, module);
+        Ok(())
+    }
+
+    async fn call_function(
+        &mut self,
+        ctx: Ctx<'js>,
+        module_name: String,
+        function: String,
+        args: Vec<JsonValue>,
+    ) -> Result<JsonValue, LoadError> {
+        debug!(target: "MAPPING", "link({module_name}.{function})");
+        let module = self
+            .modules
+            .get(&module_name)
+            .ok_or_else(|| LoadError::UnknownModule {
+                module_name: module_name.clone(),
+            })?;
+        let f: rquickjs::Value = module
+            .get(&function)
+            .map_err(|_| LoadError::UnknownFunction {
+                module_name: module_name.clone(),
+                function: function.clone(),
+            })?;
+        let f = rquickjs::Function::from_value(f)?;
+
+        debug!(target: "MAPPING", "execute({module_name}.{function})");
+        let r = match &args[..] {
+            [] => f.call(()),
+            [v0] => f.call((v0,)),
+            [v0, v1] => f.call((v0, v1)),
+            [v0, v1, v2] => f.call((v0, v1, v2)),
+            [v0, v1, v2, v3] => f.call((v0, v1, v2, v3)),
+            [v0, v1, v2, v3, v4] => f.call((v0, v1, v2, v3, v4)),
+            [v0, v1, v2, v3, v4, v5] => f.call((v0, v1, v2, v3, v4, v5)),
+            [v0, v1, v2, v3, v4, v5, v6] => f.call((v0, v1, v2, v3, v4, v5, v6)),
+            _ => return Err(anyhow::anyhow!("Too many args").into()),
+        };
+
+        debug!(target: "MAPPING", "execute({module_name}.{function}) => {r:?}");
+        r.map_err(|err| {
+            if let Some(ex) = ctx.catch().as_exception() {
+                let err = anyhow::anyhow!("{ex}");
+                err.context("JS raised exception").into()
+            } else {
+                debug!(target: "MAPPING", "execute({module_name}.{function}) => {err:?}");
+                err.into()
+            }
+        })
+    }
+}
