ccfd5dda6d531bdf36ff9de4a6e0c6246388cb52
Didier Wenzek <didier.wenzek@free.fr>
2025-06-25 19:35:04 +0200

    Extract message processing from gen-mapper actor
    The goal is to be able to use the message processor from within the
    tedge cli command, notably for testing pipelines and filters.
    
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/extensions/tedge_gen_mapper/src/actor.rs
+++ crates/extensions/tedge_gen_mapper/src/actor.rs
@@ -1,14 +1,10 @@
-use crate::config::PipelineConfig;
-use crate::js_runtime::JsRuntime;
 use crate::pipeline::DateTime;
 use crate::pipeline::Message;
-use crate::pipeline::Pipeline;
+use crate::runtime::MessageProcessor;
 use crate::InputMessage;
 use crate::OutputMessage;
 use async_trait::async_trait;
 use camino::Utf8PathBuf;
-use std::collections::HashMap;
-use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::Mutex;
 use tedge_actors::Actor;
@@ -23,14 +19,11 @@ use tedge_mqtt_ext::TopicFilter;
 use tokio::time::interval;
 use tokio::time::Duration;
 use tracing::error;
-use tracing::info;
 
 pub struct GenMapper {
     pub(super) messages: SimpleMessageBox<InputMessage, OutputMessage>,
-    pub(super) pipelines: HashMap<String, Pipeline>,
     pub(super) subscriptions: Arc<Mutex<TopicFilter>>,
-    pub(super) js_runtime: JsRuntime,
-    pub(super) config_dir: PathBuf,
+    pub(super) processor: MessageProcessor,
 }
 
 #[async_trait]
@@ -60,9 +53,9 @@ impl Actor for GenMapper {
                                 continue;
                             };
                             if matches!(path.extension(), Some("js" | "ts")) {
-                                self.reload_filter(path).await;
+                                self.processor.reload_filter(path).await;
                             } else if path.extension() == Some("toml") {
-                                self.reload_pipeline(path).await;
+                                self.processor.reload_pipeline(path).await;
                                 self.send_updated_subscriptions().await?;
                             }
                         },
@@ -79,51 +72,6 @@ impl Actor for GenMapper {
 }
 
 impl GenMapper {
-    async fn reload_filter(&mut self, path: Utf8PathBuf) {
-        for pipeline in self.pipelines.values_mut() {
-            for stage in &mut pipeline.stages {
-                if stage.filter.path() == path {
-                    match self.js_runtime.load_file(&path).await {
-                        Ok(()) => {
-                            info!("Reloaded filter {path}");
-                        }
-                        Err(e) => {
-                            error!("Failed to reload filter {path}: {e}");
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    async fn reload_pipeline(&mut self, path: Utf8PathBuf) {
-        for pipeline in self.pipelines.values_mut() {
-            if pipeline.source == path {
-                let Ok(source) = tokio::fs::read_to_string(&path).await else {
-                    error!("Failed to read updated filter {path}");
-                    break;
-                };
-                let config: PipelineConfig = match toml::from_str(&source) {
-                    Ok(config) => config,
-                    Err(e) => {
-                        error!("Failed to parse toml for updated filter {path}: {e}");
-                        break;
-                    }
-                };
-                match config.compile(&self.js_runtime, &self.config_dir, path.clone()) {
-                    Ok(p) => {
-                        *pipeline = p;
-                        info!("Reloaded pipeline {path}");
-                    }
-                    Err(e) => {
-                        error!("Failed to load updated pipeline {path}: {e}")
-                    }
-                };
-            }
-        }
-    }
-
     async fn send_updated_subscriptions(&mut self) -> Result<(), RuntimeError> {
         let topics = self.update_subscriptions();
         let diff = SubscriptionDiff::new(&topics, &TopicFilter::empty());
@@ -135,19 +83,14 @@ impl GenMapper {
 
     fn update_subscriptions(&self) -> TopicFilter {
         let mut topics = self.subscriptions.lock().unwrap();
-        for pipeline in self.pipelines.values() {
-            topics.add_all(pipeline.topics())
-        }
+        topics.add_all(self.processor.subscriptions());
         topics.clone()
     }
 
     async fn filter(&mut self, message: Message) -> Result<(), RuntimeError> {
         let timestamp = DateTime::now();
-        for (pipeline_id, pipeline) in self.pipelines.iter_mut() {
-            match pipeline
-                .process(&self.js_runtime, &timestamp, &message)
-                .await
-            {
+        for (pipeline_id, pipeline_messages) in self.processor.process(&timestamp, &message).await {
+            match pipeline_messages {
                 Ok(messages) => {
                     for message in messages {
                         match MqttMessage::try_from(message) {
@@ -174,10 +117,10 @@ impl GenMapper {
     async fn tick(&mut self) -> Result<(), RuntimeError> {
         let timestamp = DateTime::now();
         if timestamp.seconds % 300 == 0 {
-            self.js_runtime.dump_memory_stats().await;
+            self.processor.dump_memory_stats().await;
         }
-        for (pipeline_id, pipeline) in self.pipelines.iter_mut() {
-            match pipeline.tick(&self.js_runtime, &timestamp).await {
+        for (pipeline_id, pipeline_messages) in self.processor.tick(&timestamp).await {
+            match pipeline_messages {
                 Ok(messages) => {
                     for message in messages {
                         match MqttMessage::try_from(message) {
