7b01456c57663a4c58aff630e68d45c59690be3b
Didier Wenzek <didier.wenzek@free.fr>
2025-06-27 09:16:36 +0200

    tedge mapping test consumes messages from stdin
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/core/tedge/src/cli/mapping/test.rs
+++ crates/core/tedge/src/cli/mapping/test.rs
@@ -5,6 +5,10 @@ use anyhow::Error;
 use std::path::PathBuf;
 use tedge_config::TEdgeConfig;
 use tedge_gen_mapper::pipeline::*;
+use tedge_gen_mapper::MessageProcessor;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
+use tokio::io::Stdin;
 
 pub struct TestCommand {
     pub mapping_dir: PathBuf,
@@ -25,25 +29,41 @@ impl Command for TestCommand {
         let mut processor = TEdgeMappingCli::load_pipelines(&self.mapping_dir).await?;
         if let Some(message) = &self.message {
             let timestamp = DateTime::now();
-            match &self.filter {
-                Some(filter) => {
-                    let filter_name = filter.display().to_string();
-                    print(
-                        processor
-                            .process_with_pipeline(&filter_name, &timestamp, message)
-                            .await,
-                    )
-                }
-                None => processor
-                    .process(&timestamp, message)
-                    .await
-                    .into_iter()
-                    .map(|(_, v)| v)
-                    .for_each(print),
+            self.process(&mut processor, message, &timestamp).await;
+        } else {
+            let mut stdin = BufReader::new(tokio::io::stdin());
+            while let Some(message) = next_message(&mut stdin).await {
+                let timestamp = DateTime::now();
+                self.process(&mut processor, &message, &timestamp).await;
             }
         }
-
         Ok(())
+    }
+}
+
+impl TestCommand {
+    async fn process(
+        &self,
+        processor: &mut MessageProcessor,
+        message: &Message,
+        timestamp: &DateTime,
+    ) {
+        match &self.filter {
+            Some(filter) => {
+                let filter_name = filter.display().to_string();
+                print(
+                    processor
+                        .process_with_pipeline(&filter_name, timestamp, message)
+                        .await,
+                )
+            }
+            None => processor
+                .process(timestamp, message)
+                .await
+                .into_iter()
+                .map(|(_, v)| v)
+                .for_each(print),
+        }
     }
 }
 
@@ -56,6 +76,42 @@ fn print(messages: Result<Vec<Message>, FilterError>) {
         }
         Err(err) => {
             eprintln!("Error: {}", err)
+        }
+    }
+}
+
+fn parse(line: String) -> Result<Option<Message>, Error> {
+    let line = line.trim();
+    if line.is_empty() {
+        return Ok(None);
+    }
+    if !line.starts_with("[") {
+        return Err(anyhow::anyhow!("Missing opening bracket: {}", line));
+    }
+    let Some(closing_bracket) = line.find(']') else {
+        return Err(anyhow::anyhow!("Missing closing bracket: {}", line));
+    };
+
+    let topic = line[1..closing_bracket].to_string();
+    let payload = line[closing_bracket + 1..].to_string();
+
+    Ok(Some(Message { topic, payload }))
+}
+
+async fn next_message(input: &mut BufReader<Stdin>) -> Option<Message> {
+    let mut line = String::new();
+    match input.read_line(&mut line).await {
+        Ok(0) => None,
+        Ok(_) => match parse(line) {
+            Ok(message) => message,
+            Err(err) => {
+                eprintln!("Fail to parse input message {}", err);
+                None
+            }
+        },
+        Err(err) => {
+            eprintln!("Fail to read input stream {}", err);
+            None
         }
     }
 }
