95e5f4a74dc4b27cbd2896f6bc1e521fbf9a1771
Marcel Guzik <marcel.guzik@cumulocity.com>
2025-09-16 11:38:20 +0000

    fixup! feat: Add `tedge cert create-key` command

--- crates/extensions/tedge-p11-server/src/pkcs11/mod.rs
+++ crates/extensions/tedge-p11-server/src/pkcs11/mod.rs
@@ -679,8 +679,7 @@ impl CryptokiSession<'_> {
         };
 
         // generate unique ids
-        let mut id = vec![0u8; 20];
-        rand::fill(&mut id[..]);
+        let id: [u8; 20] = rand::random();
 
         let mut pub_key_template = attrs_pub;
         pub_key_template.extend_from_slice(&[
@@ -689,7 +688,7 @@ impl CryptokiSession<'_> {
             Attribute::Verify(true),
             Attribute::Encrypt(true),
             Attribute::Label(params.label.clone().into()),
-            Attribute::Id(id.clone()),
+            Attribute::Id(id.to_vec()),
         ]);
 
         let mut priv_key_template = attrs_priv;
@@ -701,7 +700,7 @@ impl CryptokiSession<'_> {
             Attribute::Sign(true),
             Attribute::Decrypt(true),
             Attribute::Label(params.label.clone().into()),
-            Attribute::Id(id.clone()),
+            Attribute::Id(id.to_vec()),
         ]);
 
         trace!(?pub_key_template, ?priv_key_template, "Generating keypair");
