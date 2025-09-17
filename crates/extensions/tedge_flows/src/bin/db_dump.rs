144532a11a779050bec9bf48dfe30361d29df15d
James Rhodes <jarhodes314@gmail.com>
2025-09-17 11:41:51 +0100

    Refactor database into separate module, and add in memory database for testing

--- crates/extensions/tedge_flows/src/bin/db_dump.rs
+++ crates/extensions/tedge_flows/src/bin/db_dump.rs
@@ -1,7 +1,8 @@
 use anyhow::Context;
+use tedge_flows::database::FjallMeaDb;
+use tedge_flows::database::MeaDb;
 use tedge_flows::flow::DateTime;
 use tedge_flows::flow::Message;
-use tedge_flows::MeaDB;
 
 const DB_PATH: &str = "/etc/tedge/tedge-gen.db";
 
@@ -13,7 +14,7 @@ async fn main() -> anyhow::Result<()> {
     let series_name = args.nth(1).unwrap_or("latest-data-points".to_string());
     println!("Reading series name: {series_name}");
 
-    let mut db = MeaDB::open(DB_PATH)
+    let mut db = FjallMeaDb::open(DB_PATH)
         .await
         .with_context(|| format!("Failed to open DB at path={DB_PATH}"))?;
 
