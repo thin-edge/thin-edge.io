5478d39ff1a9d0dddfbd07446ab1abb3488500d0
Albin Suresh <albin.suresh@cumulocity.com>
2025-10-17 12:03:13 +0000

    feat: publish supported log types on config_update completion

--- crates/extensions/tedge_log_manager/src/config.rs
+++ crates/extensions/tedge_log_manager/src/config.rs
@@ -63,10 +63,14 @@ impl LogManagerConfig {
             ChannelFilter::Command(OperationType::LogUpload),
         );
 
-        let log_metadata_sync_topics = mqtt_schema.topics(
+        let mut log_metadata_sync_topics = mqtt_schema.topics(
             EntityFilter::Entity(&mqtt_device_topic_id),
             ChannelFilter::Command(OperationType::SoftwareUpdate),
         );
+        log_metadata_sync_topics.add_all(mqtt_schema.topics(
+            EntityFilter::Entity(&mqtt_device_topic_id),
+            ChannelFilter::Command(OperationType::ConfigUpdate),
+        ));
 
         Ok(Self {
             mqtt_schema,
