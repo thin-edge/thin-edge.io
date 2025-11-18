99a2eabcd39377e8aa3dcfc8df6694bd964c1fe3
Marcel Guzik <marcel.guzik@cumulocity.com>
2025-11-18 09:49:01 +0000

    tests: Add postcard example serde tests
    Add some example tests that showcase properties of the postcard format.
    
    Signed-off-by: Marcel Guzik <marcel.guzik@cumulocity.com>

--- crates/extensions/tedge-p11-server/src/proxy/frame.rs
+++ crates/extensions/tedge-p11-server/src/proxy/frame.rs
@@ -18,3 +18,78 @@ pub use super::frame1::ProtocolError;
 pub enum Frame {
     Version1(Frame1),
 }
+
+/// Documents the properties of the serialization/deserialization format for purposes of adding new requests while
+/// maintaining compatibility.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum F1 {
+        V1 { a: u32 },
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum F2 {
+        V1 { a: u32, b: u16 },
+    }
+
+    /// Shows that when a new field is added to a request, it can't be deserialized if sent as older type.
+    #[test]
+    fn cant_deserialize_older_as_newer_when_new_fields() {
+        let f1 = F1::V1 { a: 42 };
+        let f1 = postcard::to_allocvec(&f1).unwrap();
+        let f2 = postcard::from_bytes::<F2>(&f1);
+
+        f2.expect_err("can't deserialize older type into newer type");
+    }
+
+    /// Shows that when a new field is added to a request, new type can still be deserialized as the older type (new
+    /// fields are appended at the end but are ignored by the deserializer).
+    #[test]
+    fn can_deserialize_newer_as_older_when_new_fields_added_after() {
+        let f2 = F2::V1 { a: 42, b: 9 };
+        let f2 = postcard::to_allocvec(&f2).unwrap();
+        let f1 = postcard::from_bytes::<F1>(&f2);
+
+        f1.unwrap();
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    enum F3 {
+        V1 { b: u16, a: u32 },
+    }
+
+    /// postcard is not self-describing, so fields can be confused if new fields are added before older ones
+    #[test]
+    fn cant_deserialize_newer_as_older_when_new_fields_added_before() {
+        let f3 = F3::V1 { a: 42, b: 9 };
+        let bytes = postcard::to_allocvec(&f3).unwrap();
+        let f1 = postcard::from_bytes::<F1>(&bytes).unwrap();
+
+        let F3::V1 { a: f3_a, .. } = f3;
+        let F1::V1 { a: f1_a, .. } = f1;
+
+        // fields are serialized without identifiers, and with the order as defined in the type, so F3::V1.b becomes
+        // F1::V1.a, because it's the first number
+        assert_eq!(f3_a, 42);
+        assert_eq!(f1_a, 9);
+    }
+
+    #[test]
+    fn deserialize_demo() {
+        let frame = Frame::Version1(Frame1::Error(ProtocolError("aaaa".to_string())));
+        let bytes = postcard::to_allocvec(&frame).unwrap();
+
+        postcard::from_bytes::<ProtocolError>(&bytes[2..]).unwrap();
+        postcard::from_bytes::<Frame1>(&bytes[1..]).unwrap();
+        postcard::from_bytes::<Frame>(&bytes).unwrap();
+        dbg!(bytes);
+
+        let a = "Hello, World!".as_bytes();
+        let mut b = [0u8; 5];
+        let b_len = b.len();
+        b.copy_from_slice(&a[..b_len]);
+    }
+}
