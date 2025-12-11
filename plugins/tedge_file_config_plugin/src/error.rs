43272e962551dd85627e553be4b944d01118c11f
Albin Suresh <albin.suresh@cumulocity.com>
2025-12-11 11:52:24 +0000

    feat: file config management plugin

---
+++ plugins/tedge_file_config_plugin/src/error.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::path::PathBuf;
+
+use camino::Utf8PathBuf;
+
+#[derive(Debug, thiserror::Error)]
+pub enum PluginError {
+    #[error("The requested config_type {0} is not defined in the plugin configuration file.")]
+    InvalidConfigType(String),
+
+    #[error("Config file not found: {0}")]
+    FileNotFound(Utf8PathBuf),
+
+    #[error("Failed to parse TOML config: {0}")]
+    TomlError(#[from] toml::de::Error),
+
+    #[error("Failed to create parent directory: {path}")]
+    ParentDirCreationFailed { path: PathBuf, source: io::Error },
+
+    #[error("Failed to set file permissions: {path}")]
+    PermissionError { path: PathBuf, source: io::Error },
+
+    #[error(transparent)]
+    AnyhowError(#[from] anyhow::Error),
+}
