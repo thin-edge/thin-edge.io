f500c405daa76f7e2a0e7c5a9684b0d4c3a935aa
Didier Wenzek <didier.wenzek@free.fr>
2025-12-12 17:23:05 +0100

    fixup! fixup! The flow-based az mapper is fully compatible with the legacy version

--- crates/extensions/tedge_flows/src/transformers/add_timestamp.rs
+++ crates/extensions/tedge_flows/src/transformers/add_timestamp.rs
@@ -3,6 +3,8 @@ use crate::transformers::Transformer;
 use crate::FlowError;
 use crate::Message;
 use std::time::SystemTime;
+use tedge_utils::timestamp::TimeFormat;
+use time::OffsetDateTime;
 
 #[derive(Default, Clone)]
 pub struct AddTimestamp;
@@ -14,7 +16,7 @@ impl Transformer for AddTimestamp {
 
     fn on_message(
         &self,
-        timestamp: SystemTime,
+        time: SystemTime,
         message: &Message,
         config: &JsonValue,
     ) -> Result<Vec<Message>, FlowError> {
@@ -25,19 +27,30 @@ impl Transformer for AddTimestamp {
         };
 
         let time_property = config.string_property("property").unwrap_or("time");
-        if json_message.get(time_property).is_some() {
-            return Ok(vec![message.clone()]);
-        }
+        let format_name = config.string_property("format").unwrap_or("unix");
+        let reformat = config.bool_property("reformat").unwrap_or(false);
+        let Ok(format) = TimeFormat::try_from(format_name) else {
+            return Err(FlowError::IncorrectSetting(format!(
+                "Unknown time format: {format_name}"
+            )));
+        };
 
         let mut json_message = json_message;
-        json_message.insert(
-            time_property.to_owned(),
-            timestamp
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs()
-                .into(),
-        );
+
+        let result = match json_message.get(time_property) {
+            Some(_) if !reformat => return Ok(vec![message.clone()]),
+            Some(timestamp) => format.reformat_json(timestamp.clone()),
+            None => format.to_json(OffsetDateTime::from(time)),
+        };
+
+        let Ok(new_timestamp) = result else {
+            return Err(FlowError::UnsupportedMessage(format!(
+                "Failed to format message timestamp as {format_name}"
+            )));
+        };
+
+        json_message.insert(time_property.to_owned(), new_timestamp);
+
         let transformed_topic = message.topic.to_owned();
         let transformed_payload = serde_json::Value::Object(json_message).to_string();
         Ok(vec![Message::new(transformed_topic, transformed_payload)])
