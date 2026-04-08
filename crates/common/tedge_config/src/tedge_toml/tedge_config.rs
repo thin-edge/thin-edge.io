11dce39424c90967a8d6a6dee176ec316c4720dc
reubenmiller <reuben.d.miller@gmail.com>
2026-04-08 21:04:23 +0200

    decrease initial backoff interval to align more closely with the mosquitto bridge backoff algorithm
    Signed-off-by: reubenmiller <reuben.d.miller@gmail.com>

--- crates/common/tedge_config/src/tedge_toml/tedge_config.rs
+++ crates/common/tedge_config/src/tedge_toml/tedge_config.rs
@@ -1153,7 +1153,7 @@ define_tedge_config! {
 
             reconnect_policy: {
                 /// The minimum time the built-in bridge will wait before reconnecting
-                #[tedge_config(example = "30s", default(from_str = "30s"))]
+                #[tedge_config(example = "30s", default(from_str = "5s"))]
                 initial_interval: SecondsOrHumanTime,
 
                 /// The maximum time the built-in bridge will wait before reconnecting
